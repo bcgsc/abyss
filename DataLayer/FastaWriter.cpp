f8e9ce5b5f04dc47512996cb89f438c1589071e3
Shaun Jackman <sjackman@bcgsc.ca>
2008-06-12 22:21:06 +0000

    * DataLayer/FastaWriter.cpp (WriteSequence): Use the MPI rank in the contig identifier to guarantee that it is unique.
    git-svn-id: https://svn.bcgsc.ca/svn/personal/jsimpson/partitionAssembly/PathWalker/src@39406 ad02020f-2a26-4133-947e-1829317c1416

--- DataLayer/FastaWriter.cpp
+++ DataLayer/FastaWriter.cpp
@@ -1,4 +1,5 @@
 #include "FastaWriter.h"
+#include "Options.h"
 #include <cstdio>
 #include <sys/file.h>
 
@@ -21,10 +22,17 @@ void FastaWriter::WriteSequence(const Sequence& seq,
 		const int64_t id, const double multiplicity)
 {
 	assert(m_fileHandle != NULL);
-	int fd = fileno(m_fileHandle);
-	flock(fd, LOCK_EX);
-	fprintf(m_fileHandle, ">%llu %u %g\n%s\n",
-			id, seq.length(), multiplicity, seq.c_str());
-	fflush(m_fileHandle);
-	flock(fd, LOCK_UN);
+	if (opt::rank < 0) {
+		// Non-parallel
+		fprintf(m_fileHandle, ">%llu %u %g\n%s\n",
+				id, seq.length(), multiplicity, seq.c_str());
+	} else {
+		// Parallel
+		int fd = fileno(m_fileHandle);
+		flock(fd, LOCK_EX);
+		fprintf(m_fileHandle, ">%u:%llu %u %g\n%s\n", opt::rank,
+				id, seq.length(), multiplicity, seq.c_str());
+		fflush(m_fileHandle);
+		flock(fd, LOCK_UN);
+	}
 }
