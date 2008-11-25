b78e9eed8f00abb53bbe3094711039310121af0f
Shaun Jackman <sjackman@bcgsc.ca>
2008-11-25 19:51:01 +0000

    * Common/AssemblyAlgorithms.cpp (erodeEnds): Trim the end of a blunt contig unless it is represented in both strands.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@42837 ad02020f-2a26-4133-947e-1829317c1416

--- Common/AssemblyAlgorithms.cpp
+++ Common/AssemblyAlgorithms.cpp
@@ -586,9 +586,6 @@ void removeExtensionsToSequence(ISequenceCollection* seqCollection, const Packed
 	}	
 }
 
-/** Remove tips with multiplicity at or below this limit. */
-enum { EROSION_MULTIPLICITY = 1 };
-
 //
 // Erode data off the ends of the graph, one by one
 //
@@ -607,7 +604,8 @@ unsigned erodeEnds(ISequenceCollection* seqCollection)
 		{
 			continue;
 		}
-		else if (iter->getMultiplicity() <= EROSION_MULTIPLICITY) {
+		else if (iter->getMultiplicity(SENSE) < 1
+				|| iter->getMultiplicity(ANTISENSE) < 1) {
 			count++;
 			// This sequence is an endpoint, erode it
 			removeSequenceAndExtensions(seqCollection, *iter);			
