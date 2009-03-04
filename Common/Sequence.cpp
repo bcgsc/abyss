f4a41c3159b940e8260053876ec8cc10065850a2
Shaun Jackman <sjackman@bcgsc.ca>
2009-03-04 22:58:33 +0000

    * Sequence.h (SequenceAdjacency, SEQUENCE_ID_LENGTH, seqAppend, seqAppendBase, seqPrependBase): Remove this unused cruft. * Sequence.cpp: Ditto. Include cassert.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@45575 ad02020f-2a26-4133-947e-1829317c1416

--- Common/Sequence.cpp
+++ Common/Sequence.cpp
@@ -1,30 +1,4 @@
-#include <stdio.h>
-#include <map>
-#include <math.h>
 #include "Sequence.h"
-
-
-// append another string
-void seqAppend(Sequence& s, const std::string& str)
-{
-	s.append(str);
-}
-
-// append a base to the string
-void seqAppendBase(Sequence& s, const char b)
-{
-	s.append(1,b);
-}
-
-// prepend a base to the string
-void seqPrependBase(Sequence& s, const char b)
-{
-	// stl string doesnt have a prepend function
-	std::string temp = s;
-	s = b;
-	s.append(temp);
-	
-}
 
 // generate the reverse complement of the sequence
 Sequence reverseComplement(const Sequence& s)
@@ -38,13 +12,6 @@ Sequence reverseComplement(const Sequence& s)
 	
 	return rc;
 }
-
-// print the sequence
-void print(const Sequence& s)
-{
-	printf("%s", s.c_str());	
-}
-
 
 /** Return the complement of the specified base. */
 char complementBaseChar(char base)
--- Common/Sequence.h
+++ Common/Sequence.h
@@ -1,29 +1,13 @@
 #ifndef SEQUENCE_H
-#define SEQUENCE_H
+#define SEQUENCE_H 1
 
 #include <string>
 #include <vector>
-
-enum SequenceAdjacency
-{
-	SA_NONE,
-	SA_SAME_SENSE,
-	SA_SAME_ANTISENSE,
-	SA_RC_SENSE,
-	SA_RC_ANTISENSE	
-};
-
-const int SEQUENCE_ID_LENGTH = 256;
 
 typedef std::string Sequence;
 
 char complementBaseChar(char base);
 Sequence reverseComplement(const Sequence& s);
-
-// append a base to the string
-void seqAppend(Sequence& s, const std::string& str);
-void seqAppendBase(Sequence& s, const char b);
-void seqPrependBase(Sequence& s, const char b);
 
 typedef std::vector<Sequence> SequenceVector;
 typedef SequenceVector::iterator SequenceVectorIterator;
