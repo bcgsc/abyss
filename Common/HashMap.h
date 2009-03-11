598b80025efd6b44c26a618596a4051d3548535f
Shaun Jackman <sjackman@bcgsc.ca>
2009-03-11 00:41:30 +0000

    * configure.ac (AC_CHECK_HEADERS): Simply. Check for ext/hash_map but not backward/hash_map nor hash_map. tr1/unordered_map was added before backward/hash_map. * Common/HashSet.h: Ditto. * Common/HashMap.h: Ditto.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@45743 ad02020f-2a26-4133-947e-1829317c1416

--- Common/HashMap.h
+++ Common/HashMap.h
@@ -9,22 +9,16 @@
 #elif HAVE_TR1_UNORDERED_MAP
 # include <tr1/unordered_map>
 # define hash_multimap std::tr1::unordered_multimap
-#elif HAVE_BACKWARD_HASH_MAP || HAVE_EXT_HASH_MAP || HAVE_HASH_MAP
+#elif HAVE_EXT_HASH_MAP
 # undef __DEPRECATED
-# if HAVE_BACKWARD_HASH_MAP
-#  include <backward/hash_map>
-# elif HAVE_EXT_HASH_MAP
-#  include <ext/hash_map>
-# elif HAVE_HASH_MAP
-#  include <hash_map>
-# endif
+# include <ext/hash_map>
 using __gnu_cxx::hash_multimap;
 #else
 #  error A hash map implementation is required.
 #endif
 
 #if HAVE_UNORDERED_MAP || HAVE_TR1_UNORDERED_MAP
-#elif HAVE_BACKWARD_HASH_MAP || HAVE_EXT_HASH_MAP || HAVE_HASH_MAP
+#elif HAVE_EXT_HASH_MAP
 // Implement a string hash function so that a string can be used as
 // a key in STL maps and set.
 namespace __gnu_cxx {
--- Common/HashSet.h
+++ Common/HashSet.h
@@ -9,15 +9,9 @@
 #elif HAVE_TR1_UNORDERED_SET
 # include <tr1/unordered_set>
 # define hash_set std::tr1::unordered_set
-#elif HAVE_BACKWARD_HASH_SET || HAVE_EXT_HASH_SET || HAVE_HASH_SET
+#elif HAVE_EXT_HASH_SET
 # undef __DEPRECATED
-# if HAVE_BACKWARD_HASH_SET
-#  include <backward/hash_set>
-# elif HAVE_EXT_HASH_SET
-#  include <ext/hash_set>
-# elif HAVE_HASH_SET
-#  include <hash_set>
-# endif
+# include <ext/hash_set>
 using __gnu_cxx::hash_set;
 #else
 #  error A hash set implementation is required.
