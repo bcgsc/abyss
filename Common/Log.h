ec9372b9508c01c8306974ba9795d492b9ea2682
Shaun Jackman <sjackman@bcgsc.ca>
2009-03-24 21:27:44 +0000

    * Common/Log.h (PrintDebug): Add a format-checking attribute for GCC.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@46153 ad02020f-2a26-4133-947e-1829317c1416

--- Common/Log.h
+++ Common/Log.h
@@ -1,6 +1,7 @@
 #ifndef LOG_H
 #define LOG_H 1
 
-int PrintDebug(int level, const char* format, ...);
+int PrintDebug(int level, const char* format, ...)
+	__attribute__((format(printf, 2, 3)));
 
 #endif
