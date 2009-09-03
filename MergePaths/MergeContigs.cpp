e79a440d2b04ad0438a950e5aeed6337340ed7a6
Shaun Jackman <sjackman@bcgsc.ca>
2009-09-03 18:55:02 +0000

    * MergePaths/MergeContigs.cpp: New file. Merge paths of contigs to create larger contigs. * MergePaths/Makefile.am (MergeContigs): New program.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@50946 ad02020f-2a26-4133-947e-1829317c1416

---
+++ MergePaths/MergeContigs.cpp
@@ -0,0 +1,272 @@
+#include "config.h"
+#include "Dictionary.h"
+#include "FastaReader.h"
+#include "FastaWriter.h"
+#include <algorithm>
+#include <cstdlib>
+#include <cerrno>
+#include <cstring> // for strerror
+#include <fstream>
+#include <getopt.h>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define PROGRAM "MergeContigs"
+
+static const char *VERSION_MESSAGE =
+PROGRAM " (ABySS) " VERSION "\n"
+"Written by Shaun Jackman.\n"
+"\n"
+"Copyright 2009 Canada's Michael Smith Genome Science Centre\n";
+
+static const char *USAGE_MESSAGE =
+"Usage: " PROGRAM " [OPTION]... CONTIG PATH\n"
+"Merge paths of contigs to create larger contigs.\n"
+"  CONTIG  contigs in FASTA format\n"
+"  PATH    paths of these contigs\n"
+"\n"
+"  -k, --kmer=KMER_SIZE  k-mer size\n"
+"  -o, --out=FILE        write result to FILE\n"
+"  -v, --verbose         display verbose output\n"
+"      --help            display this help and exit\n"
+"      --version         output version information and exit\n"
+"\n"
+"Report bugs to <" PACKAGE_BUGREPORT ">.\n";
+
+namespace opt {
+	static unsigned k;
+	static int verbose;
+	static string out;
+	extern bool colourSpace;
+}
+
+static const char* shortopts = "k:o:v";
+
+enum { OPT_HELP = 1, OPT_VERSION };
+
+static const struct option longopts[] = {
+	{ "kmer",        required_argument, NULL, 'k' },
+	{ "out",         required_argument, NULL, 'o' },
+	{ "verbose",     no_argument,       NULL, 'v' },
+	{ "help",        no_argument,       NULL, OPT_HELP },
+	{ "version",     no_argument,       NULL, OPT_VERSION },
+	{ NULL, 0, NULL, 0 }
+};
+
+static unsigned line_num;
+
+struct ContigNode {
+	string id;
+	bool sense;
+
+	friend istream& operator >>(istream& in, ContigNode& o)
+	{
+		unsigned id;
+		char c;
+		in >> id >> c;
+
+		ostringstream ss;
+		ss << id;
+		o.id = ss.str();
+
+		if (c != '+' && c != '-') {
+			cerr << "error: " << line_num
+				<< ": expected `+' or `-' and saw `" << c << '\''
+				<< endl;
+			exit(EXIT_FAILURE);
+		}
+		o.sense = c == '-';
+
+		return in;
+	}
+
+	friend ostream& operator <<(ostream& out, const ContigNode& o)
+	{
+		return out << o.id << (o.sense ? '-' : '+');
+	}
+};
+
+typedef vector<ContigNode> Path;
+
+static const string& contigNodeToId(const ContigNode& node)
+{
+	return node.id;
+}
+
+static set<string> getContigIDs(const vector<Path>& paths)
+{
+	set<string> seen;
+	for (vector<Path>::const_iterator it = paths.begin();
+			it != paths.end(); ++it)
+		transform(it->begin(), it->end(),
+				inserter(seen, seen.begin()),
+				contigNodeToId);
+	return seen;
+}
+
+static void assert_open(std::ifstream& f, const std::string& p)
+{
+	if (f.is_open())
+		return;
+	std::cerr << p << ": " << strerror(errno) << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+static Dictionary g_dict;
+
+struct Contig {
+	string id;
+    Sequence seq;
+    unsigned coverage;
+    Contig(string id, Sequence seq, unsigned coverage)
+        : id(id), seq(seq), coverage(coverage) { }
+};
+
+static Contig mergePath(const Path& path, const string& id,
+		const vector<Contig>& contigs)
+{
+	Sequence seq;
+	unsigned coverage = 0;
+	for (Path::const_iterator it = path.begin();
+			it != path.end(); ++it) {
+		unsigned serial = g_dict.serial(it->id);
+		const Contig& contig = contigs[serial];
+		coverage += contig.coverage;
+
+		Sequence h = it->sense ? reverseComplement(contig.seq)
+			: contig.seq;
+		if (seq.empty()) {
+			seq = h;
+			continue;
+		}
+
+		unsigned overlap = opt::k - 1;
+		Sequence a(seq, 0, seq.length() - overlap);
+		Sequence ao(seq, seq.length() - overlap);
+		Sequence bo(h, 0, overlap);
+		Sequence b(h, overlap);
+		if (ao != bo) {
+			cerr << "error: the head of `" << contig.id << "' "
+				"does not match the tail of the previous contig\n"
+				<< ao << '\n' << bo << '\n';
+			copy(path.begin(), path.end(),
+					ostream_iterator<ContigNode>(cerr, " "));
+			cerr << endl;
+			exit(EXIT_FAILURE);
+		}
+		seq += b;
+	}
+	return Contig(id, seq, coverage);
+}
+
+int main(int argc, char** argv)
+{
+	bool die = false;
+	for (int c; (c = getopt_long(argc, argv,
+					shortopts, longopts, NULL)) != -1;) {
+		istringstream arg(optarg != NULL ? optarg : "");
+		switch (c) {
+			case '?': die = true; break;
+			case 'k': arg >> opt::k; break;
+			case 'o': arg >> opt::out; break;
+			case 'v': opt::verbose++; break;
+			case OPT_HELP:
+				cout << USAGE_MESSAGE;
+				exit(EXIT_SUCCESS);
+			case OPT_VERSION:
+				cout << VERSION_MESSAGE;
+				exit(EXIT_SUCCESS);
+		}
+	}
+
+	if (opt::k <= 0) {
+		cerr << PROGRAM ": missing -k,--kmer option\n";
+		die = true;
+	}
+
+	if (opt::out.empty()) {
+		cerr << PROGRAM ": " << "missing -o,--out option\n";
+		die = true;
+	}
+
+	if (argc - optind < 2) {
+		cerr << PROGRAM ": missing arguments\n";
+		die = true;
+	} else if (argc - optind > 2) {
+		cerr << PROGRAM ": too many arguments\n";
+		die = true;
+	}
+
+	if (die) {
+		cerr << "Try `" << PROGRAM
+			<< " --help' for more information.\n";
+		exit(EXIT_FAILURE);
+	}
+
+	const char* contigFile = argv[optind++];
+	string pathFile(argv[optind++]);
+
+	vector<Contig> contigs;
+	{
+		FastaReader in(contigFile, FastaReader::KEEP_N);
+		for (FastaRecord rec; in >> rec;) {
+			istringstream ss(rec.comment);
+			unsigned length, coverage = 0;
+			ss >> length >> coverage;
+			unsigned serial = g_dict.serial(rec.id);
+			assert(contigs.size() == serial);
+			contigs.push_back(Contig(rec.id, rec.seq, coverage));
+		}
+		assert(in.eof());
+		assert(!contigs.empty());
+		opt::colourSpace = isdigit(contigs[0].seq[0]);
+	}
+
+	vector<Path> paths;
+	{
+		ifstream fin(pathFile.c_str());
+		if (pathFile != "-")
+			assert_open(fin, pathFile);
+		istream& in = pathFile == "-" ? cin : fin;
+		for (string s; getline(in, s);) {
+			line_num++;
+			istringstream ss(s);
+			Path path;
+			copy(istream_iterator<ContigNode>(ss),
+					istream_iterator<ContigNode>(),
+					back_inserter(path));
+			paths.push_back(path);
+		}
+		assert(in.eof());
+	}
+
+	FastaWriter writer(opt::out.c_str());
+	set<string> seen = getContigIDs(paths);
+	for (vector<Contig>::const_iterator it = contigs.begin();
+			it != contigs.end(); ++it)
+		if (seen.count(it->id) == 0)
+			writer.WriteSequence(it->seq, it->id, it->coverage);
+
+	int id = contigs.size();
+	for (vector<Path>::const_iterator it = paths.begin();
+			it != paths.end(); ++it) {
+		const Path& path = *it;
+		ostringstream ss;
+		ss << id++;
+		Contig contig = mergePath(path, ss.str(), contigs);
+		ss.str("");
+		copy(path.begin(), path.end()-1,
+				ostream_iterator<ContigNode>(ss, ","));
+		ss << path.back();
+		writer.WriteSequence(contig.seq, contig.id,
+				contig.coverage, ss.str());
+	}
+
+	return 0;
+}
