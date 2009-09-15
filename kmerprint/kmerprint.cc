4c2d2d2556ed8566e25b98ae28e75ae6688eb485
Shaun Jackman <sjackman@bcgsc.ca>
2009-09-15 18:15:07 +0000

    * kmerprint/kmerprint.cc: New file. Print a kmer file. A kmer file is a serialized Google sparsehash. * kmerprint/Makefile.am: Ditto. * kmerprint: New directory. * configure.ac: Add kmerprint. * Makefile.am: Ditto.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@51159 ad02020f-2a26-4133-947e-1829317c1416

---
+++ kmerprint/kmerprint.cc
@@ -0,0 +1,49 @@
+#include "SequenceCollectionHash.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+namespace opt {
+	bool sequence = true;
+	bool strands = true;
+}
+
+static void print(const PackedSeq& seq)
+{
+	if (opt::sequence)
+		cout << seq.decode() << '\t';
+	cout << seq.getMultiplicity() << '\n';
+}
+
+static void print(const PackedSeq& seq, extDirection sense)
+{
+	if (opt::sequence) {
+		if (sense) {
+			PackedSeq rc(seq);
+			rc.reverseComplement();
+			cout << rc.decode();
+		} else
+			cout << seq.decode();
+		cout << '\t';
+	}
+	cout << seq.getMultiplicity(sense) << '\n';
+}
+
+int main(int argc, const char* argv[])
+{
+	SequenceCollectionHash c;
+	assert(argc > 1);
+	c.load(argv[1]);
+	for (SequenceCollectionHash::const_iterator it = c.begin();
+			it != c.end(); ++it) {
+		if (it->deleted())
+			continue;
+		if (opt::strands) {
+			print(*it, SENSE);
+			print(*it, ANTISENSE);
+		} else
+			print(*it);
+	}
+	return 0;
+}
