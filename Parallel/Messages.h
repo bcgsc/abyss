eec00afa1606df42744bfc4ca27e8c81e754730c
Shaun Jackman <sjackman@bcgsc.ca>
2009-10-26 20:22:00 +0000

    * Parallel/Messages.h: Tidy. (destructor, getNetworkSize, handle, serialize, unserialize, print): Not virtual in derived classes.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@52393 ad02020f-2a26-4133-947e-1829317c1416

--- Parallel/Messages.h
+++ Parallel/Messages.h
@@ -5,12 +5,7 @@
 #include "PackedSeq.h"
 #include <ostream>
 
-// Forward declare the network sequence class
 class NetworkSequenceCollection;
-
-//
-// Messages - a collection of inherited classes used to pass data around a network
-//
 
 enum MessageType
 {
@@ -43,7 +38,6 @@ class Message
 			: m_type(m_type), m_seq(seq) { }
 		virtual ~Message() {}
 
-		// handle this message
 		virtual void handle(int senderID, NetworkSequenceCollection& handler) = 0;
 
 		virtual size_t getNetworkSize() const
@@ -67,31 +61,24 @@ class Message
 		Kmer m_seq;
 };
 
-//
-// REQUEST MESSAGES
-//
-
 class SeqOpMessage : public Message
 {
 	public:
 		SeqOpMessage() : Message(MT_SEQ_OP) { }
 		SeqOpMessage(const Kmer& seq, MessageOp operation)
 			: Message(seq, MT_SEQ_OP), m_operation(operation) { }
-		virtual ~SeqOpMessage() { }
+		~SeqOpMessage() { }
 
-		virtual size_t getNetworkSize() const { return (Message::getNetworkSize() + sizeof(m_operation)); }
-		
-		// Handle the message by calling the appropriate function in the network sequence collection
-		virtual void handle(int senderID, NetworkSequenceCollection& handler);
-		
-		// Write the message into a buffer
-		virtual size_t serialize(char* buffer);
-		
-		// Read the message back from a buffer
-		virtual size_t unserialize(const char* buffer);
-		
-		virtual void print() const;
-				
+		size_t getNetworkSize() const
+		{
+			return Message::getNetworkSize() + sizeof m_operation;
+		}
+
+		void handle(int senderID, NetworkSequenceCollection& handler);
+		size_t serialize(char* buffer);
+		size_t unserialize(const char* buffer);
+		void print() const;
+
 		MessageOp m_operation;
 };
 
@@ -101,22 +88,18 @@ class SetFlagMessage : public Message
 		SetFlagMessage() : Message(MT_SET_FLAG) { }
 		SetFlagMessage(const Kmer& seq, SeqFlag flag)
 			: Message(seq, MT_SET_FLAG), m_flag(flag) { }
-		virtual ~SetFlagMessage() { }
+		~SetFlagMessage() { }
 
-		// Get the size that will be transmitted
-		virtual size_t getNetworkSize() const { return (Message::getNetworkSize() + sizeof(m_flag)); }
-		
-		// Handle the message by calling the appropriate function in the network sequence collection
-		virtual void handle(int senderID, NetworkSequenceCollection& handler);
-		
-		// Write the message into a buffer
-		virtual size_t serialize(char* buffer);
-		
-		// Read the message back from a buffer
-		virtual size_t unserialize(const char* buffer);
-		
-		virtual void print() const;
-				
+		size_t getNetworkSize() const
+		{
+			return Message::getNetworkSize() + sizeof m_flag;
+		}
+
+		void handle(int senderID, NetworkSequenceCollection& handler);
+		size_t serialize(char* buffer);
+		size_t unserialize(const char* buffer);
+		void print() const;
+
 		SeqFlag m_flag;
 };
 
@@ -127,18 +110,18 @@ class RemoveExtensionMessage : public Message
 		RemoveExtensionMessage(const Kmer& seq,
 				extDirection dir, SeqExt ext)
 			: Message(seq, MT_REMOVE_EXT), m_dir(dir), m_ext(ext) { }
-		virtual ~RemoveExtensionMessage() { }
+		~RemoveExtensionMessage() { }
 
-		virtual size_t getNetworkSize() const
+		size_t getNetworkSize() const
 		{
 			return Message::getNetworkSize()
 				+ sizeof m_dir + sizeof m_ext;
 		}
 
-		virtual void handle(int senderID, NetworkSequenceCollection& handler);
-		virtual size_t serialize(char* buffer);
-		virtual size_t unserialize(const char* buffer);
-		virtual void print() const;
+		void handle(int senderID, NetworkSequenceCollection& handler);
+		size_t serialize(char* buffer);
+		size_t unserialize(const char* buffer);
+		void print() const;
 
 		extDirection m_dir;
 		SeqExt m_ext;
@@ -151,27 +134,22 @@ class SeqDataRequest : public Message
 		SeqDataRequest(const Kmer& seq, IDType group, IDType id)
 			: Message(seq, MT_SEQ_DATA_REQUEST),
 				m_group(group), m_id(id) { }
-		virtual ~SeqDataRequest() { }
+		~SeqDataRequest() { }
 
-		// Get the size that will be transmitted
-		virtual size_t getNetworkSize() const { return (Message::getNetworkSize() + sizeof(m_group) + sizeof(m_id)); }		
-		
-		// Handle the message by calling the appropriate function in the network sequence collection
-		virtual void handle(int senderID, NetworkSequenceCollection& handler);
-		
-		// Write the message into a buffer
-		virtual size_t serialize(char* buffer);
-		
-		// Read the message back from a buffer
-		virtual size_t unserialize(const char* buffer);
-		
-		virtual void print() const;
-				
+		size_t getNetworkSize() const
+		{
+			return Message::getNetworkSize()
+				+ sizeof m_group + sizeof m_id;
+		}
+
+		void handle(int senderID, NetworkSequenceCollection& handler);
+		size_t serialize(char* buffer);
+		size_t unserialize(const char* buffer);
+		void print() const;
+
 		IDType m_group;
 		IDType m_id;
 };
-
-// RESPONSE MESSAGES
 
 class SeqDataResponse : public Message
 {
@@ -182,22 +160,20 @@ class SeqDataResponse : public Message
 			Message(seq, MT_SEQ_DATA_RESPONSE),
 			m_group(group), m_id(id),
 			m_extRecord(extRecord), m_multiplicity(multiplicity) { }
-		virtual ~SeqDataResponse() { }
+		~SeqDataResponse() { }
 
-		// Get the size that will be transmitted
-		virtual size_t getNetworkSize() const { return (Message::getNetworkSize() + sizeof(m_group) + sizeof(m_id) + sizeof(m_extRecord) + sizeof(m_multiplicity)); }
-				
-		// Handle the message by calling the appropriate function in the network sequence collection
-		virtual void handle(int senderID, NetworkSequenceCollection& handler);
-		
-		// Write the message into a buffer
-		virtual size_t serialize(char* buffer);
-		
-		// Read the message back from a buffer
-		virtual size_t unserialize(const char* buffer);
-		
-		virtual void print() const;
-		
+		size_t getNetworkSize() const
+		{
+			return Message::getNetworkSize()
+				+ sizeof m_group + sizeof m_id
+				+ sizeof m_extRecord + sizeof m_multiplicity;
+		}
+
+		void handle(int senderID, NetworkSequenceCollection& handler);
+		size_t serialize(char* buffer);
+		size_t unserialize(const char* buffer);
+		void print() const;
+
 		IDType m_group;
 		IDType m_id;
 		ExtensionRecord m_extRecord;
@@ -211,27 +187,21 @@ class SetBaseMessage : public Message
 		SetBaseMessage(const Kmer& seq,
 				extDirection dir, uint8_t base)
 			: Message(seq, MT_SET_BASE), m_dir(dir), m_base(base) { }
-		virtual ~SetBaseMessage() { }
+		~SetBaseMessage() { }
 
-		// Get the size that will be transmitted
-		virtual size_t getNetworkSize() const { return (Message::getNetworkSize() + sizeof(m_dir) + sizeof(m_base)); }
-		
-		// Handle the message by calling the appropriate function in the network sequence collection
-		virtual void handle(int senderID, NetworkSequenceCollection& handler);
-		
-		// Write the message into a buffer
-		virtual size_t serialize(char* buffer);
-		
-		// Read the message back from a buffer
-		virtual size_t unserialize(const char* buffer);
-		
-		virtual void print() const;
-				
+		size_t getNetworkSize() const
+		{
+			return Message::getNetworkSize()
+				+ sizeof m_dir + sizeof m_base;
+		}
+
+		void handle(int senderID, NetworkSequenceCollection& handler);
+		size_t serialize(char* buffer);
+		size_t unserialize(const char* buffer);
+		void print() const;
+
 		extDirection m_dir;
 		uint8_t m_base;
 };
-
-
-
 
 #endif
