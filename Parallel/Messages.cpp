fe5d4de035b86bae30ed9a13830fd4ecbbc32276
Shaun Jackman <sjackman@bcgsc.ca>
2009-10-30 21:34:02 +0000

    * Parallel/Messages.cpp: Remove white space at the ends of lines.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@52531 ad02020f-2a26-4133-947e-1829317c1416

--- Parallel/Messages.cpp
+++ Parallel/Messages.cpp
@@ -27,7 +27,7 @@ size_t Message::serialize(char* buffer)
 	size_t offset = 0;
 	memcpy(buffer, &m_type, sizeof(m_type));
 	offset += sizeof(m_type);
-	offset += m_seq.serialize(buffer + offset);	
+	offset += m_seq.serialize(buffer + offset);
 	return offset;
 }
 
@@ -36,7 +36,7 @@ size_t Message::unserialize(const char* buffer)
 	size_t offset = 0;
 	offset += unserializeData(&m_type, buffer + offset, sizeof(m_type));
 	offset += m_seq.unserialize(buffer + offset);
-	return offset;	
+	return offset;
 }
 
 size_t SeqOpMessage::serialize(char* buffer)
@@ -50,14 +50,14 @@ size_t SeqOpMessage::serialize(char* buffer)
 size_t SeqOpMessage::unserialize(const char* buffer)
 {
 	size_t offset = 0;
-	offset += Message::unserialize(buffer);	
+	offset += Message::unserialize(buffer);
 	offset += unserializeData(&m_operation, buffer + offset, sizeof(m_operation));
 	return offset;
 }
 
 void SeqOpMessage::handle(int senderID, NetworkSequenceCollection& handler)
 {
-	handler.handleSeqOpMessage(senderID, *this);	
+	handler.handleSeqOpMessage(senderID, *this);
 }
 
 void SeqOpMessage::print() const
@@ -76,19 +76,19 @@ size_t SetFlagMessage::serialize(char* buffer)
 size_t SetFlagMessage::unserialize(const char* buffer)
 {
 	size_t offset = 0;
-	offset += Message::unserialize(buffer);	
+	offset += Message::unserialize(buffer);
 	offset += unserializeData(&m_flag, buffer + offset, sizeof(m_flag));
 	return offset;
 }
 
 void SetFlagMessage::handle(int senderID, NetworkSequenceCollection& handler)
 {
-	handler.handleSetFlagMessage(senderID, *this);	
+	handler.handleSetFlagMessage(senderID, *this);
 }
 
 void SetFlagMessage::print() const
 {
-	printf("Message type: %d Sequence: %s Flag: %d\n", (int)Message::m_type, m_seq.decode().c_str(), (int)m_flag);	
+	printf("Message type: %d Sequence: %s Flag: %d\n", (int)Message::m_type, m_seq.decode().c_str(), (int)m_flag);
 }
 
 size_t RemoveExtensionMessage::serialize(char* buffer)
@@ -111,7 +111,7 @@ size_t RemoveExtensionMessage::unserialize(const char* buffer)
 
 void RemoveExtensionMessage::handle(int senderID, NetworkSequenceCollection& handler)
 {
-	handler.handleRemoveExtensionMessage(senderID, *this);	
+	handler.handleRemoveExtensionMessage(senderID, *this);
 }
 
 void RemoveExtensionMessage::print() const
@@ -126,14 +126,14 @@ size_t SetBaseMessage::serialize(char* buffer)
 	size_t offset = 0;
 	offset += Message::serialize(buffer + offset);
 	offset += serializeData(&m_dir, buffer + offset, sizeof(m_dir));
-	offset += serializeData(&m_base, buffer + offset, sizeof(m_base));	
+	offset += serializeData(&m_base, buffer + offset, sizeof(m_base));
 	return offset;
 }
 
 size_t SetBaseMessage::unserialize(const char* buffer)
 {
 	size_t offset = 0;
-	offset += Message::unserialize(buffer);	
+	offset += Message::unserialize(buffer);
 	offset += unserializeData(&m_dir, buffer + offset, sizeof(m_dir));
 	offset += unserializeData(&m_base, buffer + offset, sizeof(m_base));
 	return offset;
@@ -141,12 +141,12 @@ size_t SetBaseMessage::unserialize(const char* buffer)
 
 void SetBaseMessage::handle(int senderID, NetworkSequenceCollection& handler)
 {
-	handler.handleSetBaseMessage(senderID, *this);	
+	handler.handleSetBaseMessage(senderID, *this);
 }
 
 void SetBaseMessage::print() const
 {
-	printf("Message type: %d Sequence: %s Dir: %d Base: %c\n", (int)Message::m_type, m_seq.decode().c_str(), (int)m_dir, m_base);	
+	printf("Message type: %d Sequence: %s Dir: %d Base: %c\n", (int)Message::m_type, m_seq.decode().c_str(), (int)m_dir, m_base);
 }
 
 size_t SeqDataRequest::serialize(char* buffer)
@@ -161,15 +161,15 @@ size_t SeqDataRequest::serialize(char* buffer)
 size_t SeqDataRequest::unserialize(const char* buffer)
 {
 	size_t offset = 0;
-	offset += Message::unserialize(buffer);	
+	offset += Message::unserialize(buffer);
 	offset += unserializeData(&m_group, buffer + offset, sizeof(m_group));
 	offset += unserializeData(&m_id, buffer + offset, sizeof(m_id));
-	return offset;	
+	return offset;
 }
 
 void SeqDataRequest::handle(int senderID, NetworkSequenceCollection& handler)
 {
-	handler.handleSequenceDataRequest(senderID, *this);	
+	handler.handleSequenceDataRequest(senderID, *this);
 }
 
 void SeqDataRequest::print() const
@@ -191,12 +191,12 @@ size_t SeqDataResponse::serialize(char* buffer)
 size_t SeqDataResponse::unserialize(const char* buffer)
 {
 	size_t offset = 0;
-	offset += Message::unserialize(buffer);	
+	offset += Message::unserialize(buffer);
 	offset += unserializeData(&m_group, buffer + offset, sizeof(m_group));
-	offset += unserializeData(&m_id, buffer + offset, sizeof(m_id));	
+	offset += unserializeData(&m_id, buffer + offset, sizeof(m_id));
 	offset += unserializeData(&m_extRecord, buffer + offset, sizeof(m_extRecord));
 	offset += unserializeData(&m_multiplicity, buffer + offset, sizeof(m_multiplicity));
-	return offset;	
+	return offset;
 }
 
 void SeqDataResponse::handle(int senderID, NetworkSequenceCollection& handler)
