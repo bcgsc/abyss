ccaeea611471b22fd8f6fb403ff50d4382155231
Shaun Jackman <sjackman@bcgsc.ca>
2009-11-03 00:30:12 +0000

    * Common/Sequence.h (SequenceVector, SequenceVectorIterator): Remove these unused typedef.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@52609 ad02020f-2a26-4133-947e-1829317c1416

--- Common/Sequence.h
+++ Common/Sequence.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 #include <string>
-#include <vector>
 
 typedef std::string Sequence;
 
@@ -12,11 +11,7 @@ Sequence colourToNucleotideSpace(char anchor, const Sequence& seq);
 char colourToNucleotideSpace(char anchor, char cs);
 char nucleotideToColourSpace(char a, char b);
 
-// Create the two bit code for the base
 uint8_t baseToCode(char base);
 char codeToBase(uint8_t code);
-
-typedef std::vector<Sequence> SequenceVector;
-typedef SequenceVector::iterator SequenceVectorIterator;
 
 #endif
