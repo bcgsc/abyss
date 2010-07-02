7721fc112ac6757355b76da3b1b5d828faca1d5b
Shaun Jackman <sjackman@bcgsc.ca>
2010-07-02 23:35:19 +0000

    * Common/PDF.h: Tidy. (m_maxIdx, m_mean): Remove these member variables.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@58917 ad02020f-2a26-4133-947e-1829317c1416

--- Common/PDF.h
+++ Common/PDF.h
@@ -2,6 +2,7 @@
 #define STATS_H 1
 
 #include "Histogram.h"
+#include <cassert>
 #include <cmath>
 #include <vector>
 
@@ -10,40 +11,37 @@ class Histogram;
 struct PDF
 {
 	/** Construct a PDF from a histogram. */
-	PDF(const Histogram& h) : m_maxIdx(h.maximum()), m_dist(m_maxIdx + 1),
-		m_mean(h.mean()), m_stdDev(h.sd())
+	PDF(const Histogram& h) : m_dist(h.maximum() + 1), m_stdDev(h.sd())
 	{
 		unsigned count = h.size();
 		m_minp = (double)1 / count;
-
-		for (size_t i = 0; i <= m_maxIdx; i++) {
-			unsigned v = h.count(i);
-			m_dist[i] = v > 0 ? (double)v / count : m_minp;
+		for (size_t i = 0; i < m_dist.size(); i++) {
+			unsigned n = h.count(i);
+			m_dist[i] = n > 0 ? (double)n / count : m_minp;
 		}
 	}
 
 	/** Return the probability of x. */
 	double getP(size_t x) const
 	{
-		return x <= m_maxIdx ? m_dist[x] : m_minp;
+		return x < m_dist.size() ? m_dist[x] : m_minp;
 	}
 
 	double getMinP() const { return m_minp; }
-	size_t getMaxIdx() const { return m_maxIdx; }
+
+	size_t getMaxIdx() const {
+		assert(!m_dist.empty());
+		return m_dist.size() - 1;
+	}
 
 	double getSampleStdDev(unsigned n) const
 	{
-		return m_stdDev / sqrt((double)n);
+		return m_stdDev / sqrt(n);
 	}
 
-	size_t m_maxIdx;
 	std::vector<double> m_dist;
-	double m_mean;
 	double m_stdDev;
 	double m_minp;
-
-	// calculate the minimal range in which p% of the values will fall into
-	void calculateMinimalRange(double p, size_t& low, size_t& high) const;
 };
 
 #endif
