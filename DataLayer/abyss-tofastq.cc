917f9fe6a7bb439ea409eebdc1a0dfb515d94cff
Shaun Jackman <sjackman@bcgsc.ca>
2010-07-08 19:02:24 +0000

    * DataLayer/abyss-tofastq.cc: New program. Convert various file formats to FASTQ format. * DataLayer/Makefile.am: Ditto.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@59060 ad02020f-2a26-4133-947e-1829317c1416

---
+++ DataLayer/abyss-tofastq.cc
@@ -0,0 +1,27 @@
+/** Convert various file formats to FASTQ format.
+ * Written by Shaun Jackman <sjackman@bcgsc.ca>.
+ * Copyright 2010 Genome Sciences Centre
+ */
+#include "FastaReader.h"
+#include "Uncompress.h"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void convert(const char* path)
+{
+	FastaReader in(path,
+			FastaReader::KEEP_N | FastaReader::NO_FOLD_CASE);
+	for (FastaRecord fasta; in >> fasta;)
+		cout << fasta;
+}
+
+int main(int argc, const char* argv[])
+{
+	if (argc <= 1)
+		convert("-");
+	else
+		for_each(argv + 1, argv + argc, convert);
+	return 0;
+}
