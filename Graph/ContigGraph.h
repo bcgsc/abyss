9345fce510fdbb6a6283dfd31577010b693a5d48
Shaun Jackman <sjackman@bcgsc.ca>
2010-07-16 17:00:37 +0000

    * Graph/ContigGraph.h (ContigGraph): Inherit from DirectedGraph.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@59313 ad02020f-2a26-4133-947e-1829317c1416

--- Graph/ContigGraph.h
+++ Graph/ContigGraph.h
@@ -6,10 +6,24 @@
 
 struct NoContigData { };
 
-typedef DirectedGraph<NoContigData> ContigGraph;
+class ContigGraph;
 
 std::istream& operator>>(std::istream& in, ContigGraph& o);
 
 void readContigGraph(ContigGraph& graph, const std::string& path);
+
+class ContigGraph : public DirectedGraph<NoContigData> {
+	typedef DirectedGraph<NoContigData> DG;
+
+  public:
+	/** Construct an empty contig graph. */
+	ContigGraph() { }
+
+	/** Construct a contig graph with n vertices. */
+	ContigGraph(vertices_size_type n) : DG(n) { }
+
+  private:
+	ContigGraph(const ContigGraph&);
+};
 
 #endif
