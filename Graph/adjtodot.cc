4b86ce1c453d99502762a2e298a1ea115555a1fb
Shaun Jackman <sjackman@bcgsc.ca>
2010-08-04 18:02:45 +0000

    * Graph/adjtodot.cc (-k): New option. Report the mean k-mer coverage, otherwise the sum k-mer coverage is reported.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@59886 ad02020f-2a26-4133-947e-1829317c1416

--- Graph/adjtodot.cc
+++ Graph/adjtodot.cc
@@ -4,9 +4,44 @@
  */
 #include "ContigGraph.h"
 #include <fstream>
+#include <getopt.h>
 #include <iostream>
 
 using namespace std;
+
+#define PROGRAM "adjtodot"
+
+static const char VERSION_MESSAGE[] =
+PROGRAM " (" PACKAGE_NAME ") " VERSION "\n"
+"Written by Shaun Jackman.\n"
+"\n"
+"Copyright 2010 Canada's Michael Smith Genome Science Centre\n";
+
+static const char USAGE_MESSAGE[] =
+"Usage: " PROGRAM " FILE\n"
+"Convert the specified graph from adj format to dot format.\n"
+"\n"
+"  -k, --kmer=N   report the mean k-mer coverage, otherwise\n"
+"                 the sum k-mer coverage is reported\n"
+"      --help     display this help and exit\n"
+"      --version  output version information and exit\n"
+"\n"
+"Report bugs to <" PACKAGE_BUGREPORT ">.\n";
+
+namespace opt {
+	static int k;
+}
+
+static const char shortopts[] = "k:";
+
+enum { OPT_HELP = 1, OPT_VERSION };
+
+static const struct option longopts[] = {
+	{ "kmer",    required_argument, NULL, 'k' },
+	{ "help",    no_argument,       NULL, OPT_HELP },
+	{ "version", no_argument,       NULL, OPT_VERSION },
+	{ NULL, 0, NULL, 0 }
+};
 
 /** Contig properties. */
 struct Contig {
@@ -19,7 +54,12 @@ struct Contig {
 
 	friend ostream& operator <<(ostream& out, const Contig& o)
 	{
-		return out << "l=" << o.length << " C=" << o.coverage;
+		out << "l=" << o.length;
+		if (opt::k > 0)
+			return out << " c="
+				<< (float)o.coverage / (o.length - opt::k + 1);
+		else
+			return out << " C=" << o.coverage;
 	}
 
 	friend istream& operator >>(istream& in, Contig& o)
@@ -28,10 +68,39 @@ struct Contig {
 	}
 };
 
-int main(int argc, const char** argv)
+int main(int argc, char** argv)
 {
-	assert(argc <= 2);
-	string path = argc > 1 ? argv[1] : "-";
+	bool die = false;
+	for (int c; (c = getopt_long(argc, argv,
+					shortopts, longopts, NULL)) != -1;) {
+		istringstream arg(optarg != NULL ? optarg : "");
+		switch (c) {
+		  case '?': die = true; break;
+		  case 'k': arg >> opt::k; break;
+		  case OPT_HELP:
+			cout << USAGE_MESSAGE;
+			exit(EXIT_SUCCESS);
+		  case OPT_VERSION:
+			cout << VERSION_MESSAGE;
+			exit(EXIT_SUCCESS);
+		}
+	}
+
+	if (argc - optind < 0) {
+		cerr << PROGRAM ": missing arguments\n";
+		die = true;
+	} else if (argc - optind > 1) {
+		cerr << PROGRAM ": too many arguments\n";
+		die = true;
+	}
+
+	if (die) {
+		cerr << "Try `" << PROGRAM
+			<< " --help' for more information.\n";
+		exit(EXIT_FAILURE);
+	}
+
+	string path = optind < argc ? argv[optind] : "-";
 
 	ifstream fin(path.c_str());
 	if (path != "-")
@@ -42,8 +111,11 @@ int main(int argc, const char** argv)
 	in >> g;
 	assert(in.eof());
 
-	cout << "digraph \"" << path << "\" {\n"
-		<< static_cast<const DirectedGraph<Contig>&>(g)
+	cout << "digraph \"" << path << "\" {\n";
+	if (opt::k > 0)
+		cout << "k=" << opt::k << "\n"
+			"edge[d=" << -(opt::k-1) << "]\n";
+	cout << static_cast<const DirectedGraph<Contig>&>(g)
 		<< "}\n";
 	assert(cout.good());
 	return 0;
