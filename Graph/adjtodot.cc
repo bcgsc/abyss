182996b78aebcf3e261c7b8c81e6ea3f7a60b622
Shaun Jackman <sjackman@bcgsc.ca>
2010-08-30 22:27:31 +0000

    * Graph/adjtodot.cc (opt::verbose): New option.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@60814 ad02020f-2a26-4133-947e-1829317c1416

--- Graph/adjtodot.cc
+++ Graph/adjtodot.cc
@@ -24,6 +24,7 @@ static const char USAGE_MESSAGE[] =
 "\n"
 "  -k, --kmer=N   report the mean k-mer coverage, otherwise\n"
 "                 the sum k-mer coverage is reported\n"
+"  -v, --verbose  display verbose output\n"
 "      --help     display this help and exit\n"
 "      --version  output version information and exit\n"
 "\n"
@@ -31,14 +32,16 @@ static const char USAGE_MESSAGE[] =
 
 namespace opt {
 	static int k;
+	static int verbose;
 }
 
-static const char shortopts[] = "k:";
+static const char shortopts[] = "k:v";
 
 enum { OPT_HELP = 1, OPT_VERSION };
 
 static const struct option longopts[] = {
 	{ "kmer",    required_argument, NULL, 'k' },
+	{ "verbose", no_argument,       NULL, 'v' },
 	{ "help",    no_argument,       NULL, OPT_HELP },
 	{ "version", no_argument,       NULL, OPT_VERSION },
 	{ NULL, 0, NULL, 0 }
@@ -78,6 +81,7 @@ int main(int argc, char** argv)
 		switch (c) {
 		  case '?': die = true; break;
 		  case 'k': arg >> opt::k; break;
+		  case 'v': opt::verbose++; break;
 		  case OPT_HELP:
 			cout << USAGE_MESSAGE;
 			exit(EXIT_SUCCESS);
@@ -112,6 +116,15 @@ int main(int argc, char** argv)
 	Graph g;
 	in >> g;
 	assert(in.eof());
+
+	if (opt::verbose > 0) {
+		unsigned v = num_vertices(g);
+		unsigned e = num_edges(g);
+		cerr << "V=" << v
+			<< " E=" << e
+			<< " E/V=" << (float)e / v
+			<< endl;
+	}
 
 	cout << "digraph \"" << path << "\" {\n";
 	if (opt::k > 0)
