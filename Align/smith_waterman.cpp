03be77ae19460141de24c79ea23e1719536e279d
Shaun Jackman <sjackman@bcgsc.ca>
2010-11-23 23:20:25 +0000

    * Align/smith_waterman.cpp: Remove dead code, mostly pertaining to gap penalties and amino acid sequences.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@62954 ad02020f-2a26-4133-947e-1829317c1416

--- Align/smith_waterman.cpp
+++ Align/smith_waterman.cpp
@@ -52,11 +52,6 @@ T find_array_max(T array[],int length, int& ind);
 template <class T>
 void AssignScores_std(T* temp, T H_im1_jm1, T H_im1_j, T H_i_jm1, const char seq_a_im1, const char seq_b_jm1);
 
-template <class T>
-void AssignScores_scorematrix(T* temp, T H_im1_jm1, T H_im1_j, T H_i_jm1, char seq_a_im1, char seq_b_jm1,
-	bool& opengap_im1_jm1, bool& opengap_im1_j, bool& opengap_i_jm1, bool* temp_opengap);
-
-
 //index comparison functor
 template<class T>
 struct index_cmp {
@@ -178,7 +173,6 @@ unsigned Backtrack(const int i_max, const int j_max, int* I_i, int* I_j, const i
 
 //This is the Smith-Waterman algirthm (a variation of Needleman-Wunsch algorithm), finds one optimal local alignment
 //Modified to find overlap of seq_a and seq_b (alignment that is pinned at the end of seq_a and beginning of seq_b)
-//mismatch or gap positions are filled with 'N's in match_align string
 void alignOverlap(const string& seq_a, const string& seq_b, unsigned seq_a_start_pos,
 	vector<overlap_align>& overlaps, bool multi_align, bool verbose)
 {
@@ -194,56 +188,39 @@ void alignOverlap(const string& seq_a, const string& seq_b, unsigned seq_a_start
 	I_i = new int*[N_a+1];
 	I_j = new int*[N_a+1];
 
-	//bool **opengap; //use this to keep track whether a gap is opengap
-	//opengap = new bool*[N_a+1];
-
 	for(i=0;i<=N_a;i++){
 		H[i] = new double[N_b+1];
 		I_i[i] = new int[N_b+1];
 		I_j[i] = new int[N_b+1];
-		//opengap[i] = new bool[N_b+1];
 		H[i][0]=0; //only need to initialize first row and first column
-		//opengap[i][0] = false;
 	}
 
-	for (j=0;j<=N_b;j++){
+	for (j=0;j<=N_b;j++)
 		H[0][j]=0; //initialize first column
-		//opengap[0][j] = false;
-	}
 
 	double temp[4];
-	//bool temp_opengap[3];
 	int ind;
-
-	// here comes the actual algorithm
 	for(i=1;i<=N_a;i++){
 		for(j=1;j<=N_b;j++){
 			AssignScores_std<double>(temp, H[i-1][j-1], H[i-1][j], H[i][j-1], seq_a[i-1], seq_b[j-1]);
-			/*AssignScores_scorematrix<double>(temp, H[i-1][j-1], H[i-1][j], H[i][j-1], seq_a[i-1], seq_b[j-1],
-				opengap[i-1][j-1], opengap[i-1][j], opengap[i][j-1], temp_opengap);*/
-
 			temp[3] = 0.;
 			H[i][j] = find_array_max<double>(temp,4, ind);
 			switch(ind){
 			case 0: // score in (i,j) stems from a match/mismatch
 				I_i[i][j] = i-1;
 				I_j[i][j] = j-1;
-				//opengap[i][j] = temp_opengap[0];
 				break;
 			case 1: // score in (i,j) stems from a deletion in sequence A
 				I_i[i][j] = i-1;
 				I_j[i][j] = j;
-				//opengap[i][j] = temp_opengap[1];
 				break;
 			case 2: // score in (i,j) stems from a deletion in sequence B
 				I_i[i][j] = i;
 				I_j[i][j] = j-1;
-				//opengap[i][j] = temp_opengap[2];
 				break;
 			case 3: // (i,j) is the beginning of a subsequence
 				I_i[i][j] = i;
 				I_j[i][j] = j;
-				//opengap[i][j] = false;
 				break;
 			}
 		}
@@ -306,14 +283,11 @@ void alignOverlap(const string& seq_a, const string& seq_b, unsigned seq_a_start
 		delete [] H[i];
 		delete [] I_i[i];
 		delete [] I_j[i];
-		//delete [] opengap[i];
 	}
 	delete [] H;
 	delete [] I_i;
 	delete [] I_j;
-	//delete [] opengap;
-
-} // END of smith_waterman
+}
 
 /** Return the score of the alignment of a and b. */
 static int sim_score(const char a, const char b)
@@ -323,52 +297,6 @@ static int sim_score(const char a, const char b)
 		: !isMatch(a, b, consensus) ? -1 // mismatch penalty
 		: 1; // match score
 }
-
-/*
-// find the index of 'a' in the amino-acid substitution matrix, a is (a-z)|(A-Z)|-|*
-int score_index(char a)
-{
-  if (a <= 'Z' && a >= 'A')
-	  return a - 'A';
-  else
-	  if (a <= 'z' && a >= 'a')
-		return a - 'a';
-	  else
-		if (a == '-')
-			return 26;
-		else
-		{
-			if (a == '*')
-				return 27;
-			else
-			{
-				cout << "cannot find score when running SW local alignment: " << "char:" << a << endl;
-				exit(-1);
-			}
-		}
-}
-
-// find the score of (a,b) using amino acid substitution matrix, also update opengap status
-int similarity_score(char a,char b, bool opengap, bool& opengap_cur)
-{
-  //ALIGN_SCORE_MATRIX[28][28] stores the align scores, 26 Uppercase letters (a.a.), '-', '*'
-  int ai = score_index(a);
-  int bi = score_index(b);
-  if (ai != 26 && bi != 26)
-  {
-	  opengap_cur = false;
-	  return ALIGN_SCORE_MATRIX[ai][bi];
-  }
-  else
-  {
-	  opengap_cur = true;
-	  if (opengap) //previous is gap
-		  return ALIGN_SCORE_MATRIX[26][1]; //gap extension
-	  else
-		  return ALIGN_SCORE_MATRIX[26][0]; //gap opening
-  }
-}
-*/
 
 // Find the max value in array[], use 'ind' to hold the index of max
 template <class T>
@@ -387,7 +315,7 @@ T find_array_max(T array[],int length, int& ind)
   return max;                    // return highest value in array
 }
 
-//compute alignment score based on simple score matrix: match is 1, mismatch/deletion/insertion is -1
+/** Return the alignment score. */
 template <class T>
 void AssignScores_std(T* temp, T H_im1_jm1, T H_im1_j, T H_i_jm1, const char seq_a_im1, const char seq_b_jm1)
 {
@@ -395,19 +323,3 @@ void AssignScores_std(T* temp, T H_im1_jm1, T H_im1_j, T H_i_jm1, const char seq
   temp[1] = H_im1_j+sim_score(seq_b_jm1, '-');
   temp[2] = H_i_jm1+sim_score(seq_a_im1, '-');
 }
-
-/*
-//compute alignment score based on a substitution score matrix
-template <class T>
-void AssignScores_scorematrix(T* temp, T H_im1_jm1, T H_im1_j, T H_i_jm1, char seq_a_im1, char seq_b_jm1,
-	bool& opengap_im1_jm1, bool& opengap_im1_j, bool& opengap_i_jm1, bool* temp_opengap)
-{
-	temp[0] = H_im1_jm1+similarity_score(seq_a_im1,seq_b_jm1, opengap_im1_jm1, temp_opengap[0]);
-	temp[1] = H_im1_j+similarity_score(seq_a_im1, '-', opengap_im1_j, temp_opengap[1]);
-	temp[2] = H_i_jm1+similarity_score(seq_b_jm1, '-', opengap_i_jm1, temp_opengap[2]);
-	//temp[3] = 0.;
-}
-*/
-
-//////////////////////////////////////////////////////////////////////////////
-
