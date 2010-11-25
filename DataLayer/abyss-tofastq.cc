54e442620a814be8a71dd35e7744e0d1888ed268
Shaun Jackman <sjackman@bcgsc.ca>
2010-11-25 01:06:51 +0000

    * DataLayer/abyss-tofastq.cc (main): Parse the command line options relevant to DataLayer.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@62995 ad02020f-2a26-4133-947e-1829317c1416

--- DataLayer/abyss-tofastq.cc
+++ DataLayer/abyss-tofastq.cc
@@ -2,14 +2,71 @@
  * Written by Shaun Jackman <sjackman@bcgsc.ca>.
  * Copyright 2010 Genome Sciences Centre
  */
+#include "config.h"
 #include "DataLayer/Options.h"
 #include "FastaReader.h"
 #include "Uncompress.h"
 #include <algorithm>
 #include <cassert>
+#include <cstdlib>
+#include <getopt.h>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
+
+#define PROGRAM "abyss-tofastq"
+
+static const char VERSION_MESSAGE[] =
+PROGRAM " (" PACKAGE_NAME ") " VERSION "\n"
+"Written by Shaun Jackman.\n"
+"\n"
+"Copyright 2010 Canada's Michael Smith Genome Science Centre\n";
+
+static const char USAGE_MESSAGE[] =
+"Usage: " PROGRAM " [OPTION]... [FILE]...\n"
+"Convert to FASTQ format. The input format may be FASTA, FASTQ,\n"
+"qseq, export, SAM or BAM format and compressed with gz, bz2 or xz\n"
+"and may be tarred.\n"
+"\n"
+"      --chastity          discard unchaste reads [default]\n"
+"                          for qseq, export and SAM files only\n"
+"      --no-chastity       do not discard unchaste reads\n"
+"      --trim-masked       trim masked bases from the ends of reads\n"
+"      --no-trim-masked    do not trim masked bases from the ends\n"
+"                          of reads [default]\n"
+"  -q, --trim-quality=N    trim bases from the ends of reads whose\n"
+"                          quality is less than the threshold\n"
+"      --standard-quality  zero quality is `!' (33)\n"
+"                          default for FASTQ and SAM files\n"
+"      --illumina-quality  zero quality is `@' (64)\n"
+"                          default for qseq and export files\n"
+"  -v, --verbose           display verbose output\n"
+"      --help              display this help and exit\n"
+"      --version           output version information and exit\n"
+"\n"
+"Report bugs to <" PACKAGE_BUGREPORT ">.\n";
+
+namespace opt {
+	static int verbose;
+}
+
+static const char shortopts[] = "q:v";
+
+enum { OPT_HELP = 1, OPT_VERSION };
+
+static const struct option longopts[] = {
+	{ "chastity",         no_argument, &opt::chastityFilter, 1 },
+	{ "no-chastity",      no_argument, &opt::chastityFilter, 0 },
+	{ "trim-masked",      no_argument, &opt::trimMasked, 1 },
+	{ "no-trim-masked",   no_argument, &opt::trimMasked, 0 },
+	{ "trim-quality",     required_argument, NULL, 'q' },
+	{ "standard-quality", no_argument, &opt::qualityOffset, 33 },
+	{ "illumina-quality", no_argument, &opt::qualityOffset, 64 },
+	{ "help",             no_argument, NULL, OPT_HELP },
+	{ "version",          no_argument, NULL, OPT_VERSION },
+	{ NULL, 0, NULL, 0 }
+};
 
 template <class Record>
 static void convert(const char* path)
@@ -21,17 +78,53 @@ static void convert(const char* path)
 	assert(in.eof());
 }
 
-int main(int argc, const char* argv[])
+int main(int argc, char** argv)
 {
 	opt::trimMasked = false;
+
+	bool die = false;
+	for (int c; (c = getopt_long(argc, argv,
+					shortopts, longopts, NULL)) != -1;) {
+		istringstream arg;
+		if (optarg != NULL)
+			arg.str(optarg);
+		switch (c) {
+			case '?':
+				die = true;
+				break;
+			case 'q':
+				arg >> opt::qualityThreshold;
+				break;
+			case 'v':
+				opt::verbose++;
+				break;
+			case OPT_HELP:
+				cout << USAGE_MESSAGE;
+				exit(EXIT_SUCCESS);
+			case OPT_VERSION:
+				cout << VERSION_MESSAGE;
+				exit(EXIT_SUCCESS);
+		}
+		if (optarg != NULL && !arg.eof()) {
+			cerr << PROGRAM ": invalid option: `-"
+				<< (char)c << optarg << "'\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (die) {
+		cerr << "Try `" << PROGRAM
+			<< " --help' for more information.\n";
+		exit(EXIT_FAILURE);
+	}
+
 	typedef void (*F)(const char*);
 	F convertFasta = convert<FastaRecord>;
 	F convertFastq = convert<FastqRecord>;
 	F f = string(argv[0]).find("tofasta") != string::npos
 		? convertFasta : convertFastq;
-	if (argc <= 1)
-		f("-");
+	if (optind < argc)
+		for_each(argv + optind, argv + argc, f);
 	else
-		for_each(argv + 1, argv + argc, f);
+		f("-");
 	return 0;
 }
