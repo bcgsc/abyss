4cb36787ffc1b36f72c39ba131b0a8a9396aaef4
Shaun Jackman <sjackman@bcgsc.ca>
2010-11-29 22:49:16 +0000

    * Graph/GraphUtil.h (printGraphStats): Print the proportion of vertices with outdegree 0, 1, 2-4 and 5+.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@63113 ad02020f-2a26-4133-947e-1829317c1416

--- Graph/GraphUtil.h
+++ Graph/GraphUtil.h
@@ -2,6 +2,7 @@
 #define GRAPHUTIL_H 1
 
 #include "Histogram.h"
+#include <iomanip>
 #include <ostream>
 
 template <typename Graph>
@@ -20,9 +21,18 @@ std::ostream& printGraphStats(std::ostream& out, const Graph& g)
 	std::pair<vertex_iterator, vertex_iterator> vit = vertices(g);
 	for (vertex_iterator u = vit.first; u != vit.second; ++u)
 		h.insert(out_degree(*u, g));
+	unsigned n = h.size();
+	unsigned n0 = h.count(0), n1 = h.count(1), n234 = h.count(2, 4);
+	unsigned n5 = n - (n0 + n1 + n234);
+	using std::setprecision;
 	return out <<
 		"Degree: " << h.barplot(h.maximum() + 1) << "\n"
-		"        01234\n";
+		"        01234\n"
+		"0: " << setprecision(2) << (float)100 * n0 / n << "% "
+		"1: " << setprecision(2) << (float)100 * n1 / n << "% "
+		"2-4: " << setprecision(2) << (float)100 * n234 / n << "% "
+		"5+: " << setprecision(2) << (float)100 * n5 / n << "% "
+		"max: " << h.maximum() << std::endl;
 }
 
 #endif
