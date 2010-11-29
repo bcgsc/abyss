8e015395246e8d970ca1d835f1993284748bfa82
Shaun Jackman <sjackman@bcgsc.ca>
2010-11-29 23:46:39 +0000

    * AdjList/AdjList.cpp: Find overlaps of fewer than k-1 bp by using a suffix array to index blunt contigs. (-m, --min-overlap): New parameter. Default is 25. (addOverlapsSA): New function. (main): Use it.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@63119 ad02020f-2a26-4133-947e-1829317c1416

--- AdjList/AdjList.cpp
+++ AdjList/AdjList.cpp
@@ -10,6 +10,8 @@
 #include "HashMap.h"
 #include "Iterator.h"
 #include "Kmer.h"
+#include "StringUtil.h"
+#include "SuffixArray.h"
 #include "Uncompress.h"
 #include <cassert>
 #include <cctype>
@@ -17,6 +19,7 @@
 #include <getopt.h>
 #include <iostream>
 #include <iterator>
+#include <set>
 #include <sstream>
 #include <vector>
 
@@ -32,11 +35,14 @@ PROGRAM " (" PACKAGE_NAME ") " VERSION "\n"
 
 static const char USAGE_MESSAGE[] =
 "Usage: " PROGRAM " [OPTION]... [FILE]...\n"
-"Find overlaps of exactly k-1 bases. Contigs may be read from\n"
-"FILE(s) or standard input. Output is written to standard output.\n"
+"Find overlaps of [m,k) bases. Contigs may be read from FILE(s)\n"
+"or standard input. Output is written to standard output.\n"
+"Overlaps of exactly k-1 bases are found using a hash table.\n"
+"Overlaps of fewer than k-1 bases are found using a suffix array.\n"
 "\n"
-"  -k, --kmer=KMER_SIZE  k-mer size\n"
-"      --adj             output the results in adj format [DEFAULT]\n"
+"  -k, --kmer=K          find overlaps of up to K-1 bases\n"
+"  -m, --min-overlap=M   require a minimum overlap of M bases [25]\n"
+"      --adj             output the results in adj format [default]\n"
 "      --dot             output the results in dot format\n"
 "      --sam             output the results in SAM format\n"
 "  -v, --verbose         display verbose output\n"
@@ -48,14 +54,18 @@ static const char USAGE_MESSAGE[] =
 namespace opt {
 	unsigned k; // used by GraphIO
 	int format; // used by GraphIO
+
+	/** The minimum required amount of overlap. */
+	static unsigned minOverlap;
 }
 
-static const char shortopts[] = "k:v";
+static const char shortopts[] = "k:m:v";
 
 enum { OPT_HELP = 1, OPT_VERSION };
 
 static const struct option longopts[] = {
 	{ "kmer",    required_argument, NULL, 'k' },
+	{ "min-overlap", required_argument, NULL, 'm' },
 	{ "adj",     no_argument,       &opt::format, ADJ },
 	{ "dot",     no_argument,       &opt::format, DOT },
 	{ "sam",     no_argument,       &opt::format, SAM },
@@ -66,16 +76,10 @@ static const struct option longopts[] = {
 };
 
 /** A contig adjacency graph. */
-typedef DirectedGraph<ContigProperties> DG;
+typedef DirectedGraph<ContigProperties, Distance> DG;
 typedef ContigGraph<DG> Graph;
 
-/** Return the distance between two vertices. */
-static inline int get(edge_distance_t, const Graph&,
-		graph_traits<Graph>::edge_descriptor)
-{
-	return -opt::k + 1;
-}
-
+/** Parse and return the coverage from the specified FASTA comment. */
 static unsigned getCoverage(const string& comment)
 {
 	istringstream ss(comment);
@@ -84,14 +88,63 @@ static unsigned getCoverage(const string& comment)
 	return coverage;
 }
 
-/** An index of suffixes. */
-typedef hash_map<Kmer, vector<ContigNode>, hashKmer> KmerMap;
+/** Add the overlaps of vseq to the graph. */
+static void addOverlapsSA(Graph& g, const SuffixArray& sa,
+		ContigNode v, const string& vseq)
+{
+	assert(!vseq.empty());
+	set<ContigNode> seen;
+	typedef SuffixArray::const_iterator It;
+	for (string q(vseq, 0, vseq.size() - 1);
+			q.size() >= opt::minOverlap; chop(q)) {
+		pair<It, It> range = sa.equal_range(q);
+		for (It it = range.first; it != range.second; ++it) {
+			ContigNode u(it->second);
+			if (seen.insert(u).second) {
+				// Add the longest overlap between two vertices.
+				unsigned overlap = it->first.size();
+				add_edge<DG>(u, v, -overlap, g);
+			}
+		}
+	}
+}
+
+/** Add overlaps of fewer than k-1 bp to the graph. */
+static void addOverlapsSA(Graph& g, const vector<Kmer>& prefixes)
+{
+	// Construct a suffix array of the blunt contigs.
+	typedef pair<string, ContigNode> Suffix;
+	typedef vector<Suffix> Suffixes;
+	Suffixes suffixes;
+	SuffixArray sa(opt::minOverlap);
+
+	typedef graph_traits<Graph>::vertex_iterator Vit;
+	pair<Vit, Vit> vertices = g.vertices();
+	for (Vit it = vertices.first; it != vertices.second; ++it) {
+		ContigNode u(*it);
+		if (out_degree(u, g) > 0)
+			continue;
+		string suffix(reverseComplement(
+					prefixes[(~u).index()]).str());
+		suffixes.push_back(Suffix(suffix, u));
+		sa.insert(suffixes.back());
+	}
+	sa.construct();
+
+	for (Suffixes::const_iterator it = suffixes.begin();
+			it != suffixes.end(); ++it)
+		addOverlapsSA(g, sa,
+				~it->second, reverseComplement(it->first));
+}
+
+/** An index of suffixes of k-1 bp. */
+typedef hash_map<Kmer, vector<ContigNode>, hashKmer> SuffixMap;
 
 /** Read contigs. Add contig properties to the graph. Add prefixes to
  * the collection and add suffixes to their index.
  */
 static void readContigs(const string& path,
-		Graph& g, vector<Kmer>& prefixes, KmerMap& suffixes)
+		Graph& g, vector<Kmer>& prefixes, SuffixMap& suffixMap)
 {
 	if (opt::verbose > 0)
 		cerr << "Reading `" << path << "'...\n";
@@ -122,8 +175,8 @@ static void readContigs(const string& path,
 		ContigID::insert(rec.id);
 		ContigProperties vp(seq.length(), getCoverage(rec.comment));
 		ContigNode u = add_vertex(vp, g);
-		suffixes[suffix].push_back(u);
-		suffixes[reverseComplement(prefix)].push_back(~u);
+		suffixMap[suffix].push_back(u);
+		suffixMap[reverseComplement(prefix)].push_back(~u);
 	}
 	assert(in.eof());
 }
@@ -146,6 +199,7 @@ int main(int argc, char** argv)
 		switch (c) {
 			case '?': die = true; break;
 			case 'k': arg >> opt::k; break;
+			case 'm': arg >> opt::minOverlap; break;
 			case 'v': opt::verbose++; break;
 			case OPT_HELP:
 				cout << USAGE_MESSAGE;
@@ -161,6 +215,15 @@ int main(int argc, char** argv)
 		die = true;
 	}
 
+	if (opt::minOverlap == 0)
+		opt::minOverlap = min(25U, opt::k);
+
+	if (opt::minOverlap > opt::k) {
+		cerr << PROGRAM ": " << "minimum overlap -m "
+		   "must be no more than k-mer size -k\n";
+		die = true;
+	}
+
 	if (die) {
 		cerr << "Try `" << PROGRAM
 			<< " --help' for more information.\n";
@@ -171,26 +234,38 @@ int main(int argc, char** argv)
 	Kmer::setLength(opt::k - 1);
 	Graph g;
 	vector<Kmer> prefixes;
-	KmerMap suffixes(prefixes.size());
+	SuffixMap suffixMap(prefixes.size());
 	if (optind < argc) {
 		for (; optind < argc; optind++)
-			readContigs(argv[optind], g, prefixes, suffixes);
+			readContigs(argv[optind], g, prefixes, suffixMap);
 	} else
-		readContigs("-", g, prefixes, suffixes);
+		readContigs("-", g, prefixes, suffixMap);
 	ContigID::lock();
 
-	// Add the edges.
+	// Add the overlap edges of exactly k-1 bp.
+	if (opt::verbose > 0)
+		cerr << "Finding overlaps of exactly k-1 bp...\n";
 	for (vector<Kmer>::const_iterator it = prefixes.begin();
 			it != prefixes.end(); ++it) {
 		ContigNode v(it - prefixes.begin());
-		const KmerMap::mapped_type& edges = suffixes[*it];
-		for (KmerMap::mapped_type::const_iterator
+		const SuffixMap::mapped_type& edges = suffixMap[*it];
+		for (SuffixMap::mapped_type::const_iterator
 				itu = edges.begin(); itu != edges.end(); ++itu)
-			add_edge<DG>(~v, ~*itu, g);
+			add_edge<DG>(~v, ~*itu, -(int)opt::k + 1, g);
 	}
+	SuffixMap().swap(suffixMap);
 
 	if (opt::verbose > 0)
 		printGraphStats(cerr, g);
+
+	if (opt::minOverlap < opt::k) {
+		// Add the overlap edges of fewer than k-1 bp.
+		if (opt::verbose > 0)
+			cerr << "Finding overlaps of fewer than k-1 bp...\n";
+		addOverlapsSA(g, prefixes);
+		if (opt::verbose > 0)
+			printGraphStats(cerr, g);
+	}
 
 	// Output the graph.
 	write_graph(cout, g, PROGRAM, commandLine);
