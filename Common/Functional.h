a5168c7654637ac914db0f952142d7b41e7a9616
Shaun Jackman <sjackman@bcgsc.ca>
2010-12-09 23:14:30 +0000

    * Common/Functional.h: New file. * Common/Makefile.am (SOURCES): Ditto.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@63361 ad02020f-2a26-4133-947e-1829317c1416

---
+++ Common/Functional.h
@@ -0,0 +1,30 @@
+#ifndef FUNCTIONAL_H
+#define FUNCTIONAL_H 1
+
+#include <functional>
+
+/** Compose two unary functions. */
+template <typename F1, typename F2>
+struct unary_compose : std::unary_function <
+        typename F2::argument_type, typename F1::result_type>
+{
+    unary_compose(F1 f1, F2 f2) : f1(f1), f2(f2) {}
+
+    typename F1::result_type operator()(typename F2::argument_type x)
+    {
+		return f1(f2(x));
+	}
+
+private:
+    F1 f1;
+    F2 f2;
+};
+
+/** Compose two unary functions. */
+template <typename F1, typename F2>
+unary_compose<F1, F2> compose1(F1 f1, F2 f2)
+{
+	return unary_compose<F1, F2>(f1, f2);
+}
+
+#endif
