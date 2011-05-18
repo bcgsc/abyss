c6f520928d33a98e1246220ab25eecb2196edf5e
Shaun Jackman <sjackman@bcgsc.ca>
2011-05-18 23:11:43 +0000

    * Scaffold/scaffold.cc (--help): --min-gap takes an argument
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@68244 ad02020f-2a26-4133-947e-1829317c1416

--- Scaffold/scaffold.cc
+++ Scaffold/scaffold.cc
@@ -38,7 +38,7 @@ static const char USAGE_MESSAGE[] =
 "  -n, --npairs=N        minimum number of pairs [0]\n"
 "  -s, --seed-length=N   minimum contig length [0]\n"
 "  -k, --kmer=N          length of a k-mer\n"
-"      --min-gap         minimum scaffold gap length to output\n"
+"      --min-gap=N       minimum scaffold gap length to output\n"
 "  -o, --out=FILE        write the paths to FILE\n"
 "  -g, --graph=FILE      write the graph to FILE\n"
 "  -v, --verbose         display verbose output\n"
