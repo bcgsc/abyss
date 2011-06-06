c6fc19f13b6490a23b412e974ac2d3887e6832ca
Shaun Jackman <sjackman@bcgsc.ca>
2011-06-06 19:13:28 +0000

    * Graph/GraphUtil.h (printGraphStats): Ignore vertices that have been marked as removed. (num_vertices_removed): New function.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@68855 ad02020f-2a26-4133-947e-1829317c1416

--- Graph/GraphUtil.h
+++ Graph/GraphUtil.h
@@ -5,13 +5,30 @@
 #include <iomanip>
 #include <ostream>
 
+/** Return the number of vertices that have been marked as removed. */
+template <typename Graph>
+typename graph_traits<Graph>::vertices_size_type
+num_vertices_removed(const Graph& g)
+{
+	typedef graph_traits<Graph> GTraits;
+	typedef typename GTraits::vertices_size_type vertices_size_type;
+	typedef typename GTraits::vertex_iterator vertex_iterator;
+	vertices_size_type n = 0;
+	std::pair<vertex_iterator, vertex_iterator> vit = vertices(g);
+	for (vertex_iterator u = vit.first; u != vit.second; ++u)
+		if (get(vertex_removed, g, *u))
+			n++;
+	return n;
+}
+
+/** Print statistics of the number of vertices and edges. */
 template <typename Graph>
 std::ostream& printGraphStats(std::ostream& out, const Graph& g)
 {
 	typedef typename graph_traits<Graph>::vertex_iterator
 		vertex_iterator;
 
-	unsigned v = num_vertices(g);
+	unsigned v = num_vertices(g) - num_vertices_removed(g);
 	unsigned e = num_edges(g);
 	out << "V=" << v << " E=" << e
 		<< " E/V=" << (float)e / v << std::endl;
@@ -19,8 +36,11 @@ std::ostream& printGraphStats(std::ostream& out, const Graph& g)
 	// Print a histogram of the degree.
 	Histogram h;
 	std::pair<vertex_iterator, vertex_iterator> vit = vertices(g);
-	for (vertex_iterator u = vit.first; u != vit.second; ++u)
+	for (vertex_iterator u = vit.first; u != vit.second; ++u) {
+		if (get(vertex_removed, g, *u))
+			continue;
 		h.insert(out_degree(*u, g));
+	}
 	unsigned n = h.size();
 	unsigned n0 = h.count(0), n1 = h.count(1), n234 = h.count(2, 4);
 	unsigned n5 = n - (n0 + n1 + n234);
