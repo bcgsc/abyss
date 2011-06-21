e1625f954ebfe224fcf813521267f0c3ebe4d1b0
Shaun Jackman <sjackman@bcgsc.ca>
2011-06-21 23:47:26 +0000

    * DistanceEst/DistanceEst.cpp (main) <opt::dot>: Do not output the vertex properties.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@69471 ad02020f-2a26-4133-947e-1829317c1416

--- DistanceEst/DistanceEst.cpp
+++ DistanceEst/DistanceEst.cpp
@@ -377,19 +377,6 @@ int main(int argc, char** argv)
 			<< h.barplot() << endl;
 	PDF empiricalPDF(h);
 
-	if (opt::dot) {
-		// Output the vertex properties.
-		for (vector<unsigned>::const_iterator it = contigLens.begin();
-				it != contigLens.end(); ++it) {
-			ContigID id(it - contigLens.begin());
-			unsigned length = contigLens[id];
-			out << '"' << ContigNode(id, false)
-				<< "\" [l=" << length << "]\n"
-				<< '"' << ContigNode(id, true)
-				<< "\" [l=" << length << "]\n";
-		}
-	}
-
 	// Check that the input is sorted.
 	vector<bool> seen(contigLens.size());
 
