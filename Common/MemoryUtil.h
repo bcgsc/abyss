5903cb78ad4a91a37075422f217e862913ff6789
Shaun Jackman <sjackman@bcgsc.ca>
2011-08-11 19:26:38 +0000

    * Common/MemoryUtil.h (getMemoryUsage) [!HAVE_GETPAGESIZE]: Return -1. * configure.ac (AC_CHECK_FUNCS): Add getpagesize.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@71362 ad02020f-2a26-4133-947e-1829317c1416

--- Common/MemoryUtil.h
+++ Common/MemoryUtil.h
@@ -1,6 +1,7 @@
 #ifndef MEMORYUTIL_H
 #define MEMORYUTIL_H 1
 
+#include "config.h"
 #include <fstream>
 #include <unistd.h> // for getpagesize
 
@@ -9,10 +10,14 @@
  */
 static inline ssize_t getMemoryUsage()
 {
+#if HAVE_GETPAGESIZE
 	std::ifstream in("/proc/self/statm");
 	size_t size, resident, share, text, lib, data;
 	return in >> size >> resident >> share >> text >> lib >> data
 		? data * getpagesize() : -1;
+#else
+	return -1;
+#endif
 }
 
 #endif
