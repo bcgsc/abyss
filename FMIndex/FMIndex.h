590bbf196e9c7cc5e69d277f70b8a5244cf84294
Shaun Jackman <sjackman@bcgsc.ca>
2011-09-14 18:16:51 +0000

    * FMIndex/FMIndex.h: White space changes only. Undent the class.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@72197 ad02020f-2a26-4133-947e-1829317c1416

--- FMIndex/FMIndex.h
+++ FMIndex/FMIndex.h
@@ -69,9 +69,7 @@ class FMIndex
 	/** The type of a symbol. */
 	typedef T value_type;
 
-	void read(const char *path, std::vector<T> &s);
-
-	FMIndex() : m_sampleSA(1) { }
+FMIndex() : m_sampleSA(1) { }
 
 /** Return the size of the string not counting the sentinel. */
 size_t size() const { return m_occ.size() - 1; }
@@ -155,169 +153,166 @@ size_t locate(size_t i) const
 	return pos < m_occ.size() ? pos : pos - m_occ.size();
 }
 
-	/** Decompress the index. */
-	template <typename It>
-	void decompress(It out)
-	{
-		// Construct the original string.
-		std::vector<T> s;
-		for (size_t i = 0;;) {
-			assert(i < m_occ.size());
-			T c = m_occ.at(i);
-			if (c == SENTINEL())
-				break;
-			s.push_back(c);
-			i = m_cf[c] + m_occ.rank(c, i);
-			assert(i > 0);
-		}
-
-		// Translate the character set and output the result.
-		for (std::vector<T>::const_reverse_iterator it = s.rbegin();
-				it != s.rend(); ++it) {
-			T c = *it;
-			assert(c < m_alphabet.size());
-			*out++ = m_alphabet[c];
-		}
+/** Decompress the index. */
+template <typename It>
+void decompress(It out)
+{
+	// Construct the original string.
+	std::vector<T> s;
+	for (size_t i = 0;;) {
+		assert(i < m_occ.size());
+		T c = m_occ.at(i);
+		if (c == SENTINEL())
+			break;
+		s.push_back(c);
+		i = m_cf[c] + m_occ.rank(c, i);
+		assert(i > 0);
 	}
 
-	/** Search for an exact match. */
-	template <typename It>
-	std::pair<size_t, size_t> findExact(It first, It last) const
-	{
-		assert(first < last);
-		size_t l = 1, u = m_occ.size();
-		It it;
-		for (it = last - 1; it >= first && l < u; --it) {
-			T c = *it;
-			if (c == SENTINEL())
-				return std::make_pair(0, 0);
-			l = m_cf[c] + m_occ.rank(c, l);
-			u = m_cf[c] + m_occ.rank(c, u);
+	// Translate the character set and output the result.
+	for (std::vector<T>::const_reverse_iterator it = s.rbegin();
+			it != s.rend(); ++it) {
+		T c = *it;
+		assert(c < m_alphabet.size());
+		*out++ = m_alphabet[c];
+	}
+}
+
+/** Search for an exact match. */
+template <typename It>
+std::pair<size_t, size_t> findExact(It first, It last) const
+{
+	assert(first < last);
+	size_t l = 1, u = m_occ.size();
+	It it;
+	for (it = last - 1; it >= first && l < u; --it) {
+		T c = *it;
+		if (c == SENTINEL())
+			return std::make_pair(0, 0);
+		l = m_cf[c] + m_occ.rank(c, l);
+		u = m_cf[c] + m_occ.rank(c, u);
+	}
+	return std::make_pair(l, u);
+}
+
+/** Search for an exact match. */
+template <typename String>
+std::pair<size_t, size_t> findExact(const String& q) const
+{
+	String s(q.size());
+	std::transform(q.begin(), q.end(), s.begin(), Translate(*this));
+	return findExact(s.begin(), s.end());
+}
+
+/** Search for a matching suffix of the query. */
+template <typename It, typename MemoIt>
+FMInterval findSuffix(It first, It last, MemoIt memoIt) const
+{
+	assert(first < last);
+	typedef typename std::iterator_traits<MemoIt>::value_type
+		Interval;
+
+	size_t l = 1, u = m_occ.size();
+	It it;
+	for (it = last - 1; it >= first && l < u; --it) {
+		T c = *it;
+		if (c == SENTINEL())
+			break;
+		size_t l1 = m_cf[c] + m_occ.rank(c, l);
+		size_t u1 = m_cf[c] + m_occ.rank(c, u);
+		if (l1 >= u1)
+			break;
+		l = l1;
+		u = u1;
+
+		Interval fmi = Interval(l, u);
+		if (*memoIt == fmi) {
+			// This vertex of the prefix DAWG has been visited.
+			break;
 		}
-		return std::make_pair(l, u);
+		*memoIt++ = fmi;
 	}
+	return FMInterval(l, u, it - first + 1, last - first);
+}
 
-	/** Search for an exact match. */
-	template <typename String>
-	std::pair<size_t, size_t> findExact(const String& q) const
+/** Search for a matching substring of the query at least k long.
+ * @return the longest match
+ */
+template <typename It>
+FMInterval findSubstring(It first, It last, unsigned k) const
+{
+	assert(first < last);
+	FMInterval best(0, 0, 0, k > 0 ? k - 1 : 0);
+
+	// Record which vertices of the prefix DAWG have been visited.
+	std::vector<std::pair<size_type, size_type> > memo(last - first);
+	std::pair<size_type, size_type>* memoIt = memo.data();
+	for (It it = last; it > first; --it) {
+		if (unsigned(it - first) < best.qspan())
+			return best;
+		FMInterval interval = findSuffix(first, it, memoIt++);
+		if (interval.qspan() > best.qspan())
+			best = interval;
+	}
+	return best;
+}
+
+/** Translate from ASCII to the indexed alphabet. */
+struct Translate {
+	Translate(const FMIndex& fmIndex) : m_fmIndex(fmIndex) { }
+	T operator()(unsigned char c) const
 	{
-		String s(q.size());
-		std::transform(q.begin(), q.end(), s.begin(),
-				Translate(*this));
-		return findExact(s.begin(), s.end());
+		return c < m_fmIndex.m_mapping.size()
+			? m_fmIndex.m_mapping[c] : SENTINEL();
+	}
+  private:
+	const FMIndex& m_fmIndex;
+};
+
+/** Search for a matching substring of the query at least k long.
+ * @return the longest match and the number of matches
+ */
+Match find(const std::string& q, unsigned k) const
+{
+	std::string s = q;
+	std::transform(s.begin(), s.end(), s.begin(), Translate(*this));
+
+	FMInterval interval = findSubstring(s.begin(), s.end(), k);
+	assert(interval.l <= interval.u);
+	size_t count = interval.u - interval.l;
+	if (count == 0)
+		return Match(0, 0, 0, 0);
+	return Match(interval.qstart, interval.qend,
+			locate(interval.l), count);
+}
+
+/** Set the alphabet to [first, last). */
+template <typename It>
+void setAlphabet(It first, It last)
+{
+	std::vector<bool> mask(std::numeric_limits<T>::max());
+	for (It it = first; it < last; ++it) {
+		assert((size_t)*it < mask.size());
+		mask[*it] = true;
 	}
 
-	/** Search for a matching suffix of the query. */
-	template <typename It, typename MemoIt>
-	FMInterval findSuffix(It first, It last, MemoIt memoIt) const
-	{
-		assert(first < last);
-		typedef typename std::iterator_traits<MemoIt>::value_type
-			Interval;
-
-		size_t l = 1, u = m_occ.size();
-		It it;
-		for (it = last - 1; it >= first && l < u; --it) {
-			T c = *it;
-			if (c == SENTINEL())
-				break;
-			size_t l1 = m_cf[c] + m_occ.rank(c, l);
-			size_t u1 = m_cf[c] + m_occ.rank(c, u);
-			if (l1 >= u1)
-				break;
-			l = l1;
-			u = u1;
-
-			Interval fmi = Interval(l, u);
-			if (*memoIt == fmi) {
-				// This vertex of the prefix DAWG has been visited.
-				break;
-			}
-			*memoIt++ = fmi;
-		}
-		return FMInterval(l, u, it - first + 1, last - first);
+	m_alphabet.clear();
+	m_mapping.clear();
+	for (unsigned c = 0; c < mask.size(); ++c) {
+		if (!mask[c])
+			continue;
+		m_mapping.resize(c + 1, std::numeric_limits<T>::max());
+		m_mapping[c] = m_alphabet.size();
+		m_alphabet.push_back(c);
 	}
+	assert(!m_alphabet.empty());
+}
 
-	/** Search for a matching substring of the query at least k long.
-	 * @return the longest match
-	 */
-	template <typename It>
-	FMInterval findSubstring(It first, It last, unsigned k) const
-	{
-		assert(first < last);
-		FMInterval best(0, 0, 0, k > 0 ? k - 1 : 0);
-
-		// Record which vertices of the prefix DAWG have been visited.
-		std::vector<std::pair<size_type, size_type> >
-			memo(last - first);
-		std::pair<size_type, size_type>* memoIt = memo.data();
-		for (It it = last; it > first; --it) {
-			if (unsigned(it - first) < best.qspan())
-				return best;
-			FMInterval interval = findSuffix(first, it, memoIt++);
-			if (interval.qspan() > best.qspan())
-				best = interval;
-		}
-		return best;
-	}
-
-	/** Translate from ASCII to the indexed alphabet. */
-	struct Translate {
-		Translate(const FMIndex& fmIndex) : m_fmIndex(fmIndex) { }
-		T operator()(unsigned char c) const
-		{
-			return c < m_fmIndex.m_mapping.size()
-				? m_fmIndex.m_mapping[c] : SENTINEL();
-		}
-	  private:
-		const FMIndex& m_fmIndex;
-	};
-
-	/** Search for a matching substring of the query at least k long.
-	 * @return the longest match and the number of matches
-	 */
-	Match find(const std::string& q, unsigned k) const
-	{
-		std::string s = q;
-		std::transform(s.begin(), s.end(), s.begin(),
-				Translate(*this));
-
-		FMInterval interval = findSubstring(s.begin(), s.end(), k);
-		assert(interval.l <= interval.u);
-		size_t count = interval.u - interval.l;
-		if (count == 0)
-			return Match(0, 0, 0, 0);
-		return Match(interval.qstart, interval.qend,
-				locate(interval.l), count);
-	}
-
-	/** Set the alphabet to [first, last). */
-	template <typename It>
-	void setAlphabet(It first, It last)
-	{
-		std::vector<bool> mask(std::numeric_limits<T>::max());
-		for (It it = first; it < last; ++it) {
-			assert((size_t)*it < mask.size());
-			mask[*it] = true;
-		}
-
-		m_alphabet.clear();
-		m_mapping.clear();
-		for (unsigned c = 0; c < mask.size(); ++c) {
-			if (!mask[c])
-				continue;
-			m_mapping.resize(c + 1, std::numeric_limits<T>::max());
-			m_mapping[c] = m_alphabet.size();
-			m_alphabet.push_back(c);
-		}
-		assert(!m_alphabet.empty());
-	}
-
-	/** Set the alphabet to the characters of s. */
-	void setAlphabet(const std::string& s)
-	{
-		setAlphabet(s.begin(), s.end());
-	}
+/** Set the alphabet to the characters of s. */
+void setAlphabet(const std::string& s)
+{
+	setAlphabet(s.begin(), s.end());
+}
 
 #define STRINGIFY(X) #X
 #define FM_VERSION_BITS(BITS) "FM " STRINGIFY(BITS) " 1"
