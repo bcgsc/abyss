ba3aba944166a6bc0d6c65b7b48d664defb5a89c
Shaun Jackman <sjackman@bcgsc.ca>
2011-12-06 00:17:55 +0000

    * Common/StringUtil.h (chomp): Bug fix. Fix the bug causing the error bool chomp(std::string&, char): Assertion `s.length() > 1' failed.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@74584 ad02020f-2a26-4133-947e-1829317c1416

--- Common/StringUtil.h
+++ Common/StringUtil.h
@@ -19,9 +19,8 @@ static inline char chop(std::string& s)
 /** If the last character of s is c, remove it and return true. */
 static inline bool chomp(std::string& s, char c = '\n')
 {
-	assert(s.length() > 1);
 	unsigned back = s.length() - 1;
-	if (s[back] == c) {
+	if (!s.empty() && s[back] == c) {
 		s.erase(back);
 		return true;
 	} else
