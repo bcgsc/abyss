7d3ee4c5f92ff14877351b4a5aa4614d1f466264
Shaun Jackman <sjackman@bcgsc.ca>
2012-02-07 18:56:35 +0000

    * DataLayer/fac.cc (-j,--jira): New option.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@76177 ad02020f-2a26-4133-947e-1829317c1416

--- DataLayer/fac.cc
+++ DataLayer/fac.cc
@@ -27,7 +27,7 @@ static const char USAGE_MESSAGE[] =
 "\n"
 "  -s, -t, --min-length=N  ignore sequences shorter than N bp [200]\n"
 "  -d, --delimiter=S       use S instead of tab for field delimiter\n"
-"  -j, --pipes             separate columns with pipes\n"
+"  -j, --jira              separate columns with pipes\n"
 "      --chastity          discard unchaste sequences [default]\n"
 "      --no-chastity       do not discard unchaste sequences\n"
 "      --trim-masked       trim masked bases from the end\n"
@@ -42,6 +42,7 @@ static const char USAGE_MESSAGE[] =
 namespace opt {
 	static unsigned minLength = 200;
 	static string delimiter;
+	static bool jira;
 	static int verbose;
 }
 
@@ -52,7 +53,7 @@ enum { OPT_HELP = 1, OPT_VERSION };
 static const struct option longopts[] = {
 	{ "min-length", no_argument, NULL, 's' },
 	{ "delimiter", required_argument, NULL, 'd' },
-	{ "pipes", no_argument, NULL, 'j' },
+	{ "jira", no_argument, NULL, 'j' },
 	{ "chastity", no_argument, &opt::chastityFilter, 1 },
 	{ "no-chastity", no_argument, &opt::chastityFilter, 0 },
 	{ "trim-masked", no_argument, &opt::trimMasked, 1 },
@@ -65,18 +66,43 @@ static const struct option longopts[] = {
 /** FastaReader flags. */
 static const int FASTAREADER_FLAGS = FastaReader::FOLD_CASE;
 
+/** Print contiguity statistics. */
 static void printContiguityStatistics(const char* path)
 {
+	// Read the sequences and count the lengths.
 	Histogram h;
 	FastaReader in(path, FASTAREADER_FLAGS);
 	for (FastaRecord record; in >> record;)
 		h.insert(record.seq.size());
+	assert(in.eof());
+
+	// Print the table header.
 	static bool printHeader = true;
+	if (opt::jira && printHeader) {
+		printHeader = false;
+		const char* sep = "\t||";
+		cout << "||"
+			<< "n" << sep
+			<< "n:" << opt::minLength << sep
+			<< "n:N50" << sep
+			<< "min" << sep
+			<< "N80" << sep
+			<< "N50" << sep
+			<< "N20" << sep
+			<< "max" << sep
+			<< "sum" << sep << '\n';
+	}
+
+	// Print the table.
+	if (opt::jira)
+		cout << '|';
 	printContiguityStats(cout, h, opt::minLength,
 			printHeader, opt::delimiter)
-		<< opt::delimiter << path << '\n';
+		<< opt::delimiter << path;
+	if (opt::jira)
+		cout << opt::delimiter;
+	cout << '\n';
 	printHeader = false;
-	assert(in.eof());
 }
 
 int main(int argc, char** argv)
@@ -98,6 +124,7 @@ int main(int argc, char** argv)
 			break;
 		  case 'j':
 			opt::delimiter = "\t|";
+			opt::jira = true;
 			break;
 		  case 's': case 't':
 			arg >> opt::minLength;
