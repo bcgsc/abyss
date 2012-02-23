ba3483b1fde586d4b10b45675aa6e873d4594314
Shaun Jackman <sjackman@bcgsc.ca>
2012-02-23 00:34:33 +0000

    * Common/MemoryUtil.h (getMemoryUsage) [__MACH__]: Use task_info.
    git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@76887 ad02020f-2a26-4133-947e-1829317c1416

--- Common/MemoryUtil.h
+++ Common/MemoryUtil.h
@@ -2,15 +2,28 @@
 #define MEMORYUTIL_H 1
 
 #include "config.h"
+#include <cassert>
 #include <fstream>
-#include <unistd.h>
+#include <unistd.h> // for sbrk
+
+#if __MACH__
+# include <mach/mach.h> // for mach_task_self
+# include <mach/task.h> // for task_info
+#endif
 
 /** Return the number of bytes used by the data and stack segments.
  * @return -1 on error
  */
 static inline ssize_t getMemoryUsage()
 {
-#if HAVE_GETPAGESIZE
+#if __MACH__
+    struct task_basic_info t_info;
+    mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+	int status = task_info(mach_task_self(),
+			TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count);
+	assert(status == KERN_SUCCESS);
+	return status == KERN_SUCCESS ? t_info.virtual_size : -1;
+#elif HAVE_GETPAGESIZE
 	std::ifstream in("/proc/self/statm");
 	size_t size, resident, share, text, lib, data;
 	return in >> size >> resident >> share >> text >> lib >> data
