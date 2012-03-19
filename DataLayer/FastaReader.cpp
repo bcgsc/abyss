bcgsc/abyss

commit 86daaf36420254ef7388ddba512dac2431bb66aa

* FastaReader.cpp (ReadSequence): Use an ostringstream instead of append.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@49549 ad02020f-2a26-4133-947e-1829317c1416

DataLayer/FastaReader.cpp
@@ -83,14 +83,13 @@ Sequence FastaReader::ReadSequence(string& id)
 			fields.push_back(field);
 
 		if (fields.size() == 11) {
-			id = fields[0];
-			for (int i = 1; i < 6; i++) {
-				id.append("_");
-				id.append(fields[i]);
-			}
-			id.append("/");
-			id.append(fields[7]);
+			ostringstream o(fields[0]);
+			for (int i = 1; i < 6; i++)
+				o << '_' << fields[i];
+			o << '/' << fields[7];
+			id = o.str();
 			s = fields[8];
+			assert(s.length() > 2);
 		} else {
 			fprintf(stderr, "error: `%s' is an unknown format\n"
 					"Expected either `>' or `@' or 11 fields\n"

commit 339bfb6da2a7599272cc71fcc2dfc440eb93f198

* DataLayer/FastaReader.cpp (read): Bug fix. The first field, machine name, of qseq and export files was being ignored. This bug was introduced in r49549.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@50535 ad02020f-2a26-4133-947e-1829317c1416

DataLayer/FastaReader.cpp
@@ -105,7 +105,8 @@ next_record:
 				goto next_record;
 			}
 
-			ostringstream o(fields[0]);
+			ostringstream o;
+			o << fields[0];
 			for (int i = 1; i < 6; i++)
 				o << '_' << fields[i];
 			o << '/' << fields[7];