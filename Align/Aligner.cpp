bcgsc/abyss

commit 063f4f273b8e07b91f94f2fa526fc94805ff6a3c

* Align/Aligner.cpp (getAlignmentsInternal, coalesceAlignments): Avoid makeing multiple copies of the target ID. * Align/Aligner.h (coalesceAlignments): No longer static, since this function calls contigIndexToID, which is not static.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@61155 ad02020f-2a26-4133-947e-1829317c1416

Align/Aligner.cpp
@@ -112,7 +112,7 @@ Aligner<SeqPosHashMap>::getAlignmentsInternal(
 				: Alignment::calculateReverseReadStart(
 						i, seqLen, m_hashSize);
 			unsigned ctgIndex = resultIter->second.contig;
-			Alignment align(contigIndexToID(ctgIndex).c_str(),
+			Alignment align(string(),
 					resultIter->second.pos, read_pos, m_hashSize,
 					seqLen, isRC);
 			aligns[ctgIndex].push_back(align);
@@ -165,6 +165,7 @@ void Aligner<SeqPosHashMap>::coalesceAlignments(
 			currIter++;
 		}
 
+		currAlign.contig = contigIndexToID(ctgIter->first).c_str();
 		*dest++ = value_type(currAlign, qid, seq);
 	}
 }

Align/Aligner.h
@@ -218,7 +218,7 @@ class Aligner
 				const Sequence& seq, bool isRC);
 
 		template <class oiterator>
-		static void coalesceAlignments(
+		void coalesceAlignments(
 				const std::string& qid, const std::string& seq,
 				const AlignmentSet& alignSet,
 				oiterator& dest);

commit 23d359ad01aeab5cce046c86ed568296ed5aa2d2

* Align/Aligner.cpp (coalesceAlignments): Fix a bug introduced in r61155 that rendered KAligner unusable.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@61810 ad02020f-2a26-4133-947e-1829317c1416

Align/Aligner.cpp
@@ -153,6 +153,7 @@ void Aligner<SeqPosHashMap>::coalesceAlignments(
 						!= prevIter->read_start_pos + qstep
 					|| currIter->contig_start_pos
 						!= prevIter->contig_start_pos + tstep) {
+				currAlign.contig = contigIndexToID(ctgIter->first);
 				*dest++ = value_type(currAlign, qid, seq);
 				currAlign = *currIter;
 			} else {
@@ -165,7 +166,7 @@ void Aligner<SeqPosHashMap>::coalesceAlignments(
 			currIter++;
 		}
 
-		currAlign.contig = contigIndexToID(ctgIter->first).c_str();
+		currAlign.contig = contigIndexToID(ctgIter->first);
 		*dest++ = value_type(currAlign, qid, seq);
 	}
 }