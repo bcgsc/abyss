bcgsc/abyss

commit fd540226fbd1c45aeedbd70c612515fd39fe914f

* KAligner/KAligner.cpp (alignReadsToDB): Use clear rather than str("").
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@49271 ad02020f-2a26-4133-947e-1829317c1416

KAligner/KAligner.cpp
@@ -225,6 +225,7 @@ void *alignReadsToDB(void* readsFile)
 		else
 			assert(isalpha(seq[0]));
 
+		output.clear();
 		size_t pos = seq.find_first_not_of("ACGT0123");
 		if (pos == string::npos)
 			g_aligner->alignRead(seq, out);
@@ -233,8 +234,6 @@ void *alignReadsToDB(void* readsFile)
 		cout << id << output.str() << '\n';
 		assert(cout.good());
 		pthread_mutex_unlock(&g_mutexCout);
-
-		output.str("");
 
 		if (opt::verbose > 0) {
 			pthread_mutex_lock(&g_mutexCerr);

commit 3d097eb0d4b6aa6e1d9b3ed0edaf9259fa905fb9

* KAligner/KAligner.cpp (alignReadsToDB): Fix a bug introduced in r49271. ostringstream.clear() does not have the expected effect of clearing the contents of the ostringstream buffer. str("") can be used instead. I moved the declaration of ostringstream inside the loop to obviate these shenanigans.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@49273 ad02020f-2a26-4133-947e-1829317c1416

KAligner/KAligner.cpp
@@ -212,10 +212,6 @@ static void readContigsIntoDB(string refFastaFile, Aligner& aligner)
 void *alignReadsToDB(void* readsFile)
 {
 	FastaReader fileHandle((const char *)readsFile);
-
-	ostringstream output;
-	prefix_ostream_iterator<Alignment> out(output, "\t");
-
 	while (fileHandle.isGood()) {
 		string id;
 		Sequence seq = fileHandle.ReadSequence(id);
@@ -225,10 +221,12 @@ void *alignReadsToDB(void* readsFile)
 		else
 			assert(isalpha(seq[0]));
 
-		output.clear();
+		ostringstream output;
 		size_t pos = seq.find_first_not_of("ACGT0123");
-		if (pos == string::npos)
+		if (pos == string::npos) {
+			prefix_ostream_iterator<Alignment> out(output, "\t");
 			g_aligner->alignRead(seq, out);
+		}
 
 		pthread_mutex_lock(&g_mutexCout);
 		cout << id << output.str() << '\n';