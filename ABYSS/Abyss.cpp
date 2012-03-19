bcgsc/abyss

commit 4c96994b87da180b3b106f12ac9fbb20579d6f22

* Assembly/AssemblyAlgorithms.cpp (openBubbleFile): New function. Factored from opt::parse. (writeBubble): Use ostream rather than FILE. Renamed from writeSNP. (popBubbles): Use it. * Assembly/AssemblyAlgorithms.h: Ditto. * Parallel/NetworkSequenceCollection.cpp (performNetworkPopBubbles): Use it. * Assembly/Options.cpp (opt::parse): Factor out the code that opens the bubble file. * Assembly/Options.h (snpFile): Remove this global variable.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@52174 ad02020f-2a26-4133-947e-1829317c1416

Assembly/AssemblyAlgorithms.cpp
@@ -13,6 +13,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -255,6 +256,26 @@ unsigned splitAmbiguous(ISequenceCollection* pSC)
 	return count;
 }
 
+/** Open the bubble file. */
+void openBubbleFile(ofstream& out)
+{
+	if (opt::snpPath.empty())
+		return;
+	string path;
+	if (opt::rank < 0) {
+		path = opt::snpPath;
+	} else {
+		ostringstream s;
+		s << "snp-" << opt::rank << ".fa";
+		path = s.str();
+	}
+	out.open(path.c_str());
+	if (!out.is_open()) {
+		perror(path.c_str());
+		exit(EXIT_FAILURE);
+	}
+}
+
 int popBubbles(ISequenceCollection* seqCollection, int kmerSize)
 {
 	Timer timer("PopBubbles");
@@ -263,6 +284,9 @@ int popBubbles(ISequenceCollection* seqCollection, int kmerSize)
 	// Set the cutoffs
 	const unsigned int expectedBubbleSize = 2*(kmerSize + 1);
 	const unsigned int maxNumBranches = 3;
+
+	ofstream out;
+	openBubbleFile(out);
 
 	for (ISequenceCollection::iterator iter = seqCollection->begin();
 			iter != seqCollection->end(); ++iter) {
@@ -320,7 +344,7 @@ int popBubbles(ISequenceCollection* seqCollection, int kmerSize)
 					else if(status == BGS_JOINED)
 					{
 						static unsigned snpID;
-						writeSNP(branchGroup, ++snpID);
+						writeBubble(out, branchGroup, ++snpID);
 						collapseJoinedBranches(seqCollection,
 								branchGroup);
 						numPopped++;
@@ -336,9 +360,6 @@ int popBubbles(ISequenceCollection* seqCollection, int kmerSize)
 		}
 		seqCollection->pumpNetwork();
 	}
-
-	if (opt::snpFile != NULL)
-		fflush(opt::snpFile);
 
 	if (numPopped > 0)
 		printf("Removed %u bubbles\n", numPopped);
@@ -418,11 +439,10 @@ bool processBranchGroupExtension(BranchGroup& group, size_t branchIndex, const P
 	return group.isExtendable();
 }
 
-/** Write SNP information to a file. */
-void writeSNP(BranchGroup& group, unsigned id)
+/** Write a bubble to the specified file. */
+void writeBubble(ostream& out, BranchGroup& group, unsigned id)
 {
-	FILE* fout = opt::snpFile;
-	if (fout == NULL)
+	if (opt::snpPath.empty())
 		return;
 
 	unsigned selectedIndex = group.getBranchToKeep();
@@ -431,10 +451,10 @@ void writeSNP(BranchGroup& group, unsigned id)
 	BranchRecord& refBranch = group.getBranch(selectedIndex);
 	Sequence refContig;
 	refBranch.buildContig(refContig);
-	fprintf(fout, ">%u%c %zu %u\n%s\n", id, allele++,
-			refContig.length(),
-			refBranch.getBranchMultiplicity(),
-			refContig.c_str());
+	out << '>' << id << allele++ << ' '
+		<< refContig.length() << ' '
+		<< refBranch.getBranchMultiplicity() << '\n'
+		<< refContig.c_str() << '\n';
 
 	unsigned numBranches = group.getNumBranches();
 	for (unsigned i = 0; i < numBranches; ++i) {
@@ -443,11 +463,12 @@ void writeSNP(BranchGroup& group, unsigned id)
 		BranchRecord& currBranch = group.getBranch(i);
 		Sequence contig;
 		currBranch.buildContig(contig);
-		fprintf(fout, ">%u%c %zu %u\n%s\n", id, allele++,
-				contig.length(),
-				currBranch.getBranchMultiplicity(),
-				contig.c_str());
+		out << '>' << id << allele++ << ' '
+			<< contig.length() << ' '
+			<< currBranch.getBranchMultiplicity() << '\n'
+			<< contig.c_str() << '\n';
 	}
+	assert(out.good());
 }
 
 //

Assembly/AssemblyAlgorithms.h
@@ -79,8 +79,8 @@ void initiateBranchGroup(BranchGroup& group, const PackedSeq& seq, const SeqExt&
 // process an a branch group extension
 bool processBranchGroupExtension(BranchGroup& group, size_t branchIndex, const PackedSeq& seq, ExtensionRecord extensions, int multiplicity);
 
-/** Write SNP information to a file. */
-void writeSNP(BranchGroup& group, unsigned id);
+void openBubbleFile(std::ofstream& out);
+void writeBubble(std::ostream& out, BranchGroup& group, unsigned id);
 
 // collapse bubbles that are joined together
 void collapseJoinedBranches(ISequenceCollection* seqCollection, BranchGroup& group);

Parallel/NetworkSequenceCollection.cpp
@@ -951,6 +951,9 @@ int NetworkSequenceCollection::performNetworkPopBubbles(ISequenceCollection* /*s
 	pumpNetwork();
 	assert(m_comm.receiveEmpty());
 
+	ofstream out;
+	AssemblyAlgorithms::openBubbleFile(out);
+
 	unsigned numPopped = 0;
 	for (BranchGroupMap::iterator iter = m_bubbles.begin();
 			iter != m_bubbles.end(); iter++) {
@@ -959,7 +962,7 @@ int NetworkSequenceCollection::performNetworkPopBubbles(ISequenceCollection* /*s
 		if (!iter->second.isAmbiguous(this))
 			continue;
 		numPopped++;
-		AssemblyAlgorithms::writeSNP(
+		AssemblyAlgorithms::writeBubble(out,
 				iter->second, m_numPopped + numPopped);
 		AssemblyAlgorithms::collapseJoinedBranches(
 				this, iter->second);
@@ -976,9 +979,6 @@ int NetworkSequenceCollection::performNetworkPopBubbles(ISequenceCollection* /*s
 		assert(m_comm.receiveEmpty());
 	}
 	m_bubbles.clear();
-
-	if (opt::snpFile != NULL)
-		fflush(opt::snpFile);
 
 	PrintDebug(0, "Removed %u bubbles\n", numPopped);
 	return numPopped;

Assembly/Options.cpp
@@ -91,9 +91,6 @@ string graphPath;
 /** output bubble path */
 string snpPath;
 
-/** output bubble file */
-FILE* snpFile;
-
 /** input FASTA files */
 vector<string> inFiles;
 
@@ -224,22 +221,6 @@ void parse(int argc, char* const* argv)
 		ostringstream s;
 		s << "contigs-" << opt::rank << ".fa";
 		contigsTempPath = s.str();
-	}
-
-	if (snpPath.length() > 0) {
-		string path;
-		if (rank < 0) {
-			path = snpPath;
-		} else {
-			ostringstream s;
-			s << "snp-" << opt::rank << ".fa";
-			path = s.str();
-		}
-		snpFile = fopen(path.c_str(), "w");
-		if (snpFile == NULL) {
-			perror(path.c_str());
-			exit(EXIT_FAILURE);
-		}
 	}
 
 	if (opt::rank <= 0)

Assembly/Options.h
@@ -1,7 +1,6 @@
 #ifndef ASSEMBLY_OPTIONS_H
 #define ASSEMBLY_OPTIONS_H 1
 
-#include <cstdio>
 #include <string>
 #include <vector>
 
@@ -17,7 +16,6 @@ namespace opt {
 	extern std::string contigsTempPath;
 	extern std::string graphPath;
 	extern std::string snpPath;
-	extern FILE* snpFile;
 	extern std::vector<std::string> inFiles;
 
 	void parse(int argc, char* const* argv);

commit 5fd11b514e9d1b66671940e267e3022531a45ed1

Fix a bug introduced in r52174, where the bubble file would be empty. * Assembly/AssemblyAlgorithms.cpp (popBubbles): Take an ostream argument. * Assembly/AssemblyAlgorithms.h: Ditto. * Parallel/NetworkSequenceCollection.h (performNetworkPopBubbles, controlPopBubbles): Ditto. * Parallel/NetworkSequenceCollection.cpp: Ditto. (run, runControl) <NAS_POPBUBBLE>: Update. * ABYSS/Abyss.cpp (popBubbles): Update.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@52398 ad02020f-2a26-4133-947e-1829317c1416

ABYSS/Abyss.cpp
@@ -36,14 +36,17 @@ static void removeLowCoverageContigs(ISequenceCollection* pSC)
 static void popBubbles(ISequenceCollection* pSC)
 {
 	puts("Popping bubbles");
+	ofstream out;
+	AssemblyAlgorithms::openBubbleFile(out);
 	unsigned totalPopped = 0;
 	int i;
 	for (i = 0; i < opt::bubbles; i++) {
-		unsigned numPopped = AssemblyAlgorithms::popBubbles(pSC);
+		unsigned numPopped = AssemblyAlgorithms::popBubbles(pSC, out);
 		if (numPopped == 0)
 			break;
 		totalPopped += numPopped;
 	}
+	assert(out.good());
 	printf("Removed %d bubbles in %d rounds\n",
 			totalPopped, i);
 }

Assembly/AssemblyAlgorithms.cpp
@@ -241,7 +241,7 @@ void openBubbleFile(ofstream& out)
 	}
 }
 
-int popBubbles(ISequenceCollection* seqCollection)
+int popBubbles(ISequenceCollection* seqCollection, ostream& out)
 {
 	Timer timer("PopBubbles");
 	int numPopped = 0;
@@ -249,9 +249,6 @@ int popBubbles(ISequenceCollection* seqCollection)
 	// Set the cutoffs
 	const unsigned int expectedBubbleSize = 2*(opt::kmerSize + 1);
 	const unsigned int maxNumBranches = 3;
-
-	ofstream out;
-	openBubbleFile(out);
 
 	for (ISequenceCollection::iterator iter = seqCollection->begin();
 			iter != seqCollection->end(); ++iter) {

Assembly/AssemblyAlgorithms.h
@@ -5,6 +5,7 @@
 #include "BranchRecord.h"
 #include "FastaWriter.h"
 #include "ISequenceCollection.h"
+#include <ostream>
 #include <vector>
 
 class Histogram;
@@ -73,7 +74,7 @@ bool processLinearExtensionForBranch(BranchRecord& branch,
 // Polymorphism removal
 
 // Pop bubbles (loops of sequence that diverge a single base, caused by SNPs or consistent sequence errors
-int popBubbles(ISequenceCollection* seqCollection);
+int popBubbles(ISequenceCollection* pSC, std::ostream& out);
 
 // Populate the branch group with the initial extensions to this sequence
 void initiateBranchGroup(BranchGroup& group, const Kmer& seq,

Parallel/NetworkSequenceCollection.cpp
@@ -70,6 +70,9 @@ void NetworkSequenceCollection::run()
 	/** The number of contigs and k-mer assembled. */
 	pair<unsigned, unsigned> numAssembled;
 
+	ofstream bubbleFile;
+	AssemblyAlgorithms::openBubbleFile(bubbleFile);
+
 	SetState(NAS_LOADING);
 	while (m_state != NAS_DONE) {
 		switch (m_state) {
@@ -197,7 +200,7 @@ void NetworkSequenceCollection::run()
 			case NAS_POPBUBBLE:
 			{
 				unsigned numPopped
-					= performNetworkPopBubbles(this);
+					= performNetworkPopBubbles(bubbleFile);
 				EndState();
 				SetState(NAS_WAITING);
 				m_comm.sendCheckPointMessage(numPopped);
@@ -492,16 +495,21 @@ void NetworkSequenceCollection::runControl()
 
 			case NAS_POPBUBBLE:
 			{
+				ofstream out;
+				AssemblyAlgorithms::openBubbleFile(out);
+
 				puts("Popping bubbles");
 				unsigned totalPopped = 0;
 				int i;
 				for (i = 0; i < opt::bubbles; i++) {
-					unsigned numPopped = controlPopBubbles();
+					unsigned numPopped = controlPopBubbles(out);
 					if (numPopped == 0)
 						break;
 					totalPopped += numPopped;
 				}
 				assert(totalPopped == m_numPopped);
+				assert(out.good());
+				out.close();
 				printf("Removed %u bubbles in %u rounds\n",
 						totalPopped, i);
 
@@ -957,7 +965,7 @@ int NetworkSequenceCollection::performNetworkDiscoverBubbles(ISequenceCollection
 }
 
 /** Pop bubbles discovered previously. */
-int NetworkSequenceCollection::performNetworkPopBubbles(ISequenceCollection* /*seqCollection*/)
+int NetworkSequenceCollection::performNetworkPopBubbles(ostream& out)
 {
 	Timer timer("NetworkPopBubbles");
 
@@ -966,9 +974,6 @@ int NetworkSequenceCollection::performNetworkPopBubbles(ISequenceCollection* /*s
 	// delivered, but we may not have dealt with them yet.
 	pumpNetwork();
 	assert(m_comm.receiveEmpty());
-
-	ofstream out;
-	AssemblyAlgorithms::openBubbleFile(out);
 
 	unsigned numPopped = 0;
 	for (BranchGroupMap::iterator iter = m_bubbles.begin();
@@ -1062,12 +1067,12 @@ unsigned NetworkSequenceCollection::controlDiscoverBubbles()
 }
 
 /** Pop the bubbles discovered previously. */
-int NetworkSequenceCollection::controlPopBubbles()
+int NetworkSequenceCollection::controlPopBubbles(ostream& out)
 {
 	controlDiscoverBubbles();
 
 	// Perform a round-robin bubble pop to avoid concurrency issues
-	m_checkpointSum = performNetworkPopBubbles(this);
+	m_checkpointSum = performNetworkPopBubbles(out);
 	EndState();
 
 	// Now tell all the slave nodes to perform the pop one by one

Parallel/NetworkSequenceCollection.h
@@ -9,6 +9,7 @@
 #include "FastaWriter.h"
 #include "MessageBuffer.h"
 #include "Timer.h"
+#include <ostream>
 #include <set>
 #include <utility>
 
@@ -47,7 +48,7 @@ class NetworkSequenceCollection : public ISequenceCollection
 		
 		// This function is similar to AssemblyAlgorithms::popBubbles but is optimized to hide latency
 		int performNetworkDiscoverBubbles(ISequenceCollection* seqCollection, int kmerSize);
-		int performNetworkPopBubbles(ISequenceCollection* seqCollection);
+		int performNetworkPopBubbles(std::ostream& out);
 
 		unsigned controlErode();
 		unsigned controlTrimRound(unsigned trimLen);
@@ -55,7 +56,7 @@ class NetworkSequenceCollection : public ISequenceCollection
 		unsigned controlRemoveMarked();
 		void controlCoverage();
 		unsigned controlDiscoverBubbles();
-		int controlPopBubbles();
+		int controlPopBubbles(std::ostream& out);
 		unsigned controlMarkAmbiguous();
 		unsigned controlSplitAmbiguous();
 		unsigned controlSplit();