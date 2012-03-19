bcgsc/abyss

commit 7a9e14f0d24776eaab63f52f2d9df49c77627d6e

* MergePaths/MergePaths.cpp (alignCoordinates): Bug fix. Fix a possible misassembly that causes PathOverlap to die: Distance get(edge_bundle_t, const Graph&, ContigNode, ContigNode): Assertion `e.second' failed.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@66029 ad02020f-2a26-4133-947e-1829317c1416

MergePaths/MergePaths.cpp
@@ -957,6 +957,9 @@ static bool alignCoordinates(iterator& first1, iterator last1,
 			ambiguous2 -= length(*it1);
 			*out++ = *it1++;
 		} else
+			assert(false);
+
+		if (ambiguous1 == 0 && ambiguous2 == 0)
 			break;
 		if (ambiguous1 < 0 || ambiguous2 < 0)
 			return false;

commit 3f649fe938b20a96aee5c0884a10b47fa03f59e0

* MergePaths/MergePaths.cpp (align): Fix a bug introduced in r66029. Ignore the trivial alignment when aligning a path to itself.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@66049 ad02020f-2a26-4133-947e-1829317c1416

MergePaths/MergePaths.cpp
@@ -1240,8 +1240,9 @@ static ContigPath align(
 		const ContigPath& path1, const ContigPath& path2,
 		ContigNode pivot, dir_type& orientation)
 {
-	if (path1 == path2) {
-		// The two paths are identical.
+	if (path1 == path2 && &path1 != &path2) {
+		// These two paths are identical. Ignore the trivial alignment
+		// when aligning a path to itself.
 		orientation = DIR_B;
 		return path1;
 	}