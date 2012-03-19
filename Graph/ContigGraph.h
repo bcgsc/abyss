bcgsc/abyss

commit d5fd3a9560fb42572e221deb941fd905542941bf

* Graph/ContigGraph.h: Must qualify identifier to find this declaration in dependent base class.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@60059 ad02020f-2a26-4133-947e-1829317c1416

Graph/ContigGraph.h
@@ -43,24 +43,24 @@ class ContigGraph : public DirectedGraph<VertexProp> {
 	/** Return the in degree of vertex v. */
 	degree_size_type in_degree(vertex_descriptor v) const
 	{
-		return out_degree(~v);
+		return DG::out_degree(~v);
 	}
 
 	/** Remove all out edges from vertex v. */
 	void clear_out_edges(vertex_descriptor v)
 	{
 		std::pair<out_edge_iterator, out_edge_iterator>
-			edges = out_edges(v);
+			edges = DG::out_edges(v);
 		for (out_edge_iterator it = edges.first;
 				it != edges.second; ++it)
-			remove_edge(~target(*it), ~v);
+			DG::remove_edge(~DG::target(*it), ~v);
 		DG::clear_out_edges(v);
 	}
 
 	/** Remove all in edges from vertex v. */
 	void clear_in_edges(vertex_descriptor v)
 	{
-		clear_out_edges(~v);
+		DG::clear_out_edges(~v);
 	}
 
 	/** Remove all edges to and from vertex v. */

commit 3bba9b3568bf60528c77dbef1e851fe8ee52e213

* Graph/ContigGraph.h (clear_in_edges): Bug fix. Bug introduced in r60059.
git-svn-id: https://svn.bcgsc.ca/svn/abyss/trunk@60097 ad02020f-2a26-4133-947e-1829317c1416

Graph/ContigGraph.h
@@ -60,7 +60,7 @@ class ContigGraph : public DirectedGraph<VertexProp> {
 	/** Remove all in edges from vertex v. */
 	void clear_in_edges(vertex_descriptor v)
 	{
-		DG::clear_out_edges(~v);
+		clear_out_edges(~v);
 	}
 
 	/** Remove all edges to and from vertex v. */