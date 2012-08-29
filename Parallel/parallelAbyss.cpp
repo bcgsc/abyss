afd7877704d8a1e3867156cfc74cdfb2a1a5e57b
traymond <traymond@bcgsc.ca>
2012-08-29 10:37:46 -0700

    ABYSS-P: Bug fix. Closes #11.
    * Parallel/parallelAbyss.cpp (main): OMPI-1.6.1 and later reset the
    SIGCHLD handler in MPI_Init so we need to reinitialize uncompress.

--- Parallel/parallelAbyss.cpp
+++ Parallel/parallelAbyss.cpp
@@ -71,6 +71,10 @@ int main(int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &opt::rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &opt::numProc);
 
+	// OMPI-1.6.1 and later reset the SIGCHLD handler so we need to
+	// reinitialize uncompress.
+	uncompress_init();
+
 	opt::parse(argc, argv);
 	if (opt::rank == 0)
 		cout << "Running on " << opt::numProc << " processors\n";
