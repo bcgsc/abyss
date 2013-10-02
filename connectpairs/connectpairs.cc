0285ae101e69a79cc0f1eeb87203746a809afe21
traymond <traymond@bcgsc.ca>
2013-10-02 11:14:59 -0700

    Adding program skelleton.

--- connectpairs/connectpairs.cc
+++ connectpairs/connectpairs.cc
@@ -1,4 +1,32 @@
+#include "DBGBloom.h"
+#include "DBGBloomAlgorithms.h"
+#include <cassert>
+
+void buildDBGBloom() { assert(false); abort(); }
+
+void findPaths() { assert(false); abort(); }
+
+void mergePathts() { assert(false); abort(); }
+
+void buildRead() { assert(false); abort(); }
+
+void processRead()
+{
+	findPaths();
+	mergePathts();
+	buildRead();
+}
+
+void processReads()
+{
+	// for each read
+	processRead();
+}
+
 int main()
 {
+	buildDBGBloom();
+	processReads();
+
 	return 0;
 }
