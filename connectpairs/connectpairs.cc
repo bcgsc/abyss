8baee0a0a413f16f67381470997fe3aa5a9297f4
traymond <traymond@bcgsc.ca>
2013-10-03 10:37:32 -0700

    Adding commented out function seqanTests for playing with SeqAn.

--- connectpairs/connectpairs.cc
+++ connectpairs/connectpairs.cc
@@ -10,9 +10,21 @@
 #include "Graph/Options.h"
 #include "Graph/GraphUtil.h"
 
+#include <iostream>
 #include <cassert>
 
+#define USESEQAN 0
+
+#if USESEQAN
+#include <seqan/align.h>
+#include <seqan/sequence.h>
+#include <seqan/align_split.h>
+#endif
+
 using namespace std;
+#if USESEQAN
+using namespace seqan;
+#endif
 
 namespace opt {
 	/** The size of a k-mer. */
@@ -24,6 +36,48 @@ static void loadBloomFilter(DBGBloom& g, const string& path)
 {
 	g.open(path);
 }
+
+#if USESEQAN
+const string r1 =
+"AGAATCAACCAACCGTTCAATGATATAATCAAGAGCGATATTGTAATCTTTGTTTCT";
+const string r2 =
+"CGACGTCCACCAATTCGTCCCTGTGCACGAGCAGTTTCCAGTCCAGCTTTTGTTCGT";
+const string ins =
+"AGAATCAACCAACCGTTCAATGATATAATCAAGAGCGATATTGTAATCTTTGTTTCTGTCACCCGGCCCCCACGACTCAAGGATTAGACCATAAACACCATCCTCTTCACCTATCGAACACTCAGCTTTCAGTTCAATTCCATTATTATCAAAAACATGCATAATATTAATCTTTAATCAATTTTTCACGACAATACTACTTTTATTGATAAAATTGCAACAAGTTGCTGTTGTTTTACTTTCTTTTGTACACAAAGTGTCTTTAACTTTATTTATCCCCTGCAGGAAACCTCTTATACAAAGTTGACACACCAACATCATAGATAATCGCCACCTTCTGGCGAGGAGTTCCTGCTGCAATTAATCGTCCAGCTTGTGCCCATTGTTCTGGTGTAAGTTTGGGACGACGTCCACCAATTCGTCCCTGTGCACGAGCAGTTTCCAGTCCAGCTTTTGTTCGT";
+
+static void seqanTests()
+{
+	typedef String<Dna> DS;
+	typedef Align<DS> Alignment;
+
+    //DS seq1 = "TTGT";
+    //DS seq2 = "TTAGT";
+	DS ref = ins;
+	DS seq1 = r1;
+	DS seq2 = r2;
+
+    Alignment align1;
+	resize(rows(align1), 2);
+	assignSource(row(align1, 0), ref);
+	assignSource(row(align1, 1), seq1);
+    Alignment align2;
+	resize(rows(align2), 2);
+	assignSource(row(align2, 0), ref);
+	assignSource(row(align2, 1), seq2);
+
+	Score<int> scoring(2, -2, -50, -100);
+
+	cout << splitAlignment(align1, align2, scoring) << endl;
+	cout << align1 << endl;
+	cout << align2 << endl;
+
+	cout << localAlignment(align1, scoring) << endl;
+	cout << align1 << endl;
+
+	cout << localAlignment(align2, scoring) << endl;
+	cout << align2 << endl;
+}
+#endif
 
 static void findPaths() { assert(false); abort(); }
 
@@ -49,6 +103,10 @@ static void processReads()
  */
 int main(int argc, const char* argv[])
 {
+#if USESEQAN
+	seqanTests();
+#endif
+
 	opt::verbose = 1;
 
 	assert(opt::k > 0);
