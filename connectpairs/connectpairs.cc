f7d5dd3fb07d4431a4bde0f1205acd9fb726bad5
Shaun Jackman <sjackman@gmail.com>
2013-10-07 12:08:04 -0700

    connectpairs: Parse command line options

--- connectpairs/connectpairs.cc
+++ connectpairs/connectpairs.cc
@@ -6,14 +6,16 @@
 #include "DBGBloomAlgorithms.h"
 
 #include "Common/Options.h"
+#include "DataLayer/Options.h"
 #include "Graph/DotIO.h"
 #include "Graph/Options.h"
 #include "Graph/GraphUtil.h"
 
-#include <iostream>
 #include <cassert>
+#include <getopt.h>
+#include <iostream>
 
-#define USESEQAN 0
+#undef USESEQAN
 
 #if USESEQAN
 #include <seqan/align.h>
@@ -26,10 +28,63 @@ using namespace std;
 using namespace seqan;
 #endif
 
+#define PROGRAM "abyss-connectpairs"
+
+static const char VERSION_MESSAGE[] =
+PROGRAM " (" PACKAGE_NAME ") " VERSION "\n"
+"Written by Shaun Jackman, Hamid Mohamadi, Anthony Raymond and\n"
+"Ben Vandervalk.\n"
+"\n"
+"Copyright 2013 Canada's Michael Smith Genome Science Centre\n";
+
+static const char USAGE_MESSAGE[] =
+"Usage: " PROGRAM " [OPTION]... READS1 READS2\n"
+"Connect the pairs READS1 and READS2 and close the gap using\n"
+"a Bloom filter de Bruijn graph.\n"
+"\n"
+" Options:\n"
+"\n"
+"  -k, --kmer=N            the size of a k-mer\n"
+"      --chastity          discard unchaste reads [default]\n"
+"      --no-chastity       do not discard unchaste reads\n"
+"      --trim-masked       trim masked bases from the ends of reads\n"
+"      --no-trim-masked    do not trim masked bases from the ends\n"
+"                          of reads [default]\n"
+"  -q, --trim-quality=N    trim bases from the ends of reads whose\n"
+"                          quality is less than the threshold\n"
+"      --standard-quality  zero quality is `!' (33)\n"
+"                          default for FASTQ and SAM files\n"
+"      --illumina-quality  zero quality is `@' (64)\n"
+"                          default for qseq and export files\n"
+"  -v, --verbose           display verbose output\n"
+"      --help              display this help and exit\n"
+"      --version           output version information and exit\n"
+"\n"
+"Report bugs to <" PACKAGE_BUGREPORT ">.\n";
+
 namespace opt {
 	/** The size of a k-mer. */
-	unsigned k = 16;
+	unsigned k;
 }
+
+static const char shortopts[] = "k:q:v";
+
+enum { OPT_HELP = 1, OPT_VERSION };
+
+static const struct option longopts[] = {
+	{ "kmer",             required_argument, NULL, 'k' },
+	{ "chastity",         no_argument, &opt::chastityFilter, 1 },
+	{ "no-chastity",      no_argument, &opt::chastityFilter, 0 },
+	{ "trim-masked",      no_argument, &opt::trimMasked, 1 },
+	{ "no-trim-masked",   no_argument, &opt::trimMasked, 0 },
+	{ "trim-quality",     required_argument, NULL, 'q' },
+	{ "standard-quality", no_argument, &opt::qualityOffset, 33 },
+	{ "illumina-quality", no_argument, &opt::qualityOffset, 64 },
+	{ "verbose",          no_argument, NULL, 'v' },
+	{ "help",             no_argument, NULL, OPT_HELP },
+	{ "version",          no_argument, NULL, OPT_VERSION },
+	{ NULL, 0, NULL, 0 }
+};
 
 /** Load the bloom filter. */
 static void loadBloomFilter(DBGBloom& g, const string& path)
@@ -101,20 +156,66 @@ static void processReads()
 /**
  * Connect pairs using a Bloom filter de Bruijn graph
  */
-int main(int argc, const char* argv[])
+int main(int argc, char** argv)
 {
+	bool die = false;
+
+	for (int c; (c = getopt_long(argc, argv,
+					shortopts, longopts, NULL)) != -1;) {
+		istringstream arg(optarg != NULL ? optarg : "");
+		switch (c) {
+		  case '?':
+			die = true; break;
+		  case 'k':
+			arg >> opt::k; break;
+		  case 'q':
+			arg >> opt::qualityThreshold; break;
+		  case 'v':
+			opt::verbose++; break;
+		  case OPT_HELP:
+			cerr << USAGE_MESSAGE;
+			exit(EXIT_SUCCESS);
+		  case OPT_VERSION:
+			cerr << VERSION_MESSAGE;
+			exit(EXIT_SUCCESS);
+		}
+		if (optarg != NULL && !arg.eof()) {
+			cerr << PROGRAM ": invalid option: `-"
+				<< (char)c << optarg << "'\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (opt::k == 0) {
+		cerr << PROGRAM ": missing mandatory option `-k'\n";
+		die = true;
+	}
+
+	if (argc - optind < 2) {
+		cerr << PROGRAM ": missing arguments\n";
+		die = true;
+	}
+
+	if (argc - optind > 2) {
+		cerr << PROGRAM ": too many arguments\n";
+		die = true;
+	}
+
+	if (die) {
+		cerr << "Try `" << PROGRAM
+			<< " --help' for more information.\n";
+		exit(EXIT_FAILURE);
+	}
+
+	Kmer::setLength(opt::k);
+
 #if USESEQAN
 	seqanTests();
 #endif
 
-	opt::verbose = 1;
-
-	assert(opt::k > 0);
-	Kmer::setLength(opt::k);
-
-	assert(argc > 1);
 	DBGBloom g(opt::k);
-	loadBloomFilter(g, argv[1]);
+	assert(optind < argc);
+	loadBloomFilter(g, argv[optind]);
 	if (opt::verbose > 0)
 		printGraphStats(cerr, g);
 
