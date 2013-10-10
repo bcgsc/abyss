bb2da492c09fc55938100481870b1ea9b64ff94f
mimiko <mimi.eun.ko@gmail.com>
2013-10-10 12:00:34 -0700

    Use absolute path to #include headers

--- Common/test/StringUtilTest.cpp
+++ Common/test/StringUtilTest.cpp
@@ -1,27 +1,28 @@
-#include "../StringUtil.h"
+#include "Common/StringUtil.h"
 #include "gtest/gtest.h"
-
 using namespace std;
 
-TEST(chop_test, base_case_2){
+TEST(chop_test, base_case_2)
+{
 	string myString = "me";
 	EXPECT_EQ(2, myString.length());
 	EXPECT_EQ('e', chop(myString));
 	EXPECT_EQ(1, myString.length());
 }
 	
-TEST(chop_test, trivial_gt_length2) {
-  string myString = "something";
-  EXPECT_EQ(9, myString.length()); // sanity check
-  EXPECT_EQ('g', chop(myString));
-  EXPECT_EQ(8, myString.length());
-  EXPECT_EQ('n', chop(myString));
-  EXPECT_EQ(7, myString.length());  
+TEST(chop_test, trivial_gt_length2)
+{
+	string myString = "something";
+	EXPECT_EQ(9, myString.length());
+	EXPECT_EQ('g', chop(myString));
+	EXPECT_EQ(8, myString.length());
+	EXPECT_EQ('n', chop(myString));
+	EXPECT_EQ(7, myString.length());  
 }
 
-TEST(chomp_test, base_cases){
-
-	// want to test .length=1 
+TEST(chomp_test, base_cases)
+{
+	// test .length=1 
 	string myString = "a";
 	EXPECT_TRUE(1 == myString.length());
 	EXPECT_FALSE(chomp(myString));
@@ -29,7 +30,7 @@ TEST(chomp_test, base_cases){
 	EXPECT_TRUE(1 == anotherString.length());
 	EXPECT_TRUE(chomp(anotherString));
 	
-	// want to test .length=2
+	// test .length=2
 	string greatString = "ab";
 	EXPECT_FALSE(chomp(greatString));
 	EXPECT_EQ(2, greatString.length());
@@ -39,8 +40,8 @@ TEST(chomp_test, base_cases){
 	EXPECT_EQ(1, badString.length());
 }
 
-TEST(toSI_test, all_the_cases){
-
+TEST(toSI_test, all_the_cases)
+{
 	// negative and zero values	
 	EXPECT_EQ ("-0.000123 ", toSI(-0.0001234));
 	EXPECT_EQ("1e-13 ", toSI(0.0000000000001));
@@ -58,8 +59,8 @@ TEST(toSI_test, all_the_cases){
 	EXPECT_EQ("23.4 G", toSI(23440222111));
 }
 
-
-template <typename T> class MultiTypes{
+template <typename T> 
+class MultiTypes{
  public:
  	T myVar;
     void type_int() { ::testing::StaticAssertTypeEq<int, T>(); }
@@ -67,67 +68,61 @@ template <typename T> class MultiTypes{
     void type_string() { ::testing::StaticAssertTypeEq<string, T>(); }
 };
 
-TEST(toEng_test, integer_cases) { 
-
-    EXPECT_EQ("1234", toEng(1234)); // it's just fine without the template!!
+TEST(toEng_test, integer_cases)
+{ 
+    EXPECT_EQ("1234", toEng(1234)); 
     
-	MultiTypes<int> snoopy; 
-	snoopy.type_int();
+	MultiTypes<int> temp; 
+	temp.type_int();
+	temp.myVar = 1234;	
+	EXPECT_EQ(temp.myVar, 1234);
+	EXPECT_EQ("1234", toEng(temp.myVar));
 	
-	snoopy.myVar = 1234;	
-	EXPECT_EQ(snoopy.myVar, 1234);
-	EXPECT_EQ("1234", toEng(snoopy.myVar));
-	
-    snoopy.myVar = 12345678;	
-	EXPECT_EQ("12.35e6", toEng(snoopy.myVar));
+    temp.myVar = 12345678;	
+	EXPECT_EQ("12.35e6", toEng(temp.myVar));
 
-	snoopy.myVar = 123456789;
-	EXPECT_EQ ("123.5e6" , toEng(snoopy.myVar));
+	temp.myVar = 123456789;
+	EXPECT_EQ ("123.5e6" , toEng(temp.myVar));
 }
 
-TEST(toEng_test, double_cases){
-	MultiTypes<double> snoopy;
-	snoopy.type_double();
+TEST(toEng_test, double_cases)
+{
+	MultiTypes<double> temp;
+	temp.type_double();
 
-	snoopy.myVar = 123.456;
-	EXPECT_EQ (snoopy.myVar, 123.456); // sanity check
-	EXPECT_EQ ("123.5", toEng (snoopy.myVar));
+	temp.myVar = 123.456;
+	EXPECT_EQ (temp.myVar, 123.456);
+	EXPECT_EQ ("123.5", toEng(temp.myVar));
 	
-	snoopy.myVar = 123456789.9;
-	EXPECT_EQ ("123.5e6", toEng(snoopy.myVar));
+	temp.myVar = 123456789.9;
+	EXPECT_EQ ("123.5e6", toEng(temp.myVar));
 }
 
-TEST(toEng_test, string_cases){
-	MultiTypes<string> snoopy;
-	snoopy.type_string();
-
-	snoopy.myVar = "123.456";
-	// EXPECT_EQ ("123.5", toEng(snoopy.myVar)); // doesn't work with string! 
+TEST(toEng_test, string_cases)
+{
+	MultiTypes<string> temp;
+	temp.type_string();
+	temp.myVar = "123.456";
 }
 
-TEST (startsWith_test, have_no_idea){
-
+TEST (startsWith_test, trivial_cases)
+{
 	EXPECT_TRUE (startsWith("hello", "hell"));
 	EXPECT_TRUE (startsWith("hello", "h"));
 	EXPECT_FALSE (startsWith("hello", "hello"));
 	EXPECT_TRUE (startsWith("hello", ""));
-//	EXPECT_FALSE (startsWith("hello", NULL));
 	EXPECT_FALSE (startsWith ("whatever", "who"));
 }
 
-
-
-
-
-
-
-TEST(endsWith_test, any_cases){
+TEST(endsWith_test, any_cases)
+{
 	// suffix should not be the string itself
 	EXPECT_FALSE(endsWith("hello", "hello"));
 	EXPECT_FALSE(endsWith("", "")); 
 	EXPECT_TRUE(endsWith("hello", ""));
 	
-	//EXPECT_FALSE(endsWith("hello", NULL)); // NULL is not valid
+	// EXPECT_FALSE(endsWith("hello", NULL)); 
+	// NULL is not valid
 	EXPECT_TRUE(endsWith("hello", "ello"));
 	EXPECT_TRUE(endsWith("hello", "o"));
 	EXPECT_FALSE(endsWith("hell", "hello"));
