be9eff570a39a84180b322a58a8c7abbaa64d817
Shaun Jackman <sjackman@gmail.com>
2013-10-18 11:29:30 -0700

    CountingBloomFilter: New class

---
+++ connectpairs/CountingBloomFilter.h
@@ -0,0 +1,72 @@
+#ifndef COUNTINGBLOOMFILTER_H
+#define COUNTINGBLOOMFILTER_H 1
+
+#include "BloomFilter.h"
+#include <vector>
+
+/** A counting Bloom filter. */
+class CountingBloomFilter
+{
+  public:
+	/** The key type. */
+	typedef BloomFilter::key_type key_type;
+
+	/** The maximum count of an element in this multiset. */
+	static const unsigned MAX_COUNT = 2;
+
+	/** Constructor */
+	CountingBloomFilter(size_t n)
+		: m_data(MAX_COUNT, BloomFilter(n)) { }
+
+	/** Return the hash value of this object. */
+	static size_t hash(const key_type& key)
+	{
+		return BloomFilter::hash(key);
+	}
+
+	/** Return the size of the bit array. */
+	size_t size() const { return m_data.back().size(); }
+
+	/** Return the number of elements with count >= MAX_COUNT. */
+	size_t popcount() const
+	{
+		return m_data.back().popcount();
+	}
+
+	/** Return whether the element with this index has count >=
+	 * MAX_COUNT.
+	 */
+	bool operator[](size_t i) const
+	{
+		assert(i < m_data.back().size());
+		return m_data.back()[i];
+	}
+
+	/** Return whether this element has count >= MAX_COUNT. */
+	bool operator[](const key_type& key) const
+	{
+		return m_data.back()[hash(key) % m_data.back().size()];
+	}
+
+	/** Add the object with the specified index to this multiset. */
+	void insert(size_t index)
+	{
+		for (unsigned i = 0; i < MAX_COUNT; ++i) {
+			if (!m_data[i][index]) {
+				m_data[i].insert(index);
+				break;
+			}
+		}
+	}
+
+	/** Add the object to this counting multiset. */
+	void insert(const key_type& key)
+	{
+		insert(hash(key) % m_data.back().size());
+	}
+
+  private:
+	std::vector<BloomFilter> m_data;
+};
+
+#endif
--- connectpairs/test/BloomFilter.cc
+++ connectpairs/test/BloomFilter.cc
@@ -1,4 +1,5 @@
 #include "connectpairs/BloomFilter.h"
+#include "connectpairs/CountingBloomFilter.h"
 
 #include <gtest/gtest.h>
 #include <string>
@@ -27,6 +28,40 @@ TEST(BloomFilter, base)
 	EXPECT_EQ(x.popcount(), 3);
 	EXPECT_TRUE(x[c]);
 	EXPECT_TRUE(x[x.hash(c) % x.size()]);
+
+	EXPECT_FALSE(x[d]);
+}
+
+TEST(CountingBloomFilter, base)
+{
+	CountingBloomFilter x(100);
+	EXPECT_EQ(x.size(), 100);
+
+	Kmer::setLength(16);
+	Kmer a("AGATGTGCTGCCGCCT");
+	Kmer b("TGGACAGCGTTACCTC");
+	Kmer c("TAATAACAGTCCCTAT");
+	Kmer d("GATCGTGGCGGGCGAT");
+
+	x.insert(a);
+	EXPECT_EQ(x.popcount(), 0);
+	EXPECT_FALSE(x[a]);
+	x.insert(a);
+	EXPECT_EQ(x.popcount(), 1);
+	EXPECT_TRUE(x[a]);
+	x.insert(b);
+	EXPECT_EQ(x.popcount(), 1);
+	EXPECT_FALSE(x[b]);
+	x.insert(c);
+	EXPECT_EQ(x.popcount(), 1);
+	EXPECT_FALSE(x[c]);
+	x.insert(b);
+	EXPECT_EQ(x.popcount(), 2);
+	EXPECT_TRUE(x[b]);
+	EXPECT_TRUE(x[x.hash(b) % x.size()]);
+	x.insert(x.hash(c) % x.size());
+	EXPECT_EQ(x.popcount(), 3);
+	EXPECT_TRUE(x[c]);
 
 	EXPECT_FALSE(x[d]);
 }
