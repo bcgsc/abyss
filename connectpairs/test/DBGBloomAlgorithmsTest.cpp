80e94e6ae04ccc34370aa9f7d2e43375c3f1bda5
Ben Vandervalk <benv@bcgsc.ca>
2013-10-18 14:41:10 -0700

    DBGBloomAlgorithms: update unit test to match new genome size param for DBGBloom constructor

--- connectpairs/test/DBGBloomAlgorithmsTest.cpp
+++ connectpairs/test/DBGBloomAlgorithmsTest.cpp
@@ -11,20 +11,22 @@ TEST(DBGBloomAlgorithmsTest, MergeOverlappingPair)
 	// Read 1:     GAT  =>
 	// Read 2:      TAC <=
 
+
 	const int k = 2;
 	Kmer::setLength(k);
-	DBGBloom g(k);
-	
+	const int readLength = 3;
 	string mergedSeq = "GATG";
+
 	FastaRecord read1, read2;
 	read1.id = "read/1";
-	read1.seq = mergedSeq.substr(0,3);
+	read1.seq = mergedSeq.substr(0,readLength);
 	read2.id = "read/2";
-	read2.seq = reverseComplement(mergedSeq.substr(1,3));
+	read2.seq = reverseComplement(mergedSeq.substr(1,readLength));
 
+	DBGBloom g(k, mergedSeq.size());
 	g.assign(read1.seq);
 	g.assign(read2.seq);
-	
+
 	vector<FastaRecord> mergedSeqs;
 	PathSearchResult result;
 	result = connectPairs(read1, read2, g, mergedSeqs, 1, 4);
