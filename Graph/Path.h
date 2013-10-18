355e5e50083b1c0e9e2fb97a13d47e100ffaa38d
Ben Vandervalk <benv@bcgsc.ca>
2013-10-18 11:01:36 -0700

    Path.h: fix trailing whitespace

--- Graph/Path.h
+++ Graph/Path.h
@@ -1,11 +1,11 @@
 #ifndef PATH_H_
 #define PATH_H_
 
-enum PathSearchResult { 
-	FOUND_PATH, 
-	TOO_MANY_PATHS, 
-	TOO_MANY_BRANCHES, 
-	NO_PATH 
+enum PathSearchResult {
+	FOUND_PATH,
+	TOO_MANY_PATHS,
+	TOO_MANY_BRANCHES,
+	NO_PATH
 };
 
 const unsigned NO_LIMIT = 0;
