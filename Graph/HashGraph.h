221f70fcb599b741df9460505d41cb9075da6b80
Ben Vandervalk <benv@bcgsc.ca>
2013-10-28 12:45:46 -0700

    HashGraph: graph_traits and in_edges/in_degree methods

--- Graph/HashGraph.h
+++ Graph/HashGraph.h
@@ -6,27 +6,150 @@
 #include <boost/graph/graph_traits.hpp>
 #include <vector>
 #include <cassert>
+#include <iostream>
 
 template <class VertexType>
 class HashGraph
 {
 public:
 
+	typedef HashGraph<VertexType> Graph;
+	typedef boost::graph_traits<Graph> GraphTraits;
+	typedef typename GraphTraits::EdgeList EdgeList;
+
+	typedef typename GraphTraits::vertex_descriptor vertex_descriptor;
+	typedef typename GraphTraits::edge_descriptor edge_descriptor;
+	typedef typename GraphTraits::out_edge_iterator out_edge_iterator;
+	typedef typename GraphTraits::in_edge_iterator in_edge_iterator;
+	typedef typename GraphTraits::degree_size_type degree_size_type;
+
+	typedef unordered_map<vertex_descriptor, EdgeList> EdgeMap;
+	typedef std::pair<vertex_descriptor, EdgeList> EdgeMapEntry;
+	typedef typename EdgeMap::iterator EdgeMapIterator;
+
+protected:
+
+	unordered_set<vertex_descriptor> m_vertices;
+	EdgeMap m_outEdges;
+	EdgeMap m_inEdges;
+
+public:
+
+	std::pair<out_edge_iterator, out_edge_iterator>
+	out_edges(vertex_descriptor v) const
+	{
+		out_edge_iterator ei, ei_end;
+		typename EdgeMap::const_iterator mapi = m_outEdges.find(v);
+
+		if (mapi != m_outEdges.end()) {
+			ei = mapi->second.begin();
+			ei_end = mapi->second.end();
+		}
+
+
+		return std::pair<out_edge_iterator, out_edge_iterator>(ei, ei_end);
+	}
+
+	degree_size_type
+	out_degree(vertex_descriptor v) const
+	{
+		typename EdgeMap::const_iterator mapi = m_outEdges.find(v);
+		if (mapi == m_outEdges.end())
+			return 0;
+		else
+			return mapi->second.size();
+	}
+
+	std::pair<in_edge_iterator, in_edge_iterator>
+	in_edges(vertex_descriptor v) const
+	{
+		in_edge_iterator ei, ei_end;
+		typename EdgeMap::const_iterator mapi = m_inEdges.find(v);
+
+		if (mapi != m_inEdges.end()) {
+			ei = mapi->second.begin();
+			ei_end = mapi->second.end();
+			std::cerr << "ei->second: " << ei->second << "\n";
+		}
+
+		return std::pair<in_edge_iterator, in_edge_iterator>(ei, ei_end);
+	}
+
+	degree_size_type
+	in_degree(vertex_descriptor v) const
+	{
+		typename EdgeMap::const_iterator mapi = m_inEdges.find(v);
+		if (mapi == m_inEdges.end())
+			return 0;
+		else
+			return mapi->second.size();
+	}
+
+	std::pair<edge_descriptor, bool>
+	add_edge(vertex_descriptor u, vertex_descriptor v)
+	{
+		edge_descriptor e(u, v);
+
+		EdgeList& outEdges = m_outEdges[u];
+		bool foundOutEdge = false;
+		for (unsigned i = 0; i < outEdges.size(); i++) {
+			if (outEdges[i] == e) {
+				foundOutEdge = true;
+				break;
+			}
+		}
+
+		EdgeList& inEdges = m_inEdges[v];
+		bool foundInEdge = false;
+		for (unsigned i = 0; i < inEdges.size(); i++) {
+			if (inEdges[i] == e) {
+				foundInEdge = true;
+				break;
+			}
+		}
+		
+		assert(foundInEdge == foundOutEdge);
+
+		if (foundOutEdge)
+			return std::pair<edge_descriptor, bool>(e, false);
+
+		outEdges.push_back(e);
+		inEdges.push_back(e);
+		m_vertices.insert(u);
+		m_vertices.insert(v);
+
+		return std::pair<edge_descriptor, bool>(e, true);
+	}
+
+};
+
+namespace boost {
+
+template <class VertexType>
+struct graph_traits<HashGraph<VertexType> > {
+
 	// Graph
 	typedef VertexType vertex_descriptor;
 
 	// IncidenceGraph
 	typedef std::pair<vertex_descriptor, vertex_descriptor> edge_descriptor;
+	typedef std::vector<edge_descriptor> EdgeList;
+	typedef typename EdgeList::const_iterator edge_iterator;
+	typedef edge_iterator out_edge_iterator;
 	typedef unsigned degree_size_type;
 
 	// BidirectionalGraph
-	typedef void in_edge_iterator;
+	typedef edge_iterator in_edge_iterator;
+
+	// AdjacencyGraph
+	typedef void adjacency_iterator;
 
 	// VertexListGraph
 	typedef void vertices_size_type;
 
 	// EdgeListGraph
 	typedef void edges_size_type;
+	typedef void vertex_iterator;
 
 	// PropertyGraph
 	typedef void vertex_bundled;
@@ -42,51 +165,9 @@ public:
 		boost::vertex_list_graph_tag,
 		boost::edge_list_graph_tag { };
 
-	typedef std::vector<edge_descriptor> EdgeList;
-
-	typedef typename EdgeList::const_iterator out_edge_iterator;
-
-protected:
-
-	typedef unordered_map<vertex_descriptor, EdgeList> EdgeMap;
-	typedef std::pair<vertex_descriptor, EdgeList> EdgeMapEntry;
-	typedef typename EdgeMap::iterator EdgeMapIterator;
-
-	unordered_set<vertex_descriptor> m_vertices;
-	EdgeMap m_outEdges;
-
-public:
-
-	std::pair<out_edge_iterator, out_edge_iterator>
-	out_edges(vertex_descriptor v) const
-	{
-		out_edge_iterator ei, ei_end;
-		typename EdgeMap::const_iterator mapi = m_outEdges.find(v);
-
-		if (mapi != m_outEdges.end()) {
-			ei = mapi->second.begin();
-			ei_end = mapi->second.end();
-		}
-
-		return std::pair<out_edge_iterator, out_edge_iterator>(ei, ei_end);
-	}
-
-	std::pair<edge_descriptor, bool>
-	add_edge(vertex_descriptor u, vertex_descriptor v)
-	{
-		edge_descriptor e(u, v);
-
-		EdgeList& outEdges = m_outEdges[u];
-		for (unsigned i = 0; i < outEdges.size(); i++) {
-			if (outEdges[i] == e) 
-				return std::pair<edge_descriptor, bool>(e, false);
-		}
-
-		outEdges.push_back(e);
-		return std::pair<edge_descriptor, bool>(e, true);
-	}
-
 };
+
+}
 
 // IncidenceGraph
 
@@ -99,6 +180,37 @@ out_edges(
 	const HashGraph<VertexType>& g)
 {
 	return g.out_edges(v);
+}
+
+template <class VertexType>
+typename HashGraph<VertexType>::degree_size_type
+out_degree(
+	typename HashGraph<VertexType>::vertex_descriptor v,
+	const HashGraph<VertexType>& g)
+{
+	return g.out_degree(v);
+}
+
+// BidirectionalGraph
+
+template <class VertexType>
+std::pair<
+	typename HashGraph<VertexType>::in_edge_iterator,
+	typename HashGraph<VertexType>::in_edge_iterator>
+in_edges(
+	typename HashGraph<VertexType>::vertex_descriptor v,
+	const HashGraph<VertexType>& g)
+{
+	return g.in_edges(v);
+}
+
+template <class VertexType>
+typename HashGraph<VertexType>::degree_size_type
+in_degree(
+	typename HashGraph<VertexType>::vertex_descriptor v,
+	const HashGraph<VertexType>& g)
+{
+	return g.in_degree(v);
 }
 
 // MutableGraph
--- Graph/test/HashGraphTest.cpp
+++ Graph/test/HashGraphTest.cpp
@@ -1,22 +1,80 @@
 #include "Graph/HashGraph.h"
-#include <boost/tuple/tuple.hpp> // for boost::tie
+#include <string>
 #include <gtest/gtest.h>
 
 using namespace std;
 
-TEST(HashGraphTest, GetOutEdges)
+namespace {
+
+class HashGraphTest : public ::testing::Test {
+
+protected:
+
+	typedef HashGraph<string> Graph;
+	typedef boost::graph_traits<Graph>::edge_descriptor edge_descriptor;
+	typedef boost::graph_traits<Graph>::out_edge_iterator out_edge_iterator;
+	typedef boost::graph_traits<Graph>::in_edge_iterator in_edge_iterator;
+
+	Graph simpleCyclicGraph;
+	
+	string a;
+	string b;
+	string c;
+	string d;
+
+	HashGraphTest() : a("a"), b("b"), c("c"), d("d") {
+
+		add_edge(a, b, simpleCyclicGraph);
+		add_edge(a, c, simpleCyclicGraph);
+		add_edge(b, d, simpleCyclicGraph);
+		add_edge(c, d, simpleCyclicGraph);
+
+	}
+};
+
+TEST_F(HashGraphTest, GetOutEdges)
 {
-	HashGraph<string> g;
-
-	string a("a");
-	string b("b");
-
-	HashGraph<string>::edge_descriptor edge(a, b);
+	edge_descriptor expectedEdge1(a, b);
+	edge_descriptor expectedEdge2(a, c);
 	
-	add_edge(a, b, g);
-	
-	HashGraph<string>::out_edge_iterator ei, ei_end;
-	boost::tie(ei, ei_end) = out_edges(a, g);
+	out_edge_iterator ei, ei_end;
+	boost::tie(ei, ei_end) = out_edges(a, simpleCyclicGraph);
 
-	ASSERT_EQ(edge, *ei);
+	ASSERT_TRUE(ei != ei_end);
+	edge_descriptor edge1 = *ei;
+	EXPECT_TRUE(edge1 == expectedEdge1 || edge1 == expectedEdge2);
+
+
+	ei++;
+	ASSERT_TRUE(ei != ei_end);
+	edge_descriptor edge2 = *ei;
+	EXPECT_TRUE(edge2 != edge1);
+	EXPECT_TRUE(edge2 == expectedEdge1 || edge2 == expectedEdge2);
+	
+	ei++;
+	EXPECT_TRUE(ei == ei_end);
+}
+
+TEST_F(HashGraphTest, GetInEdges)
+{
+	edge_descriptor expectedEdge1(b, d);
+	edge_descriptor expectedEdge2(c, d);
+	
+	out_edge_iterator ei, ei_end;
+	boost::tie(ei, ei_end) = in_edges(d, simpleCyclicGraph);
+
+	ASSERT_TRUE(ei != ei_end);
+	edge_descriptor edge1 = *ei;
+	EXPECT_TRUE(edge1 == expectedEdge1 || edge1 == expectedEdge2);
+
+	ei++;
+	ASSERT_TRUE(ei != ei_end);
+	edge_descriptor edge2 = *ei;
+	EXPECT_TRUE(edge2 != edge1);
+	EXPECT_TRUE(edge2 == expectedEdge1 || edge2 == expectedEdge2);
+	
+	ei++;
+	EXPECT_TRUE(ei == ei_end);
+}
+
 }
