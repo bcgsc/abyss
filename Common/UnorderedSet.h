aee1a3b8c60e9aa1b3a9b542a861c3ed15856ed2
Ben Vandervalk <ben.vvalk@gmail.com>
2013-10-27 18:53:26 -0700

    UnorderedSet.h: platform-independent unordered_set

---
+++ Common/UnorderedSet.h
@@ -0,0 +1,20 @@
+#ifndef UNORDEREDSET_H
+#define UNORDEREDSET_H 1
+
+#include "config.h"
+
+#if HAVE_UNORDERED_SET
+# include <unordered_set>
+using std::unordered_set;
+using std::unordered_multiset;
+#elif HAVE_TR1_UNORDERED_SET
+# include <tr1/unordered_set>
+using std::tr1::unordered_set;
+using std::tr1::unordered_multiset;
+#else
+# include <boost/unordered_set.hpp>
+using boost::unordered_set;
+using boost::unordered_multiset;
+#endif
+
+#endif
