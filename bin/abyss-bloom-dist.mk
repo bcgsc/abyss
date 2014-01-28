be00dd644fbc76d446a06cb75e3a164d0c023842
Ben Vandervalk <benv@bcgsc.ca>
2014-01-28 15:23:35 -0800

    abyss-bloom-dist.mk: calculate mem requirements for qmake sub-tasks

--- bin/abyss-bloom-dist.mk
+++ bin/abyss-bloom-dist.mk
@@ -14,6 +14,19 @@ l:=2
 
 PROGRAM_NAME=abyss-bloom-dist.mk
 
+# convert bloom filter size from kilobytes/megs/gigs to bytes
+override b:=$(shell echo '$b' | \
+	sed 's/k/*1024/I' | \
+	sed 's/m/*1024^2/I' | \
+	sed 's/g/*1024^3/I' | \
+	bc)
+
+# compute mem limits (in gigabytes) for SGE_RREQ lines below
+b_gb:=$(shell echo '$b / 1024^3' | bc -l)
+l1_mem_gb:=$(shell echo '$(b_gb) / $w + 1.05' | bc -l | xargs printf '%.1f')
+l2_mem_gb:=$(shell echo '2 * $(l1_mem_gb)' | bc -l)
+union_mem_gb:=$(shell echo '$(b_gb) + 2.05' | bc -l | xargs printf '%.1f')
+
 # a single space character
 space:=$(noop) $(noop)
 b_times_l:=$(shell echo '$b * $l' | bc)
@@ -88,11 +101,13 @@ build: args_check $(name).bloom.gz
 
 # level 1 bloom filter files
 $(name)_l1_%.bloom.gz: $(files)
+	SGE_RREQ="-N l1 -l mem_token=$(l1_mem_gb)G,mem_free=$(l1_mem_gb)G,h_vmem=$(l1_mem_gb)G" \
 	abyss-bloom build -v -k$k -b$b -w$(call getWindow,$@)/$w \
 		- $(call getReadFilename,$@) | gzip -c > $@
 
 # level 2 bloom filter files
 $(name)_l2_%.bloom.gz: $(l1_bloom_files)
+	SGE_RREQ="-N l2 -l mem_token=$(l2_mem_gb)G,mem_free=$(l2_mem_gb)G,h_vmem=$(l2_mem_gb)G" \
 	zcat $(call getSiblingBloomFiles,$(subst l2,l1,$@)) | \
 		abyss-bloom build -v -k$k -b$(b_times_l) -l2 \
 		-w$(call getWindow,$@)/$w \
@@ -102,6 +117,7 @@ $(name)_l2_%.bloom.gz: $(l1_bloom_files)
 
 # final output file
 $(name).bloom.gz: $(l2_bloom_files)
+	SGE_RREQ="-N union -l mem_token=$(union_mem_gb)G,mem_free=$(union_mem_gb)G,h_vmem=$(union_mem_gb)G" \
 	zcat $(l2_bloom_files) | \
 		abyss-bloom union -v -k$k - \
 		$(foreach i,$(l2_bloom_files),-) | \
@@ -112,8 +128,13 @@ $(name).bloom.gz: $(l2_bloom_files)
 #------------------------------------------------------------
 
 debug:
+	@echo 'b=$b'
 	@echo 'b_times_l=$(b_times_l)'
 	@echo 'b_mod_w=$(b_mod_w)'
+	@echo 'b_gb=$(b_gb)'
+	@echo 'l1_mem_gb=$(l1_mem_gb)'
+	@echo 'l2_mem_gb=$(l2_mem_gb)'
+	@echo 'union_mem_gb=$(union_mem_gb)'
 	@echo 'l1_bloom_files="$(l1_bloom_files)"'
 	@echo 'l2_bloom_files="$(l2_bloom_files)"'
 	@echo '$$(call getWindow, $(word 1,$(l1_bloom_files))): $(call getWindow,$(word 1,$(l1_bloom_files)))'
