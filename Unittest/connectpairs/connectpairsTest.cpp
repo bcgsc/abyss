b26ca43556b80dc51aa0420799a708a4b315492c
Anthony Raymond <traymond@bcgsc.ca>
2014-02-07 12:11:19 -0800

    Fix connectpairs tests

--- Unittest/connectpairs/connectpairsTest.cpp
+++ Unittest/connectpairs/connectpairsTest.cpp
@@ -1,6 +1,9 @@
 #include "connectpairs/connectpairs.h"
+#include <iostream>
 
 #include <gtest/gtest.h>
+
+using namespace std;
 
 TEST(maskNew, read1)
 {
@@ -8,10 +11,23 @@ TEST(maskNew, read1)
 	FastqRecord r2;
 	FastaRecord read("2", "", "ACGTACGT");
 
-	EXPECT_TRUE(maskNew(r1, r2, read) == 0u);
+	int mask = 1;
+	EXPECT_TRUE(maskNew(r1, r2, read, mask) == 0u);
 	EXPECT_TRUE(read.seq == "ACGTACGT");
 
 	read = FastaRecord("2", "", "ACGTACGTA");
-	EXPECT_TRUE(maskNew(r1, r2, read);
+	EXPECT_TRUE(maskNew(r1, r2, read, mask) == 0u);
+	cout << read.seq << endl;
 	EXPECT_TRUE(read.seq == "ACGTACGTa");
+}
+
+TEST(maskNew, mask)
+{
+	FastqRecord r1("1", "", "ACGTA", "BBBBB");
+	FastqRecord r2;
+	FastaRecord read("2", "", "ACGTACGT");
+
+	int mask = 0;
+	EXPECT_TRUE(maskNew(r1, r2, read, mask) == 0u);
+	EXPECT_TRUE(read.seq == "ACGTACGT");
 }
