f32569b6efb5070f2c36a53d5ef327a005af3a55
Anthony Raymond <traymond@bcgsc.ca>
2014-02-07 12:09:44 -0800

    Inline maskNew so it can be tested as a unit

--- connectpairs/connectpairs.cc
+++ connectpairs/connectpairs.cc
@@ -227,37 +227,6 @@ static void seqanTests()
 }
 #endif
 
-/** Uppercase only bases that are present in original reads.
- *  @return number of mis-matching bases. */
-static unsigned maskNew(const FastqRecord& read1, const FastqRecord& read2,
-		FastaRecord& merged)
-{
-	Sequence r1 = read1.seq, r2 = reverseComplement(read2.seq);
-	if (opt::mask) {
-		transform(r1.begin(), r1.end(), r1.begin(), ::tolower);
-		transform(r2.begin(), r2.end(), r2.begin(), ::tolower);
-		transform(merged.seq.begin(), merged.seq.end(), merged.seq.begin(),
-				::tolower);
-	}
-	unsigned mismatches = 0;
-	for (unsigned i = 0; i < r1.size(); i++) {
-		assert(i < merged.seq.size());
-		if (r1[i] == merged.seq[i])
-			merged.seq[i] = toupper(r1[i]);
-		else
-			mismatches++;
-	}
-	for (unsigned i = 0; i < r2.size(); i++) {
-		assert(r2.size() <= merged.seq.size());
-		unsigned merged_loc = i + merged.seq.size() - r2.size();
-		if (r2[i] == merged.seq[merged_loc])
-			merged.seq[merged_loc] = toupper(r2[i]);
-		else
-			mismatches++;
-	}
-	return mismatches;
-}
-
 /** Connect a read pair. */
 static void connectPair(const DBGBloom& g,
 	const FastqRecord& read1,
@@ -313,7 +282,7 @@ static void connectPair(const DBGBloom& g,
 	  case FOUND_PATH:
 		assert(!paths.empty());
 		if (paths.size() == 1) {
-			if (maskNew(read1, read2, paths.front()) > opt::maxMismatches) {
+			if (maskNew(read1, read2, paths.front(), opt::mask) > opt::maxMismatches) {
 #pragma omp atomic
 				++g_count.tooManyReadMismatches;
 			} else {
@@ -331,7 +300,7 @@ static void connectPair(const DBGBloom& g,
 			if (size - matches <= opt::maxMismatches) {
 				FastaRecord read = paths.front();
 				read.seq = aln.match_align;
-				if (maskNew(read1, read2, read) > opt::maxMismatches) {
+				if (maskNew(read1, read2, read, opt::mask) > opt::maxMismatches) {
 #pragma omp atomic
 					++g_count.tooManyReadMismatches;
 				} else {
--- connectpairs/connectpairs.h
+++ connectpairs/connectpairs.h
@@ -2,7 +2,35 @@
 #define CONNECTPAIRS_H
 #include "DataLayer/FastaInterleave.h"
 
-static unsigned maskNew(const FastqRecord& read1, const FastqRecord& read2,
-		FastaRecord& merged);
+/** Uppercase only bases that are present in original reads.
+ *  @return number of mis-matching bases. */
+static inline unsigned maskNew(const FastqRecord& read1, const FastqRecord& read2,
+		FastaRecord& merged, int mask = 0)
+{
+	Sequence r1 = read1.seq, r2 = reverseComplement(read2.seq);
+	if (mask) {
+		transform(r1.begin(), r1.end(), r1.begin(), ::tolower);
+		transform(r2.begin(), r2.end(), r2.begin(), ::tolower);
+		transform(merged.seq.begin(), merged.seq.end(), merged.seq.begin(),
+				::tolower);
+	}
+	unsigned mismatches = 0;
+	for (unsigned i = 0; i < r1.size(); i++) {
+		assert(i < merged.seq.size());
+		if (r1[i] == merged.seq[i])
+			merged.seq[i] = toupper(r1[i]);
+		else
+			mismatches++;
+	}
+	for (unsigned i = 0; i < r2.size(); i++) {
+		assert(r2.size() <= merged.seq.size());
+		unsigned merged_loc = i + merged.seq.size() - r2.size();
+		if (r2[i] == merged.seq[merged_loc])
+			merged.seq[merged_loc] = toupper(r2[i]);
+		else
+			mismatches++;
+	}
+	return mismatches;
+}
 
 #endif
