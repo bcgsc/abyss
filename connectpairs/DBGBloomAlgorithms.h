51f769a704ef423318723f36c1136a953b2374a6
Ben Vandervalk <benv@bcgsc.ca>
2014-02-27 11:23:23 -0800

    connectpairs: refactoring -- move maskNew helper method to DBGBloomAlgorithms.h

--- connectpairs/DBGBloomAlgorithms.h
+++ connectpairs/DBGBloomAlgorithms.h
@@ -167,4 +167,35 @@ static inline bool correctSingleBaseError(
 	return true;
 }
 
+/** Uppercase only bases that are present in original reads.
+ *  @return number of mis-matching bases. */
+static inline unsigned maskNew(const FastaRecord& read1, const FastaRecord& read2,
+		FastaRecord& merged, int mask = 0)
+{
+	Sequence r1 = read1.seq, r2 = reverseComplement(read2.seq);
+	if (mask) {
+		transform(r1.begin(), r1.end(), r1.begin(), ::tolower);
+		transform(r2.begin(), r2.end(), r2.begin(), ::tolower);
+		transform(merged.seq.begin(), merged.seq.end(), merged.seq.begin(),
+				::tolower);
+	}
+	unsigned mismatches = 0;
+	for (unsigned i = 0; i < r1.size(); i++) {
+		assert(i < merged.seq.size());
+		if (r1[i] == merged.seq[i])
+			merged.seq[i] = toupper(r1[i]);
+		else
+			mismatches++;
+	}
+	for (unsigned i = 0; i < r2.size(); i++) {
+		assert(r2.size() <= merged.seq.size());
+		unsigned merged_loc = i + merged.seq.size() - r2.size();
+		if (r2[i] == merged.seq[merged_loc])
+			merged.seq[merged_loc] = toupper(r2[i]);
+		else
+			mismatches++;
+	}
+	return mismatches;
+}
+
 #endif
