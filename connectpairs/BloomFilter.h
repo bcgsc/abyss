01b7841c63ec3f9e7833e7812656680173e5d566
Ben Vandervalk <benv@traymond01-imac.phage.bcgsc.ca>
2014-05-22 16:53:49 -0700

    BloomFilter.h: change #defines to static const

--- connectpairs/BloomFilter.h
+++ connectpairs/BloomFilter.h
@@ -15,8 +15,8 @@
 #include <boost/dynamic_bitset.hpp>
 #include <algorithm>
 
-#define BLOOM_VERSION 2
-#define IO_BUFFER_SIZE (unsigned long)(32*1024)
+static const unsigned BLOOM_VERSION = 2;
+static const unsigned long IO_BUFFER_SIZE = 32*1024;
 
 /** A Bloom filter. */
 class BloomFilter : public virtual BloomFilterBase
