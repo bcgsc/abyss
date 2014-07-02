4c8978e92e317eac6484f2b27fa132cec725b617
Mimi Ko <mimi.eun.ko@gmail.com>
2014-07-02 02:48:05 -0700

    API: vectorUtil.h: make_vector<t>: new friend operator +=

--- Common/VectorUtil.h
+++ Common/VectorUtil.h
@@ -6,15 +6,25 @@
 template <typename T>
 class make_vector {
 private:
-	std::vector<T> aVector;
+	std::vector<T> vec;
+
 public:
 	typedef make_vector<T> mv;
+	typedef std::vector<T> sv;
+
 	mv& operator<< (const T& val) {
-		aVector.push_back(val);
+		vec.push_back(val);
 		return *this;
 	}
-	operator std::vector<T>() const {
-		return aVector;
+
+	operator sv() const {
+		return vec;
+	}
+
+	friend sv& operator+= (sv& vec1, const mv& vec2) {
+		sv temp_vec = vec2;
+		vec1.insert (vec1.end(), temp_vec.begin(), temp_vec.end());
+		return vec1;
 	}
 };
 
