4c9dec5a1d22caf10d60999e2c38f9567704df83
Ben Vandervalk <benv@bcgsc.ca>
2014-07-18 11:36:27 -0700

    BitUtils: functions to read and copy data with bit-level resolution

--- Common/BitUtil.h
+++ Common/BitUtil.h
@@ -4,6 +4,10 @@
 #include "config.h"
 #include <cstdlib> // for exit
 #include <stdint.h>
+#include <cassert>
+#include <iostream>
+
+enum BitwiseOp { BITWISE_OVERWRITE, BITWISE_OR, BITWISE_AND };
 
 /** The return value of the CPUID instruction. */
 struct CPUID { unsigned a, b, c, d; };
@@ -48,6 +52,109 @@ static inline uint64_t popcount(uint64_t x)
 	x = x + (x >> 16);
 	x = x + (x >> 32);
 	return x & 0x7FLLU;
+}
+
+/**
+ * Memory copy with bit-level resolution.
+ *
+ * @param src source data to copy (byte array)
+ * @param dest destination of copied data (byte array)
+ * @param bits number of bits to copy from src, starting from
+ *        first bit of first byte of src
+ * @param bitOffset bit offset into dest
+ * @param op how to combine existing bits in dest with bits
+ *        src (options: BITWISE_OVERWRITE, BITWISE_OR,
+ *        BITWISE_AND)
+ */
+static inline void copyBits(char* src, char* dest, size_t bits,
+	size_t bitOffset = 0, BitwiseOp op = BITWISE_OVERWRITE)
+{
+	size_t bytes = (bits + 7) / 8;
+	size_t fullBytes = (bits % 8 == 0) ? bytes : bytes - 1;
+	size_t byteOffset = bitOffset / 8;
+	unsigned char shift = bitOffset % 8;
+	unsigned char carryMask = 0xFF << (8 - shift);
+	for (size_t i = 0; i < fullBytes; i++) {
+		if (op == BITWISE_OVERWRITE) {
+			dest[byteOffset + i] &= carryMask;
+			dest[byteOffset + i + 1] &= ~carryMask;
+		}
+		if (op == BITWISE_AND) {
+			dest[byteOffset + i] &= src[i] >> shift | carryMask;
+			dest[byteOffset + i + 1] &= src[i] << (8 - shift) | ~carryMask;
+		}
+		else {
+			dest[byteOffset + i] |= src[i] >> shift;
+			dest[byteOffset + i + 1] |= src[i] << (8 - shift);
+		}
+	}
+	if (fullBytes < bytes) {
+		unsigned char bitsInLastByte = bits % 8;
+		unsigned char lastByteMask = 0xFF << (8 - bitsInLastByte);
+		unsigned char lastCarryMask = lastByteMask << (8 - shift);
+		char lastByte = src[bytes - 1];
+		lastByte &= lastByteMask;
+		size_t lastByteIndex = byteOffset + bytes - 1;
+		if (op == BITWISE_OVERWRITE)
+			dest[lastByteIndex] &= ~(lastByteMask >> shift);
+		if (op == BITWISE_AND)
+			dest[lastByteIndex] &= lastByte >> shift | ~(lastByteMask >> shift);
+		else
+			dest[lastByteIndex] |= lastByte >> shift;
+		if (lastCarryMask > 0) {
+			if (op == BITWISE_OVERWRITE)
+				dest[lastByteIndex + 1] &= ~lastCarryMask;
+			if (op == BITWISE_AND)
+				dest[lastByteIndex + 1] &= lastByte << (8 - shift) | ~lastCarryMask;
+			else
+				dest[lastByteIndex + 1] |= lastByte << (8 - shift);
+		}
+	}
+}
+
+/**
+ * Read data from a stream with bit-level resolution.
+ *
+ * @param in input byte stream
+ * @param dest destination of copied data (byte array)
+ * @param bits number of bits to read from in, starting from
+ *        first bit of first byte of in
+ * @param bitOffset bit offset into dest
+ */
+static void readBits(std::istream& in, char* dest, size_t bits,
+	size_t bitOffset = 0, BitwiseOp op = BITWISE_OVERWRITE)
+{
+	(void)readBits;
+
+	const size_t IO_BUFFER_SIZE = 32 * 1024;
+	size_t byteOffset = bitOffset / 8;
+	unsigned char shift = bitOffset % 8;
+
+	if (op == BITWISE_OVERWRITE && shift == 0) {
+		// Simple byte-aligned copy.
+		size_t bytes = (bits + 7) / 8;
+		size_t fullBytes = (bits % 8 == 0) ? bytes : bytes - 1;
+		in.read(dest + byteOffset, fullBytes);
+		assert(in);
+		if (fullBytes < bytes) {
+			char lastByte;
+			in.read(&lastByte, 1);
+			assert(in);
+			copyBits(&lastByte, dest + byteOffset + fullBytes, bits % 8);
+		}
+	} else {
+		// Non-byte-aligned copy. A portion of each src byte is
+		// carried over into the next dest byte.
+		char buffer[IO_BUFFER_SIZE];
+		for(size_t i = 0; i < bits;) {
+			size_t bitsRead = std::min(IO_BUFFER_SIZE * 8, bits - i);
+			size_t bytesRead = (bitsRead + 7)/8;
+			in.read(buffer, bytesRead);
+			assert(in);
+			copyBits(buffer, dest + byteOffset, bitsRead, i + shift, op);
+			i += bitsRead;
+		}
+	}
 }
 
 #endif
--- Unittest/Common/BitUtilTest.cpp
+++ Unittest/Common/BitUtilTest.cpp
@@ -1,5 +1,6 @@
 #include "Common/BitUtil.h"
 #include "gtest/gtest.h"
+#include <boost/utility/binary.hpp>
 
 /** Test limits */
 TEST(popcountTest, boundaries)
@@ -14,4 +15,233 @@ TEST(popcountTest, random_values)
 	EXPECT_EQ(45ULL, popcount(0x992E54FFFFFFFBA1ULL));
 	EXPECT_EQ(45ULL, popcount(0x814BC5FFFFFFF7FULL));
 	EXPECT_EQ(46ULL, popcount(0x815BC5FFFFFFF7FULL));
+}
+
+TEST(readBitsTest, overwriteBits)
+{
+	// NOTE!: In the binary diagrams below, the rightmost bit of each byte
+	// is the LSB.
+
+	size_t bitSize, bitOffset;
+	char src[2];
+	char dest[4];
+
+	// SUBTEST: src size < 1 byte
+	//
+	// src             = 000
+	// bit offset      = 9
+	// dest            = 11111111 11111111 11111111 11111111
+	// expected result = 11111111 10001111 11111111 11111111
+
+	bitSize = 3;
+	bitOffset = 9;
+	memset(src, 0x00, 2);
+	memset(dest, 0xFF, 4);
+
+	std::istringstream in1(std::string(src, (bitSize + 7)/8));
+
+	readBits(in1, dest, bitSize, bitOffset);
+
+	EXPECT_EQ((char)0xFF, dest[0]);
+	EXPECT_EQ((char)BOOST_BINARY(10001111), dest[1]);
+	EXPECT_EQ((char)0xFF, dest[2]);
+	EXPECT_EQ((char)0xFF, dest[3]);
+
+	// SUBTEST: byte-aligned with partial last byte
+	//
+	// src             = 00000000 000
+	// bit offset      = 8
+	// dest            = 11111111 11111111 11111111 11111111
+	// expected result = 11111111 00000000 00011111 11111111
+
+	bitSize = 11;
+	bitOffset = 8;
+	memset(src, 0x00, 2);
+	memset(dest, 0xFF, 4);
+
+	std::istringstream in2(std::string(src, (bitSize + 7)/8));
+
+	readBits(in2, dest, bitSize, bitOffset);
+
+	EXPECT_EQ((char)0xFF, dest[0]);
+	EXPECT_EQ((char)0x00, dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(00011111), dest[2]);
+	EXPECT_EQ((char)0xFF, dest[3]);
+
+	// SUBTEST: not byte-aligned, src size > 1 byte
+	//
+	// src             = 00000000 00000000
+	// bit offset      = 4
+	// dest            = 11111111 11111111 11111111 11111111
+	// expected result = 11110000 00000000 00011111 11111111
+
+	bitSize = 15;
+	bitOffset = 4;
+	memset(src, 0x00, 2);
+	memset(dest, 0xFF, 4);
+
+	std::istringstream in3(std::string(src, (bitSize + 7)/8));
+
+	readBits(in3, dest, bitSize, bitOffset);
+
+	EXPECT_EQ((char)BOOST_BINARY(11110000), dest[0]);
+	EXPECT_EQ((char)0x00, dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(00011111), dest[2]);
+	EXPECT_EQ((char)0xFF, dest[3]);
+}
+
+TEST(readBitsTest, orBits)
+{
+	// NOTE!: In the binary diagrams below, the rightmost bit of each byte
+	// is the LSB.
+
+	size_t bitSize, bitOffset;
+	char src[2];
+	char dest[4];
+
+	// SUBTEST: src size < 1 byte
+	//
+	// src             = 101
+	// bit offset      = 9
+	// dest            = 10101010 10101010 10101010 10101010
+	// expected result = 10101010 11111010 10101010 10101010
+
+	bitSize = 3;
+	bitOffset = 9;
+	memset(src, 0x00, 2);
+	src[0] = BOOST_BINARY(10100000);
+	memset(dest, BOOST_BINARY(10101010), 4);
+
+	std::istringstream in1(std::string(src, (bitSize + 7)/8));
+
+	readBits(in1, dest, bitSize, bitOffset, BITWISE_OR);
+
+	EXPECT_EQ((char)BOOST_BINARY(10101010), dest[0]);
+	EXPECT_EQ((char)BOOST_BINARY(11111010), dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(10101010), dest[2]);
+	EXPECT_EQ((char)BOOST_BINARY(10101010), dest[3]);
+
+	// SUBTEST: byte-aligned with partial last byte
+	//
+	// src             = 01010101 010
+	// bit offset      = 8
+	// dest            = 10101010 10101010 10101010 10101010
+	// expected result = 10101010 11111111 11101010 10101010
+
+	bitSize = 11;
+	bitOffset = 8;
+	memset(src, 0x00, 2);
+	src[0] = BOOST_BINARY(01010101);
+	src[1] = BOOST_BINARY(01000000);
+	memset(dest, BOOST_BINARY(10101010), 4);
+
+	std::istringstream in2(std::string(src, (bitSize + 7)/8));
+
+	readBits(in2, dest, bitSize, bitOffset, BITWISE_OR);
+
+	EXPECT_EQ((char)BOOST_BINARY(10101010), dest[0]);
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(11101010), dest[2]);
+	EXPECT_EQ((char)BOOST_BINARY(10101010), dest[3]);
+
+	// SUBTEST: not byte-aligned, src size > 1 byte
+	//
+	// src             = 01010101 0101010
+	// bit offset      = 4
+	// dest            = 10101010 10101010 10101010 10101010
+	// expected result = 10101111 11111111 11101010 10101010
+
+	bitSize = 15;
+	bitOffset = 4;
+	memset(src, 0x00, 2);
+	src[0] = BOOST_BINARY(01010101);
+	src[1] = BOOST_BINARY(01010100);
+	memset(dest, BOOST_BINARY(10101010), 4);
+
+	std::istringstream in3(std::string(src, (bitSize + 7)/8));
+
+	readBits(in3, dest, bitSize, bitOffset, BITWISE_OR);
+
+	EXPECT_EQ((char)BOOST_BINARY(10101111), dest[0]);
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(11101010), dest[2]);
+	EXPECT_EQ((char)BOOST_BINARY(10101010), dest[3]);
+}
+
+TEST(readBitsTest, andBits)
+{
+	// NOTE!: In the binary diagrams below, the rightmost bit of each byte
+	// is the LSB.
+
+	size_t bitSize, bitOffset;
+	char src[2];
+	char dest[4];
+
+	// SUBTEST: src size < 1 byte
+	//
+	// src             = 101
+	// bit offset      = 9
+	// dest            = 11111111 11111111 11111111 11111111
+	// expected result = 11111111 11011111 11111111 11111111
+
+	bitSize = 3;
+	bitOffset = 9;
+	memset(src, 0x00, 2);
+	src[0] = BOOST_BINARY(10100000);
+	memset(dest, BOOST_BINARY(11111111), 4);
+
+	std::istringstream in1(std::string(src, (bitSize + 7)/8));
+
+	readBits(in1, dest, bitSize, bitOffset, BITWISE_AND);
+
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[0]);
+	EXPECT_EQ((char)BOOST_BINARY(11011111), dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[2]);
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[3]);
+
+	// SUBTEST: byte-aligned with partial last byte
+	//
+	// src             = 01010101 010
+	// bit offset      = 8
+	// dest            = 11111111 11111111 11111111 11111111
+	// expected result = 11111111 01010101 01011111 11111111
+
+	bitSize = 11;
+	bitOffset = 8;
+	memset(src, 0x00, 2);
+	src[0] = BOOST_BINARY(01010101);
+	src[1] = BOOST_BINARY(01000000);
+	memset(dest, BOOST_BINARY(11111111), 4);
+
+	std::istringstream in2(std::string(src, (bitSize + 7)/8));
+
+	readBits(in2, dest, bitSize, bitOffset, BITWISE_AND);
+
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[0]);
+	EXPECT_EQ((char)BOOST_BINARY(01010101), dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(01011111), dest[2]);
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[3]);
+
+	// SUBTEST: not byte-aligned, src size > 1 byte
+	//
+	// src             = 01010101 0101010
+	// bit offset      = 4
+	// dest            = 11111111 11111111 11111111 11111111
+	// expected result = 11110101 01010101 01011111 11111111
+
+	bitSize = 15;
+	bitOffset = 4;
+	memset(src, 0x00, 2);
+	src[0] = BOOST_BINARY(01010101);
+	src[1] = BOOST_BINARY(01010100);
+	memset(dest, BOOST_BINARY(11111111), 4);
+
+	std::istringstream in3(std::string(src, (bitSize + 7)/8));
+
+	readBits(in3, dest, bitSize, bitOffset, BITWISE_AND);
+
+	EXPECT_EQ((char)BOOST_BINARY(11110101), dest[0]);
+	EXPECT_EQ((char)BOOST_BINARY(01010101), dest[1]);
+	EXPECT_EQ((char)BOOST_BINARY(01011111), dest[2]);
+	EXPECT_EQ((char)BOOST_BINARY(11111111), dest[3]);
 }
