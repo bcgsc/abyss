f2a09d719f427b398c4d4a9286ac230ccc8a5f09
Shaun Jackman <sjackman@gmail.com>
2014-10-16 18:02:07 -0700

    Remove Assembly/SeqExt.cc

--- Assembly/SeqExt.h
+++ Assembly/SeqExt.h
@@ -90,8 +90,18 @@ class SeqExt
 			return m_record > 0 && powerOfTwo;
 		}
 
-		/** Return the complementary adjacency. */
-		SeqExt complement() const;
+		/** Return the complementary adjacency.
+		 * If the assembly is in colour space, this is a no-op.
+		 */
+		SeqExt complement() const
+		{
+			static const uint8_t complements[16] = {
+				0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe,
+				0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf
+			};
+			assert(m_record < 1 << NUM);
+			return opt::colourSpace ? *this : mask(complements[m_record]);
+		}
 
 		friend std::ostream& operator <<(std::ostream& out,
 				const SeqExt& o)
