213427240240fd121b0576e4ec074bd00320b6bf
Shaun Jackman <sjackman@gmail.com>
2015-02-18 12:02:45 -0800

    PairedDBG/BranchRecord: Disable ambiguity warning
    Output the warning when verbose is at least -vv.

--- PairedDBG/BranchRecord.h
+++ PairedDBG/BranchRecord.h
@@ -1,6 +1,8 @@
 #ifndef PAIREDDBG_BRANCHRECORD_H
 #define PAIREDDBG_BRANCHRECORD_H 1
 
+#include "Common/Options.h" // for opt::verbose
+
 /** Generate the sequence of this contig. */
 template <typename It, typename OutIt>
 void branchRecordToStr(It it, It last, OutIt out)
@@ -17,14 +19,16 @@ void branchRecordToStr(It it, It last, OutIt out)
 			*outa = x.first;
 		} else {
 			char amb = ambiguityOr(*outa, x.first);
-			std::cerr << "Warning: Expected '" << *outa
-				<< "' and saw '" << x.first
-				<< "' at " << outa - out << '\n';
-			std::copy(out, outb, std::ostream_iterator<char>(std::cerr));
-			std::cerr << '\n'
-				<< std::string(outa - out - Kmer::length() + 1, ' ')
-				<< it->first.str() << '\n'
-				<< std::string(outa - out, ' ') << amb << '\n';
+			if (opt::verbose > 1) {
+				std::cerr << "Warning: Expected '" << *outa
+					<< "' and saw '" << x.first
+					<< "' at " << outa - out << '\n';
+				std::copy(out, outb, std::ostream_iterator<char>(std::cerr));
+				std::cerr << '\n'
+					<< std::string(outa - out - Kmer::length() + 1, ' ')
+					<< it->first.str() << '\n'
+					<< std::string(outa - out, ' ') << amb << '\n';
+			}
 			*outa = amb;
 		}
 		++outa;
