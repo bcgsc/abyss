b5480e0d00e1a69e7330997d87b29b0c1af4ae85
Ben Vandervalk <benv@bcgsc.ca>
2015-03-05 15:32:23 -0800

    konnector: remove debugging statement

--- Konnector/konnector.cc
+++ Konnector/konnector.cc
@@ -671,8 +671,6 @@ static void connectPairs(const Graph& g,
 #pragma omp critical(in)
 		good = in >> a >> b;
 		if (good) {
-#pragma omp critical(cerr)
-			std::cerr << "processing pair: " << a.id << std::endl;
 			connectPair(g, a, b, params, mergedStream, read1Stream,
 				read2Stream, traceStream);
 #pragma omp atomic
