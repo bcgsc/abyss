63d9670cd84752aa3e522257b45827b58fb47fff
Ben Vandervalk <benv@bcgsc.ca>
2015-06-22 11:56:58 -0700

    Bloom/bloom.cc: fix whitespace

--- Bloom/bloom.cc
+++ Bloom/bloom.cc
@@ -127,15 +127,15 @@ namespace opt {
 	/** Number of windows in complete bloom filter.
 	  ("N" for -w option) */
 	unsigned windows = 0;
-    
-    /* Method for similarity or distance calculation.
-     -m option
-     */
-    string method("jaccard");
-    /* Inverse option to retrieve kmers which are not
-     in the filter
-     */
-    bool inverse = false;
+
+	/* Method for similarity or distance calculation.
+	 -m option
+	 */
+	string method("jaccard");
+	/* Inverse option to retrieve kmers which are not
+	 in the filter
+	 */
+	bool inverse = false;
 }
 
 static const char shortopts[] = "b:B:j:k:l:L:m:n:q:rvw:";
@@ -143,26 +143,26 @@ static const char shortopts[] = "b:B:j:k:l:L:m:n:q:rvw:";
 enum { OPT_HELP = 1, OPT_VERSION };
 
 static const struct option longopts[] = {
-	{ "bloom-size",       required_argument, NULL, 'b' },
-	{ "buffer-size",      required_argument, NULL, 'B' },
-	{ "threads",          required_argument, NULL, 'j' },
-	{ "kmer",             required_argument, NULL, 'k' },
-	{ "levels",           required_argument, NULL, 'l' },
-	{ "init-level",       required_argument, NULL, 'L' },
-	{ "chastity",         no_argument, &opt::chastityFilter, 1 },
-	{ "no-chastity",      no_argument, &opt::chastityFilter, 0 },
-	{ "trim-masked",      no_argument, &opt::trimMasked, 1 },
+	{ "bloom-size",		  required_argument, NULL, 'b' },
+	{ "buffer-size",	  required_argument, NULL, 'B' },
+	{ "threads",		  required_argument, NULL, 'j' },
+	{ "kmer",			  required_argument, NULL, 'k' },
+	{ "levels",			  required_argument, NULL, 'l' },
+	{ "init-level",		  required_argument, NULL, 'L' },
+	{ "chastity",		  no_argument, &opt::chastityFilter, 1 },
+	{ "no-chastity",	  no_argument, &opt::chastityFilter, 0 },
+	{ "trim-masked",	  no_argument, &opt::trimMasked, 1 },
 	{ "no-trim-masked",   no_argument, &opt::trimMasked, 0 },
-	{ "num-locks",        required_argument, NULL, 'n' },
-	{ "trim-quality",     required_argument, NULL, 'q' },
+	{ "num-locks",		  required_argument, NULL, 'n' },
+	{ "trim-quality",	  required_argument, NULL, 'q' },
 	{ "standard-quality", no_argument, &opt::qualityOffset, 33 },
 	{ "illumina-quality", no_argument, &opt::qualityOffset, 64 },
-	{ "verbose",          no_argument, NULL, 'v' },
-	{ "help",             no_argument, NULL, OPT_HELP },
-	{ "version",          no_argument, NULL, OPT_VERSION },
-	{ "window",           required_argument, NULL, 'w' },
-    { "method",           required_argument, NULL, 'm' },
-    { "inverse",           required_argument, NULL, 'r' },
+	{ "verbose",		  no_argument, NULL, 'v' },
+	{ "help",			  no_argument, NULL, OPT_HELP },
+	{ "version",		  no_argument, NULL, OPT_VERSION },
+	{ "window",			  required_argument, NULL, 'w' },
+	{ "method",			  required_argument, NULL, 'm' },
+	{ "inverse",		   required_argument, NULL, 'r' },
 	{ NULL, 0, NULL, 0 }
 };
 
@@ -568,236 +568,236 @@ int info(int argc, char** argv)
 }
 
 int compare(int argc, char ** argv){
-    parseGlobalOpts(argc, argv);
-    // Arg parser to get `m' option in case set
-    for (int c; (c = getopt_long(argc, argv,
-                                 shortopts, longopts, NULL)) != -1;) {
-        istringstream arg(optarg != NULL ? optarg : "");
-        switch (c) {
-            case '?':
-                dieWithUsageError();
-            case 'm':
-                arg >> opt::method; break;
-                break;
-        }
-        if (optarg != NULL && (!arg.eof() || arg.fail())) {
-            cerr << PROGRAM ": invalid option: `-"
-            << (char)c << optarg << "'\n";
-            exit(EXIT_FAILURE);
-        }
-        if (opt::method != "jaccard" && opt::method != "czekanowski" && opt::method != "forbes")
-            std::cerr << "Invalid method: " << opt::method << std::endl;
-    }
-    
-    
-    // Set method strin
-    string method(opt::method);
-    if (opt::verbose)
-    std::cerr << "Computing distance for 2"
-              << " samples...\n";
-    // Get both paths and open istreams
-    BloomFilter bloomA;
-    string pathA(argv[optind]);
-    BloomFilter bloomB;
-    string pathB(argv[optind+1]);
-    if (opt::verbose)
-      std::cerr << "Loading bloom filters from "
+	parseGlobalOpts(argc, argv);
+	// Arg parser to get `m' option in case set
+	for (int c; (c = getopt_long(argc, argv,
+								 shortopts, longopts, NULL)) != -1;) {
+		istringstream arg(optarg != NULL ? optarg : "");
+		switch (c) {
+			case '?':
+				dieWithUsageError();
+			case 'm':
+				arg >> opt::method; break;
+				break;
+		}
+		if (optarg != NULL && (!arg.eof() || arg.fail())) {
+			cerr << PROGRAM ": invalid option: `-"
+			<< (char)c << optarg << "'\n";
+			exit(EXIT_FAILURE);
+		}
+		if (opt::method != "jaccard" && opt::method != "czekanowski" && opt::method != "forbes")
+			std::cerr << "Invalid method: " << opt::method << std::endl;
+	}
+
+
+	// Set method strin
+	string method(opt::method);
+	if (opt::verbose)
+	std::cerr << "Computing distance for 2"
+			  << " samples...\n";
+	// Get both paths and open istreams
+	BloomFilter bloomA;
+	string pathA(argv[optind]);
+	BloomFilter bloomB;
+	string pathB(argv[optind+1]);
+	if (opt::verbose)
+	  std::cerr << "Loading bloom filters from "
 		<< pathA << " and " << pathB << "...\n";
-    istream* inA = openInputStream(pathA);
-    istream* inB = openInputStream(pathB);
-    // Assert state of streams
-    assert_good(*inA, pathA);
-    assert_good(*inB, pathB);
-    // Not sure this conversion is needed, check docs
-    std::istream & tA = *inA;
-    std::istream & tB = *inB;
-    // Need to read header for bit start and end info
-    Bloom::FileHeader headerA = Bloom::readHeader(tA);
-    Bloom::FileHeader headerB = Bloom::readHeader(tB);
-    // Need to assert after every read operation
-    assert(tA);
-    assert(tB);
-    
-    const size_t IO_BUFFER_SIZE = 32 * 1024;
-    unsigned char mask = 1;
-    // The number of total bits in the vector
-    size_t bitsA = headerA.endBitPos - headerA.startBitPos + 1;
-    size_t bitsB = headerB.endBitPos - headerB.startBitPos + 1;
-    // They need to be the same size to be comparable
-    if(bitsA != bitsB ) {
-        std::cerr << "Bit sizes of arrays not equal" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    if (opt::verbose)
-    std::cerr << "Bits: " << bitsA << std::endl;
-    /* As in Choi et al. (2010), 
-     a - cases where both bits are set (1/1)
-     b - cases where bits are set in the first but nor the second (1/0)
-     c - cases where bits are set in the second but not the first (0/1)
-     d - cases where bits are not set in either (0/0)
-     */
-    unsigned long a = 0;
-    unsigned long b = 0;
-    unsigned long c = 0;
-    unsigned long d = 0;
-    // Iteratively compare bits
-    for(size_t i = 0; i < bitsA;){
-        char bufferA[IO_BUFFER_SIZE];
-        char bufferB[IO_BUFFER_SIZE];
-        // The number of bits in the buffer is its size * 8 except for the last iteration
-        size_t bitsRead = std::min(IO_BUFFER_SIZE * 8, bitsA - i);
-        size_t bytesRead = (bitsRead + 7)/8;
-        // Read bytes from the the istream and immediately assert
-        tA.read(bufferA, bytesRead);
-        tB.read(bufferB, bytesRead);
-        assert(tA);
-        assert(tB);
-        // For each byte in the buffer, compare bits
-        for(size_t j = 0; j < IO_BUFFER_SIZE; j++){
-            // Compare bit-wise
-            for(int bit = 0; bit < 8; bit++){
-                bool f = (bufferA[j] & (mask << bit)) != 0;
-                bool s = (bufferB[j] & (mask << bit)) != 0;
-                if( f == 1 && s == 1 ) {
-                    a++;
-                } else if( f == 1 && s == 0) {
-                    b++;
-                } else if( f == 0 && s == 1) {
-                    c++;
-                } else d++;
-            }
-        }
-        i += bitsRead;
-    }
-    assert(tA);
-    assert(tB);
-    // Result output:
-    std::cout << "1/1: " << a << "\n1/0: " << b << "\n0/1: " << c << "\n0/0: " << d << std::endl;
-    if(method == "jaccard"){
-        float Dist = (float)a/(float)(a+b+c);
-        std::cout << "Jaccard similarity: " << Dist << std::endl;
-    }
-    if(method == "czekanowski"){
-        float Dist = (2*(float)a)/(float)((2*a)+b+c);
-        std::cout << "Czekanowski similarity: " << Dist << std::endl;
-    }
-    if(method == "forbes"){
-        float n = (float)(a + b + c + d);
-        float Dist = (n*a - ((a+b)*(a+c))) / (n*std::min(a+b,a+c) - ((a+b) * (a+c)));
-        std::cout << "Forbes similarity: " << Dist << std::endl;
-    }
-    // Check and clean up
-    assert_good(tA, pathA);
-    assert_good(tA, pathB);
-    closeInputStream(inA, pathA);
-    closeInputStream(inB, pathB);
+	istream* inA = openInputStream(pathA);
+	istream* inB = openInputStream(pathB);
+	// Assert state of streams
+	assert_good(*inA, pathA);
+	assert_good(*inB, pathB);
+	// Not sure this conversion is needed, check docs
+	std::istream & tA = *inA;
+	std::istream & tB = *inB;
+	// Need to read header for bit start and end info
+	Bloom::FileHeader headerA = Bloom::readHeader(tA);
+	Bloom::FileHeader headerB = Bloom::readHeader(tB);
+	// Need to assert after every read operation
+	assert(tA);
+	assert(tB);
+
+	const size_t IO_BUFFER_SIZE = 32 * 1024;
+	unsigned char mask = 1;
+	// The number of total bits in the vector
+	size_t bitsA = headerA.endBitPos - headerA.startBitPos + 1;
+	size_t bitsB = headerB.endBitPos - headerB.startBitPos + 1;
+	// They need to be the same size to be comparable
+	if(bitsA != bitsB ) {
+		std::cerr << "Bit sizes of arrays not equal" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if (opt::verbose)
+	std::cerr << "Bits: " << bitsA << std::endl;
+	/* As in Choi et al. (2010),
+	 a - cases where both bits are set (1/1)
+	 b - cases where bits are set in the first but nor the second (1/0)
+	 c - cases where bits are set in the second but not the first (0/1)
+	 d - cases where bits are not set in either (0/0)
+	 */
+	unsigned long a = 0;
+	unsigned long b = 0;
+	unsigned long c = 0;
+	unsigned long d = 0;
+	// Iteratively compare bits
+	for(size_t i = 0; i < bitsA;){
+		char bufferA[IO_BUFFER_SIZE];
+		char bufferB[IO_BUFFER_SIZE];
+		// The number of bits in the buffer is its size * 8 except for the last iteration
+		size_t bitsRead = std::min(IO_BUFFER_SIZE * 8, bitsA - i);
+		size_t bytesRead = (bitsRead + 7)/8;
+		// Read bytes from the the istream and immediately assert
+		tA.read(bufferA, bytesRead);
+		tB.read(bufferB, bytesRead);
+		assert(tA);
+		assert(tB);
+		// For each byte in the buffer, compare bits
+		for(size_t j = 0; j < IO_BUFFER_SIZE; j++){
+			// Compare bit-wise
+			for(int bit = 0; bit < 8; bit++){
+				bool f = (bufferA[j] & (mask << bit)) != 0;
+				bool s = (bufferB[j] & (mask << bit)) != 0;
+				if( f == 1 && s == 1 ) {
+					a++;
+				} else if( f == 1 && s == 0) {
+					b++;
+				} else if( f == 0 && s == 1) {
+					c++;
+				} else d++;
+			}
+		}
+		i += bitsRead;
+	}
+	assert(tA);
+	assert(tB);
+	// Result output:
+	std::cout << "1/1: " << a << "\n1/0: " << b << "\n0/1: " << c << "\n0/0: " << d << std::endl;
+	if(method == "jaccard"){
+		float Dist = (float)a/(float)(a+b+c);
+		std::cout << "Jaccard similarity: " << Dist << std::endl;
+	}
+	if(method == "czekanowski"){
+		float Dist = (2*(float)a)/(float)((2*a)+b+c);
+		std::cout << "Czekanowski similarity: " << Dist << std::endl;
+	}
+	if(method == "forbes"){
+		float n = (float)(a + b + c + d);
+		float Dist = (n*a - ((a+b)*(a+c))) / (n*std::min(a+b,a+c) - ((a+b) * (a+c)));
+		std::cout << "Forbes similarity: " << Dist << std::endl;
+	}
+	// Check and clean up
+	assert_good(tA, pathA);
+	assert_good(tA, pathB);
+	closeInputStream(inA, pathA);
+	closeInputStream(inB, pathB);
 
   return 1;
 }
 
 int memberOf(int argc, char ** argv){
-    // Initalise bloom and get globals
-    BloomFilter bloom;
-    parseGlobalOpts(argc, argv);
-    // Arg parser to get `m' option in case set
-    for (int c; (c = getopt_long(argc, argv,
-                                 shortopts, longopts, NULL)) != -1;) {
-        istringstream arg(optarg != NULL ? optarg : "");
-        switch (c) {
-            case '?':
-                dieWithUsageError();
-            case 'r':
-                opt::inverse = true; break;
-                break;
-        }
-        if (optarg != NULL && (!arg.eof() || arg.fail())) {
-            cerr << PROGRAM ": invalid option: `-"
-            << (char)c << optarg << "'\n";
-            exit(EXIT_FAILURE);
-        }
-    }
-    string path = argv[optind];
-    string fasta = argv[++optind];
-    unsigned k = opt::k;
-    size_t taskIOBufferSize = 100000;
-    if (opt::verbose)
-        std::cerr << "Loading bloom filter from `"
-        << path << "'...\n";
-    
-    istream* in = openInputStream(path);
-    assert_good(*in, path);
-    *in >> bloom;
-    
-    // Lifted from Bloom.h `loadFilter'
-    assert(!fasta.empty());
-    if (opt::verbose)
-        std::cerr << "Reading `" << fasta << "'...\n";
-    FastaReader _in(fasta.c_str(), FastaReader::FOLD_CASE);
-    
-    /* For each for-loop, create am iterative to keep track
-     of kmers and also create FASTA headers for the output
-     */
-    unsigned long fcount = 0;
-    
-    for (std::vector<std::string> buffer(taskIOBufferSize);;) {
-        fcount++;
-        buffer.clear();
-        size_t bufferSize = 0;
-        bool good = true;
-        
-        for (; good && bufferSize < taskIOBufferSize;) {
-            
-            std::string seq;
-            good = _in >> seq;
-            
-            if (good) {
-                buffer.push_back(seq);
-                bufferSize += seq.length();
-            }
-        }
-        
-        if (buffer.size() == 0)
-            break;
-        
-        unsigned long bcount = 0;
-        
-        for (size_t j = 0; j < buffer.size(); j++) {
-            bcount++;
-            
-            unsigned long kcount = 0;
-            
-            const std::string& _seq = buffer.at(j);
-            //std::cerr << "Seq " << _seq << std::endl;
-            if (_seq.size() < k)
-                continue;
-            for (size_t i = 0; i < _seq.size() - k + 1; ++i) {
-                kcount++;
-                std::string kmer = _seq.substr(i, k);
-                
-                size_t pos = kmer.find_last_not_of("ACGTacgt");
-                
-                if (pos == std::string::npos) {
-                    if(!opt::inverse){
-                        if (bloom[Kmer(kmer)]){
-                            std::cout << ">kmer:" << fcount << "_" << bcount << "_"
-                            << kcount << std::endl;
-                            std::cout << kmer << std::endl;
-                        }
-                    } else {
-                        if (!bloom[Kmer(kmer)]){
-                            std::cout << ">kmer:" << fcount << "_" << bcount << "_"
-                            << kcount << std::endl;
-                            std::cout << kmer << std::endl;
-                        }
-                    }
-                } else
-                    i += pos;
-            }
-        }
-    }
-    assert(_in.eof());
-    
-    return 1;
+	// Initalise bloom and get globals
+	BloomFilter bloom;
+	parseGlobalOpts(argc, argv);
+	// Arg parser to get `m' option in case set
+	for (int c; (c = getopt_long(argc, argv,
+								 shortopts, longopts, NULL)) != -1;) {
+		istringstream arg(optarg != NULL ? optarg : "");
+		switch (c) {
+			case '?':
+				dieWithUsageError();
+			case 'r':
+				opt::inverse = true; break;
+				break;
+		}
+		if (optarg != NULL && (!arg.eof() || arg.fail())) {
+			cerr << PROGRAM ": invalid option: `-"
+			<< (char)c << optarg << "'\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+	string path = argv[optind];
+	string fasta = argv[++optind];
+	unsigned k = opt::k;
+	size_t taskIOBufferSize = 100000;
+	if (opt::verbose)
+		std::cerr << "Loading bloom filter from `"
+		<< path << "'...\n";
+
+	istream* in = openInputStream(path);
+	assert_good(*in, path);
+	*in >> bloom;
+
+	// Lifted from Bloom.h `loadFilter'
+	assert(!fasta.empty());
+	if (opt::verbose)
+		std::cerr << "Reading `" << fasta << "'...\n";
+	FastaReader _in(fasta.c_str(), FastaReader::FOLD_CASE);
+
+	/* For each for-loop, create am iterative to keep track
+	 of kmers and also create FASTA headers for the output
+	 */
+	unsigned long fcount = 0;
+
+	for (std::vector<std::string> buffer(taskIOBufferSize);;) {
+		fcount++;
+		buffer.clear();
+		size_t bufferSize = 0;
+		bool good = true;
+
+		for (; good && bufferSize < taskIOBufferSize;) {
+
+			std::string seq;
+			good = _in >> seq;
+
+			if (good) {
+				buffer.push_back(seq);
+				bufferSize += seq.length();
+			}
+		}
+
+		if (buffer.size() == 0)
+			break;
+
+		unsigned long bcount = 0;
+
+		for (size_t j = 0; j < buffer.size(); j++) {
+			bcount++;
+
+			unsigned long kcount = 0;
+
+			const std::string& _seq = buffer.at(j);
+			//std::cerr << "Seq " << _seq << std::endl;
+			if (_seq.size() < k)
+				continue;
+			for (size_t i = 0; i < _seq.size() - k + 1; ++i) {
+				kcount++;
+				std::string kmer = _seq.substr(i, k);
+
+				size_t pos = kmer.find_last_not_of("ACGTacgt");
+
+				if (pos == std::string::npos) {
+					if(!opt::inverse){
+						if (bloom[Kmer(kmer)]){
+							std::cout << ">kmer:" << fcount << "_" << bcount << "_"
+							<< kcount << std::endl;
+							std::cout << kmer << std::endl;
+						}
+					} else {
+						if (!bloom[Kmer(kmer)]){
+							std::cout << ">kmer:" << fcount << "_" << bcount << "_"
+							<< kcount << std::endl;
+							std::cout << kmer << std::endl;
+						}
+					}
+				} else
+					i += pos;
+			}
+		}
+	}
+	assert(_in.eof());
+
+	return 1;
 }
 
 int main(int argc, char** argv)
@@ -829,11 +829,11 @@ int main(int argc, char** argv)
 		return info(argc, argv);
 	}
 	else if (command == "compare") {
-	        return compare(argc, argv);
-        }
-    else if (command == "getKmers") {
-        return memberOf(argc, argv);
-    }
+		return compare(argc, argv);
+	}
+	else if (command == "getKmers") {
+		return memberOf(argc, argv);
+	}
 
 	dieWithUsageError();
 }
