652d1f5a76e705a4655b745cd4dcd6d8007a5578
Ben Vandervalk <benv@bcgsc.ca>
2015-11-19 17:35:40 -0800

    lib/bloomfilter: rolling hash Bloom filter

---
+++ lib/bloomfilter-521e80c5c619a9a8e3d6389dc3b597a75bdf2aaa/BloomFilter.hpp
@@ -0,0 +1,303 @@
+/*
+ *
+ * BloomFilter.hpp
+ *
+ *  Created on: Aug 10, 2012
+ *      Author: cjustin
+ */
+
+#ifndef BLOOMFILTER_H_
+#define BLOOMFILTER_H_
+#include <string>
+#include <vector>
+#include <stdint.h>
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <sys/stat.h>
+#include <cstring>
+#include <cassert>
+#include <cstdlib>
+#include <stdio.h>
+#include <cstring>
+#include "rolling.h"
+
+using namespace std;
+
+static const uint8_t bitsPerChar = 0x08;
+static const unsigned char bitMask[0x08] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20,
+    0x40, 0x80 };
+
+inline unsigned popCnt(unsigned char x) {
+    return ((0x876543210 >>
+             (((0x4332322132212110 >> ((x & 0xF) << 2)) & 0xF) << 2)) >>
+            ((0x4332322132212110 >> (((x & 0xF0) >> 2)) & 0xF) << 2))
+    & 0xf;
+}
+
+class BloomFilter {
+public:
+    /* De novo filter constructor.
+     *
+     * preconditions:
+     * filterSize must be a multiple of 64
+     * kmerSize refers to the number of bases the kmer has
+     * k-mers supplied to this object should be binary (2 bits per base)
+     */
+    BloomFilter(size_t filterSize, unsigned hashNum, unsigned kmerSize) :
+    m_size(filterSize), m_hashNum(hashNum), m_kmerSize(kmerSize) {
+        initSize(m_size);
+        memset(m_filter, 0, m_sizeInBytes);
+    }
+    
+    /*
+     * Loads the filter (file is a .bf file) from path specified
+     */
+    BloomFilter(size_t filterSize, unsigned hashNum, unsigned kmerSize,
+                string const &filterFilePath) :
+    m_size(filterSize), m_hashNum(hashNum), m_kmerSize(kmerSize) {
+        initSize(m_size);
+        
+        FILE *file = fopen(filterFilePath.c_str(), "rb");
+        if (file == NULL) {
+            cerr << "file \"" << filterFilePath << "\" could not be read."
+            << endl;
+            exit(1);
+        }
+        
+        long int lCurPos = ftell(file);
+        fseek(file, 0, 2);
+        size_t fileSize = ftell(file);
+        fseek(file, lCurPos, 0);
+        if (fileSize != m_sizeInBytes) {
+            cerr << "Error: " << filterFilePath
+            << " does not match size given by its information file. Size: "
+            << fileSize << " vs " << m_sizeInBytes << " bytes." << endl;
+            exit(1);
+        }
+        
+        size_t countRead = fread(m_filter, fileSize, 1, file);
+        if (countRead != 1 && fclose(file) != 0) {
+            cerr << "file \"" << filterFilePath << "\" could not be read."
+            << endl;
+            exit(1);
+        }
+    }
+    
+    /*
+     * For precomputing hash values. kmerSize is the number of bytes of the original string used.
+     */
+    vector<size_t> multiHash(const char* kmer) const {
+        vector<size_t> tempHashValues(m_hashNum);
+        uint64_t hVal = getChval(kmer, m_kmerSize);
+        for (size_t i = 0; i < m_hashNum; ++i) {
+            tempHashValues[i] = (rol(varSeed, i) ^ hVal);
+        }
+        return tempHashValues;
+    }
+    
+    /*
+     * For precomputing hash values. kmerSize is the number of bytes of the original string used.
+     */
+    vector<size_t> multiHash(const char * kmer, uint64_t& fhVal,
+                             uint64_t& rhVal) const {
+        vector<size_t> tempHashValues(m_hashNum);
+        fhVal = getFhval(kmer, m_kmerSize);
+        rhVal = getRhval(kmer, m_kmerSize);
+        uint64_t hVal = (rhVal < fhVal) ? rhVal : fhVal;
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            tempHashValues[i] = (rol(varSeed, i) ^ hVal);
+        }
+        return tempHashValues;
+    }
+    
+    /*
+     * For precomputing hash values. kmerSize is the number of bytes of the original string used.
+     */
+    vector<size_t> multiHash(uint64_t& fhVal, uint64_t& rhVal,
+                             const char charOut, const char charIn) const {
+        vector<size_t> tempHashValues(m_hashNum);
+        fhVal = rol(fhVal, 1)
+        ^ rol(seedTab[(unsigned char) charOut], m_kmerSize)
+        ^ seedTab[(unsigned char) charIn];
+        rhVal = ror(rhVal, 1)
+        ^ ror(seedTab[(unsigned char) (charOut + cpOff)], 1)
+        ^ rol(seedTab[(unsigned char) (charIn + cpOff)],
+              m_kmerSize - 1);
+        uint64_t hVal = (rhVal < fhVal) ? rhVal : fhVal;
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            tempHashValues[i] = (rol(varSeed, i) ^ hVal);
+        }
+        return tempHashValues;
+    }
+    
+    /*
+     * Accepts a list of precomputed hash values. Faster than rehashing each time.
+     */
+    void insert(vector<size_t> const &precomputed) {
+        
+        //iterates through hashed values adding it to the filter
+        for (size_t i = 0; i < m_hashNum; ++i) {
+            size_t normalizedValue = precomputed.at(i) % m_size;
+            __sync_or_and_fetch(&m_filter[normalizedValue / bitsPerChar],
+                                bitMask[normalizedValue % bitsPerChar]);
+            //		m_filter[normalizedValue / bitsPerChar] |= bitMask[normalizedValue
+            //				% bitsPerChar];
+        }
+    }
+    
+    void insert(const char* kmer) {
+        uint64_t hVal = getChval(kmer, m_kmerSize);
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            size_t hLoc = (rol(varSeed, i) ^ hVal) % m_size;
+            __sync_or_and_fetch(&m_filter[hLoc / 8], (1 << (7 - hLoc % 8)));
+        }
+    }
+    
+    void insert(const char * kmer, uint64_t& fhVal, uint64_t& rhVal) {
+        fhVal = getFhval(kmer, m_kmerSize);
+        rhVal = getRhval(kmer, m_kmerSize);
+        uint64_t hVal = (rhVal < fhVal) ? rhVal : fhVal;
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            size_t hLoc = (rol(varSeed, i) ^ hVal) % m_size;
+            __sync_or_and_fetch(&m_filter[hLoc / 8], (1 << (7 - hLoc % 8)));
+        }
+    }
+    
+    void insert(uint64_t& fhVal, uint64_t& rhVal, const char charOut,
+                const char charIn) {
+        fhVal = rol(fhVal, 1)
+        ^ rol(seedTab[(unsigned char) charOut], m_kmerSize)
+        ^ seedTab[(unsigned char) charIn];
+        rhVal = ror(rhVal, 1)
+        ^ ror(seedTab[(unsigned char) (charOut + cpOff)], 1)
+        ^ rol(seedTab[(unsigned char) (charIn + cpOff)],
+              m_kmerSize - 1);
+        uint64_t hVal = (rhVal < fhVal) ? rhVal : fhVal;
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            size_t hLoc = (rol(varSeed, i) ^ hVal) % m_size;
+            __sync_or_and_fetch(&m_filter[hLoc / 8], (1 << (7 - hLoc % 8)));
+        }
+    }
+    
+    /*
+     * Accepts a list of precomputed hash values. Faster than rehashing each time.
+     */
+    bool contains(vector<size_t> const &values) const {
+        for (size_t i = 0; i < m_hashNum; ++i) {
+            size_t normalizedValue = values.at(i) % m_size;
+            unsigned char bit = bitMask[normalizedValue % bitsPerChar];
+            if ((m_filter[normalizedValue / bitsPerChar] & bit) != bit) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    /*
+     * Single pass filtering, computes hash values on the fly
+     */
+    bool contains(const char* kmer) const {
+        uint64_t hVal = getChval(kmer, m_kmerSize);
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            size_t hLoc = (rol(varSeed, i) ^ hVal) % m_size;
+            if ((m_filter[hLoc / 8] & (1 << (7 - hLoc % 8))) == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    bool contains(const char * kmer, uint64_t& fhVal, uint64_t& rhVal) {
+        fhVal = getFhval(kmer, m_kmerSize);
+        rhVal = getRhval(kmer, m_kmerSize);
+        uint64_t hVal = (rhVal < fhVal) ? rhVal : fhVal;
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            size_t hLoc = (rol(varSeed, i) ^ hVal) % m_size;
+            if ((m_filter[hLoc / 8] & (1 << (7 - hLoc % 8))) == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    bool contains(uint64_t& fhVal, uint64_t& rhVal, const char charOut,
+                  const char charIn) {
+        fhVal = rol(fhVal, 1)
+        ^ rol(seedTab[(unsigned char) charOut], m_kmerSize)
+        ^ seedTab[(unsigned char) charIn];
+        rhVal = ror(rhVal, 1)
+        ^ ror(seedTab[(unsigned char) (charOut + cpOff)], 1)
+        ^ rol(seedTab[(unsigned char) (charIn + cpOff)],
+              m_kmerSize - 1);
+        uint64_t hVal = (rhVal < fhVal) ? rhVal : fhVal;
+        for (unsigned i = 0; i < m_hashNum; i++) {
+            size_t hLoc = (rol(varSeed, i) ^ hVal) % m_size;
+            if ((m_filter[hLoc / 8] & (1 << (7 - hLoc % 8))) == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    /*
+     * Stores the filter as a binary file to the path specified
+     * Stores uncompressed because the random data tends to
+     * compress poorly anyway
+     */
+    void storeFilter(string const &filterFilePath) const {
+        ofstream myFile(filterFilePath.c_str(), ios::out | ios::binary);
+        
+        cerr << "Storing filter. Filter is " << m_sizeInBytes << "bytes."
+        << endl;
+        
+        assert(myFile);
+        //write out each block
+        myFile.write(reinterpret_cast<char*>(m_filter), m_sizeInBytes);
+        
+        myFile.close();
+        assert(myFile);
+    }
+    
+    size_t getPop() const {
+        size_t i, popBF=0;
+#pragma omp parallel for reduction(+:popBF)
+        for(i=0; i<(m_size + 7)/8; i++)
+            popBF = popBF + popCnt(m_filter[i]);
+        return popBF;
+    }
+    
+    unsigned getHashNum() const {
+        return m_hashNum;
+    }
+    
+    unsigned getKmerSize() const {
+        return m_kmerSize;
+    }
+    
+    size_t getFilterSize() const { return m_size; }
+
+    ~BloomFilter() {
+        delete[] m_filter;
+    }
+private:
+    BloomFilter(const BloomFilter& that); //to prevent copy construction
+    
+    /*
+     * Checks filter size and initializes filter
+     */
+    void initSize(size_t size) {
+        if (size % 8 != 0) {
+            cerr << "ERROR: Filter Size \"" << size
+            << "\" is not a multiple of 8." << endl;
+            exit(1);
+        }
+        m_sizeInBytes = size / bitsPerChar;
+        m_filter = new unsigned char[m_sizeInBytes];
+    }
+    
+    uint8_t* m_filter;
+    size_t m_size;
+    size_t m_sizeInBytes;
+    unsigned m_hashNum;
+    unsigned m_kmerSize;
+};
+
+#endif /* BLOOMFILTER_H_ */
---
+++ lib/bloomfilter-521e80c5c619a9a8e3d6389dc3b597a75bdf2aaa/rolling.h
@@ -0,0 +1,105 @@
+#ifndef ROLLING_HASH_H
+#define ROLLING_HASH_H
+
+#include <stdint.h>
+
+// offset for the complement base in the random seeds table
+const int cpOff = -20;
+
+// seed for gerenerating multiple hash values
+const uint64_t varSeed = 2577914034309095328ul;
+
+// 64-bit random seed table corresponding to bases and their complements
+static const uint64_t seedTab[256] = {
+    0, 0, 0, 0, 0, 0, 0, 0, // 0..7
+    0, 0, 0, 0, 0, 0, 0, 0, // 8..15
+    0, 0, 0, 0, 0, 0, 0, 0, // 16..23
+    0, 0, 0, 0, 0, 0, 0, 0, // 24..31
+    0, 0, 0, 0, 0, 0, 0, 0, // 32..39
+    0, 0, 0, 0, 0, 2978368046464386134ul, 0, 2319985823310095140ul, // 40..47
+    0, 0, 0, 3572411708064410444ul, 0, 0, 0, 0, // 48..55
+    0, 0, 0, 0, 0, 0, 0, 0, // 56..63
+    4362857412768957556ul, 4362857412768957556ul, 0, 3572411708064410444ul, 0, 0, 0, 2319985823310095140ul, // 64..71
+    0, 0, 0, 0, 0, 2978368046464386134ul, 0, 2319985823310095140ul, // 72..79
+    0, 0, 0, 3572411708064410444ul, 2978368046464386134ul, 0, 0, 0, // 80..87
+    0, 0, 0, 0, 0, 0, 0, 0, // 88..95
+    4362857412768957556ul, 4362857412768957556ul, 0, 3572411708064410444ul, 0, 0, 0, 2319985823310095140ul, // 96..103
+    0, 0, 0, 0, 0, 0, 0, 0, // 104..111
+    0, 0, 0, 0, 2978368046464386134ul, 0, 0, 0, // 112..119
+    0, 0, 0, 0, 0, 0, 0, 0, // 120..127
+    0, 0, 0, 0, 0, 0, 0, 0, // 128..135
+    0, 0, 0, 0, 0, 0, 0, 0, // 136..143
+    0, 0, 0, 0, 0, 0, 0, 0, // 144..151
+    0, 0, 0, 0, 0, 0, 0, 0, // 152..159
+    0, 0, 0, 0, 0, 0, 0, 0, // 160..167
+    0, 0, 0, 0, 0, 0, 0, 0, // 168..175
+    0, 0, 0, 0, 0, 0, 0, 0, // 176..183
+    0, 0, 0, 0, 0, 0, 0, 0, // 184..191
+    0, 0, 0, 0, 0, 0, 0, 0, // 192..199
+    0, 0, 0, 0, 0, 0, 0, 0, // 200..207
+    0, 0, 0, 0, 0, 0, 0, 0, // 208..215
+    0, 0, 0, 0, 0, 0, 0, 0, // 216..223
+    0, 0, 0, 0, 0, 0, 0, 0, // 224..231
+    0, 0, 0, 0, 0, 0, 0, 0, // 232..239
+    0, 0, 0, 0, 0, 0, 0, 0, // 240..247
+    0, 0, 0, 0, 0, 0, 0, 0  // 248..255
+};
+
+// rotate "v" to the left by "s" positions
+inline uint64_t rol(const uint64_t v, const int s) {
+    return (v << s) | (v >> (64 - s));
+}
+
+// rotate "v" to the right by "s" positions
+inline uint64_t ror(const uint64_t v, const int s) {
+    return (v >> s) | (v << (64 - s));
+}
+
+// forward-strand hash value of the base kmer, i.e. fhval(kmer_0)
+inline uint64_t getFhval(const char * kmerSeq, const unsigned k) {
+    uint64_t hVal=0;
+    for(unsigned i=0; i<k; i++)
+        hVal ^= rol(seedTab[(unsigned char)kmerSeq[i]], k-1-i);
+    return hVal;
+}
+
+// reverse-strand hash value of the base kmer, i.e. rhval(kmer_0)
+inline uint64_t getRhval(const char * kmerSeq, const unsigned k) {
+    uint64_t hVal=0;
+    for(unsigned i=0; i<k; i++)
+        hVal ^= rol(seedTab[(unsigned char)kmerSeq[i]+cpOff], i);
+    return hVal;
+}
+
+// cannonical hash value of the base kmer, i.e. rhval(kmer_0)
+inline uint64_t getChval(const char * kmerSeq, const unsigned k) {
+    uint64_t fhVal = getFhval(kmerSeq, k);
+    uint64_t rhVal = getRhval(kmerSeq, k);
+    return (rhVal<fhVal)? rhVal : fhVal;
+}
+
+// initialize forward-strand hash value of the first kmer, i.e. fhval(kmer_0)
+inline uint64_t initHashes(const char * kmerSeq, const unsigned k) {
+    return getFhval(kmerSeq, k);
+}
+
+// initialize cannonical hash value of the first kmer, i.e. chval(kmer_0)
+inline uint64_t initHashes(const char * kmerSeq, const unsigned k, uint64_t& fhVal, uint64_t& rhVal) {
+    fhVal = getFhval(kmerSeq, k);
+    rhVal = getRhval(kmerSeq, k);
+    return (rhVal<fhVal)? rhVal : fhVal;
+}
+
+// recursive forward-strand hash value for next k-mer
+inline uint64_t rollHashesRight(const uint64_t fhVal, const unsigned char charOut, const unsigned char charIn, const unsigned k) {
+    return(rol(fhVal, 1) ^ rol(seedTab[charOut], k) ^ seedTab[charIn]);
+}
+
+// recursive cannonical hash value for next k-mer
+inline uint64_t rollHashesRight(uint64_t& fhVal, uint64_t& rhVal, const unsigned char charOut, const unsigned char charIn, const unsigned k) {
+    fhVal = rol(fhVal, 1) ^ rol(seedTab[charOut], k) ^ seedTab[charIn];
+    rhVal = ror(rhVal, 1) ^ ror(seedTab[charOut+cpOff], 1) ^ rol(seedTab[charIn+cpOff], k-1);
+    return (rhVal<fhVal)? rhVal : fhVal;
+}
+
+#endif
