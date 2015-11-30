f3be0a49f09364b2f7d517b14fd00498ec3ed42c
Ben Vandervalk <benv@bcgsc.ca>
2015-11-29 17:55:20 -0800

    abyss-bloom-dbg: add genome size and read coverage params

--- BloomDBG/bloom-dbg.cc
+++ BloomDBG/bloom-dbg.cc
@@ -1,6 +1,8 @@
 #include "config.h"
 
 #include "Common/Kmer.h"
+#include "Common/StringUtil.h"
+#include "lib/bloomfilter-9061f087d8714109b865415f2ac05e4796d0cd74/BloomFilter.hpp"
 
 #include <getopt.h>
 #include <iostream>
@@ -23,33 +25,52 @@ static const char VERSION_MESSAGE[] =
 	"Copyright 2015 Canada's Michael Smith Genome Science Centre\n";
 
 static const char USAGE_MESSAGE[] =
-"Usage: " PROGRAM " -k <kmer_size> [options]... <FASTQ> [FASTQ]... > assembly.fasta\n"
+"Usage: " PROGRAM " -k <kmer_size> -G <genome_size> -C <read_coverage> [options] \\\n"
+"    <FASTQ> [FASTQ]... > assembly.fasta\n"
+"\n"
 "Perform a de Bruijn graph assembly of the given FASTQ files.\n"
 "\n"
-" Options:\n"
+"Options:\n"
 "\n"
+"  -C, --read-coverage=N      approx read coverage [required]\n"
+"  -G, --genome-size=N        approx genome size with suffix\n"
+"                             'k', 'M', or 'G' [required]\n"
 "      --help                 display this help and exit\n"
 "  -j, --threads=N            use N parallel threads [1]\n"
-"  -k, --kmer=N               the size of a k-mer\n"
+"  -k, --kmer=N               the size of a k-mer [required]\n"
 "      --version              output version information and exit\n"
+"\n"
+"Example:\n"
+"\n"
+"  Assemble a 100 Mbp genome with 50X coverage and a k-mer size of 50bp:\n"
+"\n"
+"  $ " PROGRAM " -k50 -G100M -C50 reads1.fq.gz reads2.fq.gz > assembly.fa\n"
 "\n"
 "Report bugs to <" PACKAGE_BUGREPORT ">.\n";
 
 namespace opt {
 
+	/** Approx. read coverage. */
+	static float readCoverage;
+
+	/** Approx. genome size */
+	static double genomeSize;
+
 	/** The number of parallel threads. */
 	static unsigned threads = 1;
 
 	/** The size of a k-mer. */
-	unsigned k;
+	static unsigned k;
 
 }
 
-static const char shortopts[] = "j:k:";
+static const char shortopts[] = "C:G:j:k:";
 
 enum { OPT_HELP = 1, OPT_VERSION };
 
 static const struct option longopts[] = {
+	{ "read-coverage",    required_argument, NULL, 'C' },
+	{ "genome-size",      required_argument, NULL, 'G' },
 	{ "help",             no_argument, NULL, OPT_HELP },
 	{ "threads",          required_argument, NULL, 'j' },
 	{ "kmer",             required_argument, NULL, 'k' },
@@ -71,6 +92,10 @@ int main(int argc, char** argv)
 		switch (c) {
 		  case '?':
 			die = true; break;
+		  case 'C':
+			arg >> opt::readCoverage; break;
+		  case 'G':
+			  opt::genomeSize = fromSI(arg); break;
 		  case 'j':
 			arg >> opt::threads; break;
 		  case 'k':
