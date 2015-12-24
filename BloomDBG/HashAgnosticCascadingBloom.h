1b18430999d18d543c1b12b72343dcdb1bf87066
Ben Vandervalk <benv@bcgsc.ca>
2015-12-24 10:14:45 -0800

    bloom-dbg: reduce args list for helper functions
    Remove `k` and `numHashes` args. Get those parameters
    directly from the Bloom filter object instead.

--- BloomDBG/HashAgnosticCascadingBloom.h
+++ BloomDBG/HashAgnosticCascadingBloom.h
@@ -56,10 +56,10 @@ class HashAgnosticCascadingBloom
 	}
 
 	/** Return k-mer size used by Bloom filter. */
-	unsigned k() const { return m_k; }
+	unsigned getKmerSize() const { return m_k; }
 
 	/** Return number of hash functions used by Bloom filter */
-	unsigned numHashes() const { return m_hashes; }
+	unsigned getHashNum() const { return m_hashes; }
 
 	/** Return the size of the bit array. */
 	size_t size() const
