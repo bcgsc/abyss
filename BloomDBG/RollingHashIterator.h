be8bc0a864996cd15c5bbe3c6d67f80bca0b987e
Johnathan Wong <jowong@hpce705.hpc.bcgsc.ca>
2020-06-01 18:48:16 -0700

    RollingHashIterator.h: add option to roll backwards

--- BloomDBG/RollingHashIterator.h
+++ BloomDBG/RollingHashIterator.h
@@ -96,6 +96,73 @@ private:
 		m_pos = std::numeric_limits<std::size_t>::max();
 	}
 
+	/**
+	 * Advance iterator left to the prev valid k-mer.
+	 */
+	void prev()
+	{
+		if (m_seq.length() < m_k) {
+			m_pos = std::numeric_limits<std::size_t>::max();
+			return;
+		}
+
+		const std::string& spacedSeed = MaskedKmer::mask();
+
+		//while(m_pos < m_seq.length() - m_k + 1) {
+		while(m_pos < m_seq.length() - m_k + 1) {
+
+			/* skip k-mers with non-ACGT chars in unmasked positions */
+			while (!m_badCharPos.empty() && m_badCharPos.back() > m_pos + m_k - 1)
+				m_badCharPos.pop_back();
+
+			if (!m_badCharPos.empty() && m_badCharPos.back() >= m_pos) {
+				/* empty spaced seed is equivalent to a string of '1's */
+				if (spacedSeed.empty()) {
+					m_rollNextHash = false;
+					m_pos = m_badCharPos.back() - m_k;
+					continue;
+				}
+				bool goodKmer = true;
+				assert(spacedSeed.length() == m_k);
+				for (size_t i = m_badCharPos.size() - 1; i != std::numeric_limits<std::size_t>::max() &&
+				    m_badCharPos.at(i) >= m_pos; --i) {
+					size_t kmerPos = m_badCharPos.at(i) - m_pos;
+					if (spacedSeed.at(kmerPos) == '1') {
+						goodKmer = false;
+						break;
+					}
+				}
+				if (!goodKmer) {
+					m_rollNextHash = false;
+					--m_pos;
+					continue;
+				}
+			}
+
+			/* we are positioned at the next valid k-mer */
+
+			if (!m_rollNextHash) {
+				/* we don't have hash values for the
+				 * preceding k-mer, so we must compute
+				 * the hash values from scratch */
+				//std::cerr << m_pos << std::endl;
+				m_rollingHash.reset(m_seq.substr(m_pos, m_k));
+				m_rollNextHash = true;
+			} else {
+				/* compute new hash values based on
+				 * hash values of preceding k-mer */
+				assert(m_pos < m_seq.length() - m_k);
+				m_rollingHash.rollLeft(m_seq[m_pos], m_seq.c_str() + m_pos + 1);
+			}
+			m_rollingHash.getHashes(m_hashes);
+			return;
+
+		}
+
+		/* there are no more valid k-mers */
+		m_pos = std::numeric_limits<std::size_t>::max();
+	}
+
 public:
 
 	typedef uint64_t hash_t;
@@ -113,11 +180,17 @@ public:
 	 * @param k k-mer size
 	 * for each k-mer
 	 */
-	RollingHashIterator(const std::string& seq, unsigned numHashes, unsigned k)
+	RollingHashIterator(const std::string& seq, unsigned numHashes, unsigned k, bool back = false)
 		: m_seq(seq), m_numHashes(numHashes), m_k(k),
-		m_rollingHash(m_numHashes, m_k), m_rollNextHash(false), m_pos(0)
+		m_rollingHash(m_numHashes, m_k), m_rollNextHash(false)
 	{
-		init();
+		if (!back) {
+			m_pos = 0;
+			init();
+		} else {
+			m_pos = m_seq.length() - m_k;
+			initFromBack();
+		}
 	}
 
 	/**
@@ -140,6 +213,28 @@ public:
 
 		/* find first "good" k-mer in sequence */
 		next();
+	}
+
+	/**
+	 * Initialize internal state of iterator.
+	 */
+	void initFromBack()
+	{
+		/* note: empty spaced seed indicates no masking (string of '1's) */
+		assert(MaskedKmer::mask().empty() || MaskedKmer::mask().length() == m_k);
+
+		/* convert sequence to upper case */
+		std::transform(m_seq.begin(), m_seq.end(), m_seq.begin(), ::toupper);
+
+		/* record positions of non-ACGT chars */
+		size_t i = m_seq.find_first_not_of(ACGT_CHARS);
+		while (i != std::string::npos) {
+			m_badCharPos.push_back(i);
+			i = m_seq.find_first_not_of(ACGT_CHARS, i + 1);
+		}
+
+		/* find first "good" k-mer in sequence */
+		prev();
 	}
 
 	/** get reference to hash values for current k-mer */
@@ -168,12 +263,28 @@ public:
 		next();
 		return *this;
 	}
+	
+	/** pre-decrement operator */
+	RollingHashIterator& operator--()
+	{
+		--m_pos;
+		prev();
+		return *this;
+	}
 
 	/** post-increment operator */
 	RollingHashIterator operator++(int)
 	{
 		RollingHashIterator it = *this;
 		++*this;
+		return it;
+	}
+
+	/** post-decrement operator */
+	RollingHashIterator operator--(int)
+	{
+		RollingHashIterator it = *this;
+		--*this;
 		return it;
 	}
 
