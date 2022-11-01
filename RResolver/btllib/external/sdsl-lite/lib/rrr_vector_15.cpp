8b9ed5864ee2b793befd014e191e6597e9dc7ade
Johnathan Wong <jowong@jowong07.phage.bcgsc.ca>
2022-11-01 10:14:23 -0700

    remove btllib header files

--- RResolver/btllib/external/sdsl-lite/lib/rrr_vector_15.cpp
+++
@@ -1,9 +0,0 @@
-#include "sdsl/rrr_vector_15.hpp"
-
-//! Namespace for the succinct data structure library
-namespace sdsl
-{
-// initialize the inner class
-binomial15::impl binomial15::iii;
-
-} // end namespace
